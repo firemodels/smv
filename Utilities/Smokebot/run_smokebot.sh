4947e9ddcfe43322d9d12ea45221b6720198cc88
gforney <gforney@gmail.com>
2016-09-04 16:27:38 -0400

    smokebot: minor edit

--- Utilities/Smokebot/run_smokebot.sh
+++ Utilities/Smokebot/run_smokebot.sh
@@ -188,7 +188,7 @@ if [ "$KILL_SMOKEBOT" == "1" ]; then
     kill -9 $PID
     JOBIDS=`qstat -a | grep SB_ | awk -v user="$USER" '{if($2==user){print $1}}'`
     if [ "$JOBIDS" != "" ]; then
-      echo killing smokebot jobs with ID: $JOBIDS
+      echo killing smokebot jobs with Id: $JOBIDS
       qdel $JOBIDS
     fi
     echo smokebot process $PID killed
