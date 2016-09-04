62ce694de6793b122de88fb982fc7f6c440c7781
gforney <gforney@gmail.com>
2016-09-04 16:36:46 -0400

    smokebot: rearrange work flow to make it more efficient

--- Utilities/Smokebot/smokebot.sh
+++ Utilities/Smokebot/smokebot.sh
@@ -774,14 +774,13 @@ run_verification_cases_debug()
    # Submit SMV verification cases and wait for them to start
    echo 'Running SMV verification cases:' >> $OUTPUT_DIR/stage3a 2>&1
    ./Run_SMV_Cases.sh -c $cfastrepo -I $COMPILER $USEINSTALL2 -m 2 -d -q $SMOKEBOT_QUEUE -j $JOBPREFIX >> $OUTPUT_DIR/stage3a 2>&1
-
-   # Wait for SMV verification cases to end
-   wait_verification_cases_debug_end
-
 }
 
 check_verification_cases_debug()
 {
+   # Wait for SMV verification cases to end
+   wait_verification_cases_debug_end
+
    # Scan and report any errors in FDS verification cases
    cd $fdsrepo/smv/Verification
 
@@ -1016,13 +1015,13 @@ run_verification_cases_release()
    cd $fdsrepo/smv/Verification/scripts
    echo 'Running SMV verification cases:' >> $OUTPUT_DIR/stage3b 2>&1
    ./Run_SMV_Cases.sh -c $cfastrepo -I $COMPILER $USEINSTALL2 $RUN_OPENMP -q $SMOKEBOT_QUEUE -j $JOBPREFIX >> $OUTPUT_DIR/stage3b 2>&1
-
-   # Wait for all verification cases to end
-   wait_verification_cases_release_end
 }
 
 check_verification_cases_release()
 {
+   # Wait for all verification cases to end
+   wait_verification_cases_release_end
+
    # Scan and report any errors in FDS verification cases
    cd $fdsrepo/smv/Verification
 
@@ -1566,12 +1565,23 @@ BUILDSOFTWARE_beg=`GET_TIME`
 compile_cfast
 compile_fds_mpi_db
 check_compile_fds_mpi_db
+if [[ $stage1b_fdsdb_success && "$RUNDEBUG" == "1" ]] ; then
+   run_verification_cases_debug
+fi
 
 if [ "$SMOKEBOT_LITE" == "" ]; then
 if [[ $stage1b_fdsdb_success ]] ; then
    compile_fds_mpi
    check_compile_fds_mpi
 fi
+fi
+if [[ $stage1b_fdsdb_success && "$RUNDEBUG" == "1" ]] ; then
+   check_verification_cases_debug
+fi
+if [ "$SMOKEBOT_LITE" == "" ]; then
+  if [[ $stage1c_fdsrel_success ]] ; then
+     run_verification_cases_release
+  fi
 fi
 
 ### Stage 2 build smokeview ###
@@ -1592,14 +1602,9 @@ echo "Build Software: $DIFF_BUILDSOFTWARE" >> $STAGE_STATUS
 
 ### Stage 3 run verification cases ###
 RUNCASES_beg=`GET_TIME`
-if [[ $stage1b_fdsdb_success && "$RUNDEBUG" == "1" ]] ; then
-   run_verification_cases_debug
-   check_verification_cases_debug
-fi
 
 if [ "$SMOKEBOT_LITE" == "" ]; then
   if [[ $stage1c_fdsrel_success ]] ; then
-     run_verification_cases_release
      check_verification_cases_release
   fi
 fi
