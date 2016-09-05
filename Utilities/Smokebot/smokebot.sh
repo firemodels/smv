d17c5326c778e7e4f03f5e0bdc74a6c8a5a998ef
gforney <gforney@gmail.com>
2016-09-04 20:20:00 -0400

    smokebot: correct executable properties of some scripts, fix bug in smokeview

--- Utilities/Smokebot/smokebot.sh
+++ Utilities/Smokebot/smokebot.sh
@@ -1344,9 +1344,11 @@ archive_timing_stats()
   cp smv_timing_stats.csv "$HISTORY_DIR/${GIT_REVISION}_timing.csv"
   cp smv_benchmarktiming_stats.csv "$HISTORY_DIR/${GIT_REVISION}_benchmarktiming.csv"
   TOTAL_SMV_TIMES=`tail -1 smv_benchmarktiming_stats.csv`
-  if [ "$UPLOADRESULTS" == "1" && "$USER" == "smokebot" ]; then
-    cd $fdsrepo/smv/Utilities/Smokebot
-    ./smvstatus_updatepub.sh -F
+  if [ "$UPLOADRESULTS" == "1" ]; then
+     if [ "$USER" == "smokebot" ]; then
+      cd $fdsrepo/smv/Utilities/Smokebot
+      ./smvstatus_updatepub.sh -F
+    fi
   fi
   if [ ! "$web_DIR" == "" ]; then
     cd $fdsrepo/smv/Utilities/Smokebot
