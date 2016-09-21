4eecc96dd561d7b9de337f039d17e952fc512d9e
gforney <gforney@gmail.com>
2016-09-21 11:02:41 -0400

    minor edits

--- scripts/SHOW_revisions.bat
+++ scripts/SHOW_revisions.bat
@@ -20,51 +20,54 @@ call %envfile%
 
 echo.
 echo ------------------------------------------------------------------------
-echo Show revisions for the Windows repository, %svn_root%
+echo *** fds ***
 %svn_drive%
 cd %svn_root%\fds
-echo repo:%svn_root%\fds
-git describe --dirty
-echo.
-
-cd %svn_root%\smv
-echo repo:%svn_root%\smv
-git describe --dirty
-echo.
-
-cd %svn_root%\webpages
-echo repo:%svn_root%\webpages
+echo Windows
 git describe --dirty
 echo.
 
 set scriptdir=%linux_svn_root%/fds/Utilities/Scripts/
 set linux_fdsdir=%linux_svn_root%
 
-echo.
-echo ------------------------------------------------------------------------
-echo Show revisision for %linux_svn_root%/fds, on %linux_hostname%
+echo Linux
 plink %linux_logon% %scriptdir%/showrevision.sh  %linux_svn_root%/fds %linux_hostname%
 echo.
 
-echo Show revisision for %linux_svn_root%/smv, on %linux_hostname%
+echo OSX
+plink %osx_logon% %scriptdir%/showrevision_onhost.sh  %linux_svn_root%/fds %osx_hostname%
+echo.
+
+
+echo.
+echo ------------------------------------------------------------------------
+echo *** smv ***
+cd %svn_root%\smv
+echo Windows
+git describe --dirty
+echo.
+
+echo Linux
 plink %linux_logon% %scriptdir%/showrevision.sh  %linux_svn_root%/smv %linux_hostname%
 echo.
 
-echo Show revisision for %linux_svn_root%/webpages, on %linux_hostname%
-plink %linux_logon% %scriptdir%/showrevision.sh  %linux_svn_root%/webpages %linux_hostname%
+echo OSX
+plink %osx_logon% %scriptdir%/showrevision_onhost.sh  %linux_svn_root%/smv %osx_hostname%
 echo.
 
 echo.
 echo ------------------------------------------------------------------------
-echo Show revisision for %linux_svn_root%/fds, on %osx_hostname%
-plink %osx_logon% %scriptdir%/showrevision_onhost.sh  %linux_svn_root%/fds %osx_hostname%
+echo *** webpages ***
+cd %svn_root%\webpages
+echo Windows
+git describe --dirty
 echo.
 
-echo Show revisision for %linux_svn_root%/smv, on %osx_hostname%
-plink %osx_logon% %scriptdir%/showrevision_onhost.sh  %linux_svn_root%/smv %osx_hostname%
+echo Linux
+plink %linux_logon% %scriptdir%/showrevision.sh  %linux_svn_root%/webpages %linux_hostname%
 echo.
 
-echo Show revisision for %linux_svn_root%/webpages, on %osx_hostname%
+echo OSX
 plink %osx_logon% %scriptdir%/showrevision_onhost.sh  %linux_svn_root%/webpages %osx_hostname%
 echo.
 pause
