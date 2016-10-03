35e0acb2e5b33cce32e298a594c93f7108ea84ac
gforney <gforney@gmail.com>
2016-10-02 23:32:44 -0400

    guide scripts: revert debug error in smv ug, change build guide scripts to only output one  extra line when finding an error

--- Manuals/SMV_Technical_Reference_Guide/make_guide.sh
+++ Manuals/SMV_Technical_Reference_Guide/make_guide.sh
@@ -22,7 +22,7 @@ if [[ `grep -E "Undefined control sequence|Error:|Fatal error|! LaTeX Error:|Par
       :
    else
       echo "LaTeX errors detected:"
-      grep -A 3 -B 3 -E "Undefined control sequence|Error:|Fatal error|! LaTeX Error:|Paragraph ended before|Missing \\\$ inserted|Misplaced" -I SMV_Technical_Reference_Guide.err | grep -v "xpdf supports version 1.5"
+      grep -A 1 -E "Undefined control sequence|Error:|Fatal error|! LaTeX Error:|Paragraph ended before|Missing \\\$ inserted|Misplaced" -I SMV_Technical_Reference_Guide.err | grep -v "xpdf supports version 1.5"
       clean_build=0
 fi
 
