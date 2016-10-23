546dc90ec7b55434b3ac9447b7c2130ae74f0eeb
gforney <gforney@gmail.com>
2016-10-23 15:34:01 -0400

    bundle scripts: update bundle/build setup script

--- scripts/fds_smv_env.bat
+++ scripts/fds_smv_env.bat
@@ -1,38 +1,34 @@
 @echo off
 
-:: ---- FDS and smokeview version strings for bundle
-set fds_version=6.5.0
-set smv_version=6.3.7
+:: ---- official FDS and smokeview version strings
+set fds_version=6.5.3
+set smv_version=6.4.1
 
-:: ---- FDS and smokeview revision version strings
-::      (when creating test releases)
-set smv_revision=3057a8c
+:: ---- test FDS and smokeview version strings
+set smv_revision=SMV6.4.1-16-g975c1cf
 set fds_revision=02a6097
 
 :: ---- for obtaining log entries
-set smvlogdate="01-Mar-2016"
+set smvlogdate="7-Oct-2016"
 
 :: PC repo
 
 set svn_root=%userprofile%\FireModels_fork
+set fdswikirepo=%svn_root%\wikis
 set svn_drive=c:
 
 :: Linux/OSX repo
 
 set linux_svn_root=FireModels_fork
-set firebotrepo=/home2/smokevis2/firebot/FireModels_central
-set smokebotrepo=/home2/smokevis2/smokebot/FireModels_central
-set fdswikirepo=%svn_root%\wikis
 
-:: FDS/Smokeview version
+:: firebot/smokebot repos
 
-set fdssmv_major_version=6
-set fds_edition=FDS6
-set smv_edition=SMV6
+set firebotrepo=/home4/firebot/FireModels_central
+set smokebotrepo=/home4/smokebot/FireModels_central
 
 :: Linux user and host name
 
-set linux_hostname=blaze.nist.gov
+set linux_hostname=burn.el.nist.gov
 set linux_username=%username%
 set linux_logon=%linux_username%@%linux_hostname%
 
@@ -41,3 +37,9 @@ set linux_logon=%linux_username%@%linux_hostname%
 set osx_hostname=bluesky.el.nist.gov
 set osx_username=%username%
 set osx_logon=%osx_username%@%osx_hostname%
+
+:: FDS/Smokeview version
+
+set fdssmv_major_version=6
+set fds_edition=FDS6
+set smv_edition=SMV6
