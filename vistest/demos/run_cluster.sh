e1dfffff0bbf68250e9ae2cd3a842e09e6ef8239
gforney <gforney@gmail.com>
2016-11-24 09:24:41 -0500

    smv vv: corrections to run cluster script

--- vistest/demos/run_cluster.sh
+++ vistest/demos/run_cluster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+QFDS="qfds.sh -e ../../../fds/Build/mpi_intel_linux_64ib/fds_mpi_intel_linux_64ib"
 
-qfds.sh cluster1.fds
-qfds.sh cluster1b.fds
-qfds.sh cluster2.fds
-qfds.sh cluster2b.fds
+$QFDS cluster1.fds
+$QFDS cluster1b.fds
+$QFDS cluster2.fds
+$QFDS cluster2b.fds
