f7d9f77a660b664304563136647119f7e2427526
gforney <gforney@gmail.com>
2017-01-03 21:18:33 -0500

    smv build: correction to osx smokeview build script

--- Build/smokeview/intel_osx_64/make_smv.sh
+++ Build/smokeview/intel_osx_64/make_smv.sh
@@ -2,6 +2,7 @@
 source ../../scripts/setopts.sh $*
 LIBDIR=../../LIBS/intel_osx_64/
 source ../../scripts/test_libs.sh
+source $IFORT_COMPILER/bin/compilervars.sh intel64
 
 make -f ../Makefile clean
 eval make -j 4 ${SMV_MAKE_OPTS}-f ../Makefile intel_osx_64
