c83ae5acd8a522eaba83cef76c79bedd8d04f666
gforney <gforney@gmail.com>
2017-01-17 14:46:23 -0500

    smokeview source: add pp_NAN directive to make it easy to turn off NaN checking

--- Source/smokeview/update.c
+++ Source/smokeview/update.c
@@ -910,7 +910,8 @@ void ConvertSsf(void){
   }
 }
 
-  /* ------------------ test_for_nan ------------------------ */
+#ifdef pp_NAN
+/* ------------------ test_for_nan ------------------------ */
 
 void test_for_nan(float *vals, int nvals, char *array, char *label){
   int have_nan=0, i;
@@ -932,7 +933,11 @@ void test_for_nan(float *vals, int nvals, char *array, char *label){
     }
     fprintf(stderr,"\n");
   }
+#define TEST_FOR_NAN(a,b,c,d) test_for_nan(a,b,c,d)
 }
+#else
+#define TEST_FOR_NAN(a,b,c,d)
+#endif
 
   /* ------------------ UpdateTimes ------------------------ */
 
@@ -1069,7 +1074,7 @@ void UpdateTimes(void){
     geomi = geominfoptrs[i];
     if(geomi->loaded==0||geomi->display==0)continue;
     memcpy(global_times + nglobal_times_copy, geomi->times, geomi->ntimes * sizeof(float));
-    test_for_nan(geomi->times, geomi->ntimes, "geomi->times", NULL);
+    TEST_FOR_NAN(geomi->times, geomi->ntimes, "geomi->times", NULL);
     nglobal_times_copy += geomi->ntimes;
   }
   if(visTerrainType!=TERRAIN_HIDDEN){
@@ -1079,7 +1084,7 @@ void UpdateTimes(void){
       terri = terraininfo + i;
       if(terri->loaded==0)continue;
       memcpy(global_times + nglobal_times_copy, terri->times, terri->ntimes * sizeof(float));
-      test_for_nan(terri->times, terri->ntimes, "terri->times", NULL);
+      TEST_FOR_NAN(terri->times, terri->ntimes, "terri->times", NULL);
       nglobal_times_copy += terri->ntimes;
     }
   }
@@ -1095,7 +1100,7 @@ void UpdateTimes(void){
     touri = tourinfo + i;
     if(touri->display==0)continue;
     memcpy(global_times + nglobal_times_copy, touri->path_times, touri->ntimes * sizeof(float));
-    test_for_nan(touri->path_times, touri->ntimes, "touri->path_times", NULL);
+    TEST_FOR_NAN(touri->path_times, touri->ntimes, "touri->path_times", NULL);
     nglobal_times_copy += touri->ntimes;
   }
 
@@ -1106,7 +1111,7 @@ void UpdateTimes(void){
     parti = partinfo + i;
     if(parti->loaded==0)continue;
     memcpy(global_times + nglobal_times_copy, parti->times, parti->ntimes * sizeof(float));
-    test_for_nan(parti->times, parti->ntimes, "parti->times", NULL);
+    TEST_FOR_NAN(parti->times, parti->ntimes, "parti->times", NULL);
     nglobal_times_copy += parti->ntimes;
     tmax_part=MAX(parti->times[parti->ntimes-1],tmax_part);
   }
@@ -1117,7 +1122,7 @@ void UpdateTimes(void){
     sd = sliceinfo + i;
     if(sd->loaded==1||sd->vloaded==1){
       memcpy(global_times + nglobal_times_copy, sd->times, sd->ntimes * sizeof(float));
-      test_for_nan(sd->times, sd->ntimes, "sd->times", NULL);
+      TEST_FOR_NAN(sd->times, sd->ntimes, "sd->times", NULL);
       nglobal_times_copy += sd->ntimes;
     }
   }
@@ -1128,7 +1133,7 @@ void UpdateTimes(void){
     patchi = patchinfo + i;
     if(patchi->loaded==1&&patchi->filetype==PATCH_GEOMETRY){
       memcpy(global_times + nglobal_times_copy, patchi->geom_times, patchi->ngeom_times * sizeof(float));
-      test_for_nan(patchi->geom_times, patchi->ngeom_times, "patchi->geom_times", NULL);
+      TEST_FOR_NAN(patchi->geom_times, patchi->ngeom_times, "patchi->geom_times", NULL);
       nglobal_times_copy += patchi->ngeom_times;
     }
   }
@@ -1143,7 +1148,7 @@ void UpdateTimes(void){
       patchi = patchinfo + filenum;
       if(patchi->loaded==1&&patchi->filetype!=PATCH_GEOMETRY){
         memcpy(global_times + nglobal_times_copy, meshi->patch_times, meshi->npatch_times * sizeof(float));
-        test_for_nan(meshi->patch_times, meshi->npatch_times, "meshi->patch_times", NULL);
+        TEST_FOR_NAN(meshi->patch_times, meshi->npatch_times, "meshi->patch_times", NULL);
         nglobal_times_copy += meshi->npatch_times;
       }
     }
@@ -1158,13 +1163,13 @@ void UpdateTimes(void){
       if(vr->smokeslice==NULL)continue;
       if(vr->loaded==0||vr->display==0)continue;
       memcpy(global_times + nglobal_times_copy, vr->times, vr->ntimes * sizeof(float));
-      test_for_nan(vr->times, vr->ntimes, "vr->times", NULL);
+      TEST_FOR_NAN(vr->times, vr->ntimes, "vr->times", NULL);
       nglobal_times_copy += vr->ntimes;
     }
   }
   if(ReadZoneFile==1&&visZone==1){
     memcpy(global_times + nglobal_times_copy, zone_times, nzone_times * sizeof(float));
-    test_for_nan(zone_times, nzone_times, "zone_times", NULL);
+    TEST_FOR_NAN(zone_times, nzone_times, "zone_times", NULL);
     nglobal_times_copy += nzone_times;
   }
   if(ReadIsoFile==1&&visAIso!=0){
@@ -1176,7 +1181,7 @@ void UpdateTimes(void){
       if(ib->geomflag==1||ib->loaded==0)continue;
       meshi=meshinfo + ib->blocknumber;
       memcpy(global_times + nglobal_times_copy, meshi->iso_times, meshi->niso_times * sizeof(float));
-      test_for_nan(meshi->iso_times, meshi->niso_times, "meshi->iso_times", NULL);
+      TEST_FOR_NAN(meshi->iso_times, meshi->niso_times, "meshi->iso_times", NULL);
       nglobal_times_copy += meshi->niso_times;
     }
   }
@@ -1187,7 +1192,7 @@ void UpdateTimes(void){
       smoke3di = smoke3dinfo + i;
       if(smoke3di->loaded==0)continue;
       memcpy(global_times + nglobal_times_copy, smoke3di->times, smoke3di->ntimes * sizeof(float));
-      test_for_nan(smoke3di->times, smoke3di->ntimes, "smoke3di->times", NULL);
+      TEST_FOR_NAN(smoke3di->times, smoke3di->ntimes, "smoke3di->times", NULL);
       nglobal_times_copy += smoke3di->ntimes;
     }
   }
@@ -1204,12 +1209,12 @@ void UpdateTimes(void){
 
     memcpy(global_times_copy, global_times, nglobal_times * sizeof(float));
 
-    test_for_nan(global_times_copy,nglobal_times,"global_times_copy","(before qsort)");
+    TEST_FOR_NAN(global_times_copy,nglobal_times,"global_times_copy","(before qsort)");
 
     qsort((float *)global_times_copy, (size_t)nglobal_times, sizeof(float), CompareFloat);
     CheckMemory;
 
-    test_for_nan(global_times_copy,nglobal_times,"global_times_copy","(after qsort)");
+    TEST_FOR_NAN(global_times_copy,nglobal_times,"global_times_copy","(after qsort)");
 
 
 #define DT_EPS 0.0001
@@ -1225,7 +1230,7 @@ void UpdateTimes(void){
     nglobal_times = to;
     FREEMEMORY(global_times_copy);
 
-    test_for_nan(global_times,nglobal_times,"global_times","(after remove dup)");
+    TEST_FOR_NAN(global_times,nglobal_times,"global_times","(after remove dup)");
 
     for(n = 0; n < nglobal_times-1; n++){
       int i;
