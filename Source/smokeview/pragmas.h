26042814e3e4dc18aebfe8f13520ffb854c0abf4
gforney <gforney@gmail.com>
2017-02-06 14:45:09 -0500

    smokeview source: add pragma to ignore compiler warning (external function definition with no prior declaration

--- Source/smokeview/pragmas.h
+++ Source/smokeview/pragmas.h
@@ -4,6 +4,7 @@
 #pragma warning (disable:695)  // calling convention if ignored for this type 
 #pragma warning (disable:2557) // comparison between signed and unsigned operands 
 #pragma warning (disable:869)  // parameter was never referenced 
+#pragma warning (disable:1418) // external function definition with no prior declaration
 #pragma warning (disable:3457) // conversion from a string literal to "char *" is deprecated 
 #endif
 #endif
