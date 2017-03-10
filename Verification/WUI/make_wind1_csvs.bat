377c05f6a1f36261e96d6baaf3076e2e103e065a
gforney <gforney@gmail.com>
2017-03-10 10:44:39 -0500

    scripts: corrections to make wind script

--- Verification/WUI/make_wind1_csvs.bat
+++ Verification/WUI/make_wind1_csvs.bat
@@ -1,7 +1,15 @@
 @echo off
-set convert=..\..\utilities\wind2fds\intel_win_64\wind2fds_win_64
+set convert=..\..\Build\wind2fds\intel_win_64\wind2fds_win_64.exe
 
-%convert% -prefix sd11 -offset "100.0 100.0 0.0" wind_test1a.csv
+if exist %convert% goto convert_wind
+echo "***error: The program %convert% does not exist"
+
+goto eof
+
+:convert_wind
+%convert% -prefix sd11 -offset "100.0 100.0 0.0" wind_data1a.csv
 Rem %convert% -prefix sd12 -offset " 50.0 150.0 0.0" wind_test1b.csv
 Rem %convert% -prefix sd21 -offset "150.0  50.0 0.0" wind_test1c.csv
 Rem %convert% -prefix sd22 -offset "150.0 150.0 0.0" wind_test1d.csv
+
+:eof
