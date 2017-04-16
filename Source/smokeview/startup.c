4201587c2de7358d4de0a51d2b945ffeefa45553
gforney <gforney@gmail.com>
2017-04-16 18:00:42 -0400

    smokeview source: revert changes profile changes to osx smokeview

--- Source/smokeview/startup.c
+++ Source/smokeview/startup.c
@@ -514,9 +514,9 @@ void InitOpenGL(void){
 #ifdef _DEBUG
   PRINTF("%s",_("   Initializing Glut display mode - "));
 #endif
-#ifdef pp_OSX
-  type|=GLUT_3_2_CORE_PROFILE;
-#endif
+//#ifdef pp_OSX
+//  type|=GLUT_3_2_CORE_PROFILE;
+//#endif
   glutInitDisplayMode(type);
 #ifdef _DEBUG
   PRINTF("%s\n",_("initialized"));
