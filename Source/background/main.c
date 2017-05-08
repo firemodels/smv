1b1b88740f64d39fe383753df835017172b54944
gforney <gforney@gmail.com>
2017-05-08 16:23:19 -0400

    background source: code clean up - convert tabs to spaces

--- Source/background/main.c
+++ Source/background/main.c
@@ -118,8 +118,8 @@ int main(int argc, char **argv){
             }
 #ifdef pp_LINUXOSX            
             else{
-		          debug=1;
-		        }
+              debug=1;
+            }
 #endif		        
             break;
           case 'h':
@@ -174,7 +174,7 @@ int main(int argc, char **argv){
             printf("Unknown option: %s\n",arg);
             usage(argv[0]);
             return 1;
-	      }
+        }
       }
     }
     else{
@@ -347,74 +347,70 @@ static HMODULE s_hKernel = NULL;
 /* ------------------ GetSystemTimesAddress ------------------------ */
 
 void GetSystemTimesAddress(){
-	if( s_hKernel == NULL )
-	{
-		s_hKernel = LoadLibrary((wchar_t *)"Kernel32.dll" );
-		if( s_hKernel != NULL )
-		{
-			s_pfnGetSystemTimes = (pfnGetSystemTimes)GetProcAddress( s_hKernel, "GetSystemTimes" );
-			if( s_pfnGetSystemTimes == NULL )
-			{
-				FreeLibrary( s_hKernel ); s_hKernel = NULL;
-			}
-		}
-	}
+  if( s_hKernel == NULL ){
+    s_hKernel = LoadLibrary((wchar_t *)"Kernel32.dll" );
+    if( s_hKernel != NULL ){
+      s_pfnGetSystemTimes = (pfnGetSystemTimes)GetProcAddress( s_hKernel, "GetSystemTimes" );
+      if( s_pfnGetSystemTimes == NULL ){
+        FreeLibrary( s_hKernel ); s_hKernel = NULL;
+      }
+    }
+  }
 }
 
 /* ------------------ cpuusage ------------------------ */
 
-unsigned char cpuusage()
-{
-	FILETIME               ft_sys_idle;
-	FILETIME               ft_sys_kernel;
-	FILETIME               ft_sys_user;
+unsigned char cpuusage(){
+  FILETIME               ft_sys_idle;
+  FILETIME               ft_sys_kernel;
+  FILETIME               ft_sys_user;
 
-	ULARGE_INTEGER         ul_sys_idle;
-	ULARGE_INTEGER         ul_sys_kernel;
-	ULARGE_INTEGER         ul_sys_user;
+  ULARGE_INTEGER         ul_sys_idle;
+  ULARGE_INTEGER         ul_sys_kernel;
+  ULARGE_INTEGER         ul_sys_user;
 
-	static ULARGE_INTEGER	 ul_sys_idle_old;
-	static ULARGE_INTEGER  ul_sys_kernel_old;
-	static ULARGE_INTEGER  ul_sys_user_old;
+  static ULARGE_INTEGER	 ul_sys_idle_old;
+  static ULARGE_INTEGER  ul_sys_kernel_old;
+  static ULARGE_INTEGER  ul_sys_user_old;
 
-	unsigned char usage_local = 0;
+  unsigned char usage_local = 0;
 
-	// we cannot directly use GetSystemTimes on C language
-	/* add this line :: pfnGetSystemTimes */
-	s_pfnGetSystemTimes(&ft_sys_idle,    /* System idle time */
-		&ft_sys_kernel,  /* system kernel time */
-		&ft_sys_user);   /* System user time */
+// we cannot directly use GetSystemTimes on C language
+/* add this line :: pfnGetSystemTimes */
+  s_pfnGetSystemTimes(&ft_sys_idle,    /* System idle time */
+  &ft_sys_kernel,  /* system kernel time */
+  &ft_sys_user);   /* System user time */
 
-	CopyMemory(&ul_sys_idle  , &ft_sys_idle  , sizeof(FILETIME)); // Could been optimized away...
-	CopyMemory(&ul_sys_kernel, &ft_sys_kernel, sizeof(FILETIME)); // Could been optimized away...
-	CopyMemory(&ul_sys_user  , &ft_sys_user  , sizeof(FILETIME)); // Could been optimized away...
+  CopyMemory(&ul_sys_idle  , &ft_sys_idle  , sizeof(FILETIME)); // Could been optimized away...
+  CopyMemory(&ul_sys_kernel, &ft_sys_kernel, sizeof(FILETIME)); // Could been optimized away...
+  CopyMemory(&ul_sys_user  , &ft_sys_user  , sizeof(FILETIME)); // Could been optimized away...
 
-	usage_local  =
-		(
-		(
-		(
-		(
-		(ul_sys_kernel.QuadPart - ul_sys_kernel_old.QuadPart)+
-		(ul_sys_user.QuadPart   - ul_sys_user_old.QuadPart)
-		)
-		-
-		(ul_sys_idle.QuadPart-ul_sys_idle_old.QuadPart)
-		)
-		*
-		(100)
-		)
-		/
-		(
-		(ul_sys_kernel.QuadPart - ul_sys_kernel_old.QuadPart)+
-		(ul_sys_user.QuadPart   - ul_sys_user_old.QuadPart)
-		)
-		);
+  usage_local  =
+    (
+    (
+    (
+    (
+    (ul_sys_kernel.QuadPart - ul_sys_kernel_old.QuadPart)+
+    (ul_sys_user.QuadPart   - ul_sys_user_old.QuadPart)
+    )
+    -
+    (ul_sys_idle.QuadPart-ul_sys_idle_old.QuadPart)
+    )
+    *
+    (100)
+    )
+    /
+    (
+    (ul_sys_kernel.QuadPart - ul_sys_kernel_old.QuadPart)+
+    (ul_sys_user.QuadPart   - ul_sys_user_old.QuadPart)
+    )
+    );
 
-	ul_sys_idle_old.QuadPart   = ul_sys_idle.QuadPart;
-	ul_sys_user_old.QuadPart   = ul_sys_user.QuadPart;
-	ul_sys_kernel_old.QuadPart = ul_sys_kernel.QuadPart;
+  ul_sys_idle_old.QuadPart   = ul_sys_idle.QuadPart;
+  ul_sys_user_old.QuadPart   = ul_sys_user.QuadPart;
+  ul_sys_kernel_old.QuadPart = ul_sys_kernel.QuadPart;
 
-	return usage_local;
+  return usage_local;
 }
 #endif
 #ifdef pp_OSX
