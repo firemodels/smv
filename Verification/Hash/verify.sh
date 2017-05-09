9f48127020e854378ea0feb26113f88456f1d682
gforney <gforney@gmail.com>
2017-05-09 13:20:59 -0400

    hashfile vv: add a script for verifiying the program hashfile

---
+++ Verification/Hash/verify.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+CURDIR=`pwd`
+
+platform="linux"
+if [ "`uname`" == "Darwin" ]; then
+  platform="osx"
+fi
+
+HASHFILE=../../Build/hashfile/intel_${platform}_64/hashfile_${platform}_64
+if [ ! -e $HASHFILE ]; then
+  echo "***error: the program $HASHFILE does not exist"
+  echo "verification aborted"
+  exit
+fi
+
+$HASHFILE -hash_all base > result
+ndiff=`diff result reference | wc -l`
+if [ $ndiff -eq 0 ]; then
+  echo The hashes computed by hashfile match the reference file
+else
+  echo "***Warning: the hashes computed by hashfile do not match the reference file"
+  diff reference result
+fi
