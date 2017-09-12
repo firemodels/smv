628690ff4e01eb1759e232fe3e495dbb4c282c61
gforney <gforney@gmail.com>
2017-09-12 14:47:49 -0400

    bundle scripts: remove unused variable

--- scripts/fds_smv_env.bat
+++ scripts/fds_smv_env.bat
@@ -26,10 +26,9 @@ set svn_drive=c:
 
 ::*** Linux/OSX
 set linux_svn_root=FireModels_fork
-set compiler_dir=fire-notes/INSTALL/LIBS/LINUX/LIB64_i17u2
+set compiler_dir=fire-notes/INSTALL/LIBS/LINUX/LIB64_i17
 set misc_dir=fire-notes/INSTALL/LIBS/LINUX/LIB64
 set openmpi_version=2.1.1
-set linux_ib=ib
 
 ::*** firebot/smokebot
 set firebotrepo=/home2/smokevis2/firebot/FireModels_central
