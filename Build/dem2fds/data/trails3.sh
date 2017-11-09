b7835bec35f045039406fe38693a50efb2047e07
gforney <gforney@gmail.com>
2017-11-08 21:59:46 -0500

    smv vv: make trail3 script executable and make some corrections

--- Build/dem2fds/data/trails3.sh
+++ Build/dem2fds/data/trails3.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-option=$1 $2 
-if [ "$option" == ""]; then
-  option=-obst
-fi
 
 dem2fds=dem2fds
 #dem2fds=../intel_linux_64/dem2fds_linux_64
 
 terraindir=~/terrain
 
-$dem2fds $option -dir $terraindir/trails trails3.in 
-$dem2fds $option -dir $terraindir/trails trails3g.in 
+$dem2fds -obst -dir $terraindir/trails trails3.in 
+$dem2fds -geom -dir $terraindir/trails trails3g.in 
