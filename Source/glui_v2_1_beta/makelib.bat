768560f92804cf3be348be76613e272c64ab6091
gforney <gforney@gmail.com>
2017-11-29 22:59:00 -0500

    glui build: use updated target to build windows glui

--- Source/glui_v2_1_beta/makelib.bat
+++ Source/glui_v2_1_beta/makelib.bat
@@ -5,6 +5,6 @@ title Building glui library
 erase *.o *.obj libglui.a libglui.lib
 set target=libglui.lib
 if %COMPILER% == gcc set target=libglui.a
-make COMPILER=%COMPILER% COMPILER2=%COMPILER2% SIZE=%SIZE% RM=erase -f ./makefile %target%
+make COMPILER=%COMPILER% COMPILER2=%COMPILER2% SIZE=%SIZE% RM=erase intel_win_64 -f ./makefile %target%
 if %COPYLIB% == 1 copy %FROMLIB% %TOLIB%
 endlocal
