ccd462683e42ba882ba3bf3ba929b136c4a4023c
gforney <gforney@gmail.com>
2018-01-04 15:53:19 -0500

    smv vv: update run_cluster scripts

--- vistest/demos/run_cluster.sh
+++ vistest/demos/run_cluster.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 QFDS="qfds.sh -I -p 8 -e ../../../fds/Build/impi_intel_linux_64/fds_impi_intel_linux_64"
 
+$QFDS chassis.fds
 $QFDS cluster1.fds
-$QFDS cluster1b.fds
-$QFDS cluster2.fds
-$QFDS cluster2b.fds
