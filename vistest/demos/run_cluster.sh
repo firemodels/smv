49c5bb2c4164616dbc8e99535a3e4d8992e8fc6f
gforney <gforney@gmail.com>
2018-01-04 20:41:13 -0500

    smv vv: add open bcs at end of chassis

--- vistest/demos/run_cluster.sh
+++ vistest/demos/run_cluster.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 QFDS="../../../fds/Utilities/Scripts/qfds.sh -I "
 
-#$QFDS -p 4  chassis1.fds
-#$QFDS -p 32 chassis2.fds
-#$QFDS -p 16 cluster1.fds
+$QFDS -p   4      chassis1.fds
+$QFDS -p  32 -n 8 chassis2.fds
+$QFDS -p  4       chassis3.fds
+$QFDS -p  16 -n 8 cluster1.fds
 $QFDS -p 128 -n 8 cluster2.fds
