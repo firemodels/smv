d89dba0b05c77af06dc77f0d26e2f99b9353c9f1
Forney <gforney@gmail.com>
2018-04-17 16:22:21 -0400

    smv build: add -t option (for building test smokeview's) to osx gnu smokeview builds

--- Build/smokeview/gnu_osx_64/make_smokeview.sh
+++ Build/smokeview/gnu_osx_64/make_smokeview.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+source ../../scripts/setopts.sh $*
+
 LIBDIR=../../LIBS/gnu_osx_64/
 source ../../scripts/test_libs.sh
 
 make -f ../Makefile clean
-make -f ../Makefile gnu_osx_64
+eval make ${SMV_MAKE_OPTS} -f ../Makefile gnu_osx_64
