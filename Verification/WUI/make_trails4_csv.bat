310981460d3e2d89eb21326fdac11c2157f3e48d
gforney <gforney@gmail.com>
2018-04-18 16:15:27 -0400

    smv vv: add test case to visualize trails wind data

---
+++ Verification/WUI/make_trails4_csv.bat
@@ -0,0 +1,13 @@
+@echo off
+set convert=..\..\Build\wind2fds\intel_win_64\wind2fds_win_64.exe
+
+if exist %convert% goto convert_wind
+echo "***error: The program %convert% does not exist"
+
+goto eof
+
+:convert_wind
+copy ..\..\..\fig\smv\wind\a180210.csv a180210.csv 1> Nul 2>&1
+%convert% -prefix sd11 -offset "1500.0 1500.0 70.0" a180210.csv
+
+:eof
