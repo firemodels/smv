b548d33f813587e34c33eb2fc1e9e1b236a5147e
gforney <gforney@gmail.com>
2018-04-21 16:12:51 -0400

    merge script: minor edit

--- for_bundle/update_pos.bat
+++ for_bundle/update_pos.bat
@@ -8,6 +8,6 @@ set MERGEPODIR=%ROOT%\Build\mergepo\intel_win_64
 set MERGEPO=%MERGEPODIR%\mergepo_win_64
 
 for %%a in ( smokeview_de.po smokeview_fr.pl ) do (
-  echo updating 
-::  %MERGEPO% -c %%a smokeview_template.po > %%a.revised
+  echo updating %%a 
+  %MERGEPO% -c %%a smokeview_template.po > %%a.revised
 )
