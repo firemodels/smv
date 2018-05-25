738d782a7b5194b2fa126d92fc8ab7ffc74f787d
gforney <gforney@gmail.com>
2018-05-25 14:05:06 -0400

    smv vv: add another terrain test case

---
+++ Build/dem2fds/data/test3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+option=$1 $2 
+if [ "$option" == "" ]; then
+  option=-obst
+fi
+
+#dem2fds=dem2fds
+dem2fds=../intel_linux_64/dem2fds_linux_64
+
+terraindir=~/terrain
+
+$dem2fds -dir $terraindir/gatlinburg test3.in 
+$dem2fds -geom -dir $terraindir/gatlinburg test3g.in 
+fds test3.fds 
+fds test3g.fds 
