firemodels/smv

commit 672861f2e6b676ef93192b4a18b1a84d0b111787

smokeview source: fixes to code that changes smokebot size, fixes to smoke vis when only only fire is displayed

Source/smokeview/IOsmoke.c
@@ -1272,8 +1272,15 @@ void DrawSmokeVertex(smoke3ddata *smoke3di, float *v, float *t, int *hv){
       glColor4f(color[0], color[1], color[2], alpha);
       glVertex3fv(v);
     }
+    else{
+      if(hv[VSOOT]==0){
+        use_smoke = 0;
+        glColor4f(0.0, 0.0, 0.0, 0.0);
+        glVertex3fv(v);
+      }
+    }
   }
-  if(hv[VSOOT]==1&&use_smoke==1){
+  if(use_smoke==1){
     glColor4f(0.0, 0.0, 0.0, sootval);
     glVertex3fv(v);
   }
@@ -5741,6 +5748,21 @@ void SetSmokeColorFlags(void){
   }
 }
 
+/* ------------------ SmokeWrapup ------------------------ */
+
+#define SMOKE_OPTIONS 19
+void SmokeWrapup(smoke3ddata *smoke3di){
+  if(smoke3di->finalize==1){
+    smoke3di->finalize = 0;
+    UpdateSmoke3dFileParms();
+    UpdateTimes();
+    Smoke3dCB(UPDATE_SMOKEFIRE_COLORS);
+    smoke_render_option = RENDER_SLICE;
+    Smoke3dCB(SMOKE_OPTIONS);
+    IdleCB();
+  }
+}
+
 /* ------------------ ReadSmoke3D ------------------------ */
 
 FILE_SIZE ReadSmoke3D(int iframe,int ifile,int flag, int *errorcode){
@@ -5905,18 +5927,24 @@ FILE_SIZE ReadSmoke3D(int iframe,int ifile,int flag, int *errorcode){
     return 0;
   }
   if(smoke3di->maxval>=0.0){
+    int return_flag = 0;
+
     if(smoke3di->type == HRRPUV&&smoke3di->maxval<=load_hrrpuv_cutoff){
       ReadSmoke3D(iframe,ifile,UNLOAD,&error);
       *errorcode=0;
       PRINTF(" - skipped (max hrrpuv<%0.f)\n",load_hrrpuv_cutoff);
-      return 0;
+      return_flag=1;
     }
     if(smoke3di->type == SOOT&&smoke3di->maxval<=load_3dsmoke_cutoff){
       ReadSmoke3D(iframe,ifile,UNLOAD,&error);
       *errorcode=0;
       PRINTF(" - skipped (max soot opacity<%0.f)\n", load_3dsmoke_cutoff);
-      return 0;
+      return_flag = 1;
     }
+    if(smoke3di->finalize==1){
+      SmokeWrapup(smoke3di);
+    }
+    if(return_flag==1)return 0;
   }
   CheckMemory;
   if(
@@ -6072,11 +6100,7 @@ FILE_SIZE ReadSmoke3D(int iframe,int ifile,int flag, int *errorcode){
   update_makeiblank_smoke3d=1;
   plotstate=GetPlotState(DYNAMIC_PLOTS);
   if(smoke3di->finalize == 1){
-    smoke3di->finalize = 0;
-    UpdateSmoke3dFileParms();
-    UpdateTimes();
-    Smoke3dCB(UPDATE_SMOKEFIRE_COLORS);
-    IdleCB();
+    SmokeWrapup(smoke3di);
   }
   STOP_TIMER(total_time);
 
@@ -6141,6 +6165,9 @@ void UpdateSmoke3D(smoke3ddata *smoke3di){
   int iframe_local;
   int countin;
   uLongf countout;
+#ifdef pp_GPUSMOKE
+  meshdata *smokemesh;
+#endif
 
   iframe_local = smoke3di->ismoke3d_time;
   countin = smoke3di->nchars_compressed_smoke[iframe_local];
@@ -6158,7 +6185,16 @@ void UpdateSmoke3D(smoke3ddata *smoke3di){
     ASSERT(FFALSE);
     break;
   }
-  if(smoke3di->frame_all_zeros[iframe_local] == SMOKE3D_ZEROS_UNKNOWN){
+
+#ifdef pp_GPUSMOKE
+  smokemesh = meshinfo+smoke3di->blocknumber;
+#endif
+
+  if(
+#ifdef pp_GPUSMOKE
+    smokemesh->update_smokebox==1||
+#endif
+    smoke3di->frame_all_zeros[iframe_local] == SMOKE3D_ZEROS_UNKNOWN){
     int i;
     unsigned char *smokeframe_in;
 
@@ -6173,9 +6209,8 @@ void UpdateSmoke3D(smoke3ddata *smoke3di){
     }
 #ifdef pp_GPUSMOKE
     // construct a bounding box containing smoke
-    
-    if(smoke3di->frame_all_zeros[iframe_local] != SMOKE3D_ZEROS_ALL){
-      meshdata *smokemesh;
+
+    if(smokemesh->update_smokebox==1||smoke3di->frame_all_zeros[iframe_local] != SMOKE3D_ZEROS_ALL){
       float *smoke_boxmin, *smoke_boxmax;
       float *xplt, *yplt, *zplt;
       int ibar, jbar, kbar;
@@ -6184,11 +6219,10 @@ void UpdateSmoke3D(smoke3ddata *smoke3di){
       int imin=-1, imax=-1, jmin=-1, jmax=-1, kmin=-1, kmax=-1;
       unsigned char *smokeptr;
 
-      smokemesh = meshinfo+smoke3di->blocknumber;
       xplt = smokemesh->xplt_orig;
       yplt = smokemesh->yplt_orig;
       zplt = smokemesh->zplt_orig;
-      
+
       ibar = smokemesh->ibar;
       jbar = smokemesh->jbar;
       kbar = smokemesh->kbar;
@@ -6196,7 +6230,7 @@ void UpdateSmoke3D(smoke3ddata *smoke3di){
       nx = ibar+1;
       ny = jbar+1;
       nz = kbar+1;
-      
+
       smoke_boxmin = smoke3di->smoke_boxmin+3*iframe_local;
       smoke_boxmax = smoke3di->smoke_boxmax+3*iframe_local;
 
@@ -6234,6 +6268,7 @@ void UpdateSmoke3D(smoke3ddata *smoke3di){
       smoke_boxmax[1] = yplt[jmax];
       smoke_boxmax[2] = zplt[kmax];
     }
+    smokemesh->update_smokebox = 0;
 #endif
   }
   ASSERT(countout==smoke3di->nchars_uncompressed);

Source/smokeview/flowfiles.h
@@ -626,6 +626,7 @@ typedef struct _meshdata {
   float *smoke_verts, *smoke_vals;
   int max_tris, max_verts;
   int *smoke_tris, smoke_ntris, smoke_nverts;
+  int update_smokebox;
 #endif
   GLuint     volsmoke_texture_id,     volfire_texture_id,     vollight_texture_id;
   float *volsmoke_texture_buffer,*volfire_texture_buffer,*vollight_texture_buffer;

Source/smokeview/glui_smoke.cpp
@@ -71,6 +71,7 @@ extern GLUI *glui_bounds;
 #define SMOKE_DELTA_PAR 76
 #define SMOKE_NEW 77
 #define SMOKE_DELTA_MULTIPLE 78
+#define SMOKEBOX_BUFFER 79
 
 // two defines below are also defined elsewhere
 
@@ -660,7 +661,7 @@ extern "C" void Glui3dSmokeSetup(int main_window){
     glui_3dsmoke->add_checkbox_to_panel(ROLLOUT_smoke_diag, _("performance info"), &smoke_timer);
     glui_3dsmoke->add_checkbox_to_panel(ROLLOUT_smoke_diag, _("smoke outline"), &smoke_outline);
     glui_3dsmoke->add_checkbox_to_panel(ROLLOUT_smoke_diag, _("smoke box"), &use_smokebox);
-    SPINNER_smokebox_buffer=glui_3dsmoke->add_spinner_to_panel(ROLLOUT_smoke_diag, _("smokebox buffer"), GLUI_SPINNER_INT, &smokebox_buffer);
+    SPINNER_smokebox_buffer=glui_3dsmoke->add_spinner_to_panel(ROLLOUT_smoke_diag, _("smokebox buffer"), GLUI_SPINNER_INT, &smokebox_buffer, SMOKEBOX_BUFFER, Smoke3dCB);
     SPINNER_smokebox_buffer->set_int_limits(0,5);
     glui_3dsmoke->add_checkbox_to_panel(ROLLOUT_smoke_diag, _("exact distance"), &smoke_exact_dist);
     CHECKBOX_smoke_getvals = glui_3dsmoke->add_checkbox_to_panel(ROLLOUT_smoke_diag, _("get vals"), &smoke_getvals);
@@ -842,6 +843,14 @@ extern "C" void Smoke3dCB(int var){
   float temp_min, temp_max;
   
 #ifdef pp_GPUSMOKE
+  case SMOKEBOX_BUFFER:
+    for(i = 0;i<nmeshes;i++){
+      meshdata *meshi;
+
+      meshi = meshinfo+i;
+      meshi->update_smokebox = 1;
+    }
+    break;
   case SMOKE_NEW:
     if(use_newsmoke==SMOKE3D_ORIG){
       for(i = 0;i<nmeshes;i++){

Source/smokeview/readsmv.c
@@ -349,6 +349,7 @@ void InitMesh(meshdata *meshi){
   meshi->iplotz_all = NULL;
 #ifdef pp_GPU
 #ifdef pp_GPUSMOKE
+  meshi->update_smokebox = 0;
   meshi->smoke_verts  = NULL;
   meshi->smoke_vals   = NULL;
   meshi->smoke_tris   = NULL;

Source/smokeview/shaders.c
@@ -225,7 +225,7 @@ int SetNewSmokeShaders(void){
     "  if(have_fire==1){"
     "    fire_val = texture3D(fire_texture,position).x;"
     "    if(fire_val>global_hrrpuv_cutoff){"
-    "      alpha_factor = (fire_val-global_hrrpuv_cutoff)/(hrrpuv_max_smv-global_hrrpuv_cutoff);"
+    "      alpha_factor = clamp((fire_val-global_hrrpuv_cutoff)/50.0,0.0,1.0);"
     "      color_index = fire_val/hrrpuv_max_smv;"
     "      color = texture1D(colormap,color_index).rgb;"
     "      alpha = fire_alpha*alpha_factor;"

commit f3d19487a120814cff8af599fbfd41462d903d2e

smokeview source: fix error in smoke loading introduced in 672861f2 9/5/2018

Source/smokeview/IOsmoke.c
@@ -5941,10 +5941,12 @@ FILE_SIZE ReadSmoke3D(int iframe,int ifile,int flag, int *errorcode){
       PRINTF(" - skipped (max soot opacity<%0.f)\n", load_3dsmoke_cutoff);
       return_flag = 1;
     }
-    if(smoke3di->finalize==1){
-      SmokeWrapup(smoke3di);
+    if(return_flag==1){
+      if(smoke3di->finalize==1){
+        SmokeWrapup(smoke3di);
+      }
+      return 0;
     }
-    if(return_flag==1)return 0;
   }
   CheckMemory;
   if(