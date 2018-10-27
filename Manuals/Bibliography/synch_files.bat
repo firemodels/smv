6d2dcd2dd0383f6c734152ae41d535a1d86c814a
gforney <gforney@gmail.com>
2018-10-27 10:01:04 -0400

    scripts: add scripts for syching common files in fds and smv repos

--- Manuals/Bibliography/synch_files.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set fdsdir=..\..\..\fds\Manuals\Bibliography
-copy %fdsdir%\FDS_general.bib .
-copy %fdsdir%\FDS_mathcomp.bib .
-copy %fdsdir%\FDS_refs.bib .
-copy %fdsdir%\authors.tex .
-set fdsdir=..\..\..\fds\Source
-copy %fdsdir%\gsmv.f90 ..\..\Source\smokeview\gsmv.f90
