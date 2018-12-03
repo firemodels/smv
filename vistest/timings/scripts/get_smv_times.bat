df96a3ff1b2406276fe86e705aa450ebb87dce10
gforney <gforney@gmail.com>
2018-12-03 13:11:32 -0500

    smv vv: add windows batch file for outputting smokeview benchmark times

--- vistest/timings/scripts/get_smv_times.bat
+++ vistest/timings/scripts/get_smv_times.bat
@@ -17,6 +17,9 @@ call :get_time %SMOKEVIEW1% plume_timing_3dsmoke.ssf plume_timing  time3
 call :get_time %SMOKEVIEW1% plume_timing_slice.ssf   plume_timing  time4
 call :get_time %SMOKEVIEW1% plume_timing_vslice.ssf  plume_timing  time5
 echo %time1%,%time2%,%time3%,%time4%,%time5%
+
+::------------
+
 call :get_time %SMOKEVIEW2% plume_timing_startup.ssf plume_timing  time1
 call :get_time %SMOKEVIEW2% plume_timing_iso.ssf     plume_timing  time2
 call :get_time %SMOKEVIEW2% plume_timing_3dsmoke.ssf plume_timing  time3
@@ -39,10 +42,9 @@ flush_mem 1> Nul 2>&1
 ptime %smv% -scriptfile %script% %case%  1> tmp.out 2>&1
 tail -1 tmp.out | gawk "{print $3}" > tmp2.out
 set /p %timevar%=<tmp2.out
+rm tmp.out tmp2.out
 
 exit /b 0
-
-
 
 :eof
 cd %CURDIR%
