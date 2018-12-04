40c8d3f54b5dabe1b647cd7db967e08f585b2f41
gforney <gforney@gmail.com>
2018-12-04 12:47:19 -0500

    smv vv: update windows timing batch file

--- vistest/timings/scripts/get_smv_times.bat
+++ vistest/timings/scripts/get_smv_times.bat
@@ -9,8 +9,15 @@ set CASE=plume_timing
 cd ..\..\..\
 set smvrepo=%CD%
 
-set TIME=%smvrepo%\Build\cputime\intel_win_64\cputime_64.exe
-set FLUSH=%smvrepo%\Build\flush\intel_win_64\flush_win_64.exe
+set TIME=%smvrepo%\Build\timep\intel_win_64\timep_win_64.exe
+set FLUSH=%smvrepo%\Build\flush\intel_win_64\flush_win_64.exe -g 16 -w
+
+:: make sure programs used by this script exist
+
+call :does_file_exist %TIME%  || exit /b 1
+call :does_file_exist %FLUSH% || exit /b 1
+call :is_file_installed gawk|| exit /b 1
+call :is_file_installed tail|| exit /b 1
 
 cd %CASEDIR%
 call :run_cases %SMOKEVIEW1% %CASE%
@@ -43,13 +50,47 @@ set script=%2
 set case=%3
 set timevar=%4
 
+set file1=tmp.out
+set file2=tmp2.out
+
 %FLUSH% 1> Nul 2>&1
-%TIME% %smv% -scriptfile %script% %case%  1> tmp.out 2>&1
-tail -1 tmp.out | gawk "{print $1}" > tmp2.out
-set /p %timevar%=<tmp2.out
-erase tmp.out
+%TIME% %smv% -scriptfile %script% %case%  1> %file1% 2>&1
+tail -1 tmp.out | gawk "{print $1}" > %file2%
+set /p %timevar%=<%file2%
+erase %file1% %file2%
 
 exit /b 0
+
+:: -------------------------------------------------------------
+  :does_file_exist
+:: -------------------------------------------------------------
+
+set file=%1
+
+if NOT exist %file% (
+  echo ***fatal error: %file% does not exist
+  exit /b 1
+)
+exit /b 0
+
+:: -------------------------------------------------------------
+:is_file_installed
+:: -------------------------------------------------------------
+
+  set program=%1
+  set file=file_exist.txt
+  set filecount=file_exist_count.txt
+  %program% --help 1>> %file% 2>&1
+  type %file% | find /i /c "not recognized" > %filecount%
+  set /p nothave=<%filecount%
+  erase %file% %filecount%
+  if %nothave% == 1 (
+    echo "***Fatal error: %program% not present"
+    exit /b 1
+  )
+  exit /b 0
+
+
 
 :eof
 cd %CURDIR%
