586a436464ebd5be0f6d218abade10e07fe6f10c
gforney <gforney@gmail.com>
2018-12-24 23:46:02 -0500

    glui/glut build: update makefiles, build scripts for windows gnu compilers

--- Source/glui_v2_1_beta/makelib.bat
+++ Source/glui_v2_1_beta/makelib.bat
@@ -3,8 +3,10 @@ setlocal
 call ..\scripts\setopts %*
 title Building glui library
 erase *.o *.obj libglui.a libglui.lib
-set target=libglui.lib
+
+set target=intel_win_64
 if %COMPILER% == gcc set target=gnu_win_64
-make COMPILER=%COMPILER% COMPILER2=%COMPILER2% SIZE=%SIZE% RM=erase -f ./makefile %target%
+
+make COMPILER=%COMPILER% COMPILER2=%COMPILER2% SIZE=%SIZE% RM=erase %target% -f ./makefile 
 if %COPYLIB% == 1 copy %FROMLIB% %TOLIB%
 endlocal
