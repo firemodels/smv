a9e763d85dfa9c9bcf2b395739bec5ff4f8f2584
gforney <gforney@gmail.com>
2019-01-10 10:29:25 -0500

    smv build: unifiy makefiles for smokeview and libs for intel_linux_64 target

--- Build/smokeview/intel_linux_64/make_smokeview.sh
+++ Build/smokeview/intel_linux_64/make_smokeview.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 source ../../scripts/setopts.sh $*
 
+# Exit immediately if any of the build steps fail
+set -e
+
+LIBDIR=../../LIBS/intel_linux_64/
+eval make -C ${LIBDIR} -j 4 ${SMV_MAKE_OPTS} ${LUA_SCRIPTING} -f make_LIBS.make all
+
 make -f ../Makefile clean
 eval make -j 4 ${SMV_MAKE_OPTS} -f ../Makefile intel_linux_64
