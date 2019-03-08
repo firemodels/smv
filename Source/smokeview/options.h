5d33c3604db5ee7575c09a5951a1118debee8f82
gforney <gforney@gmail.com>
2019-03-08 10:35:20 -0500

    smokeview source: mask html option in release version

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -17,7 +17,6 @@
 
 //*** options: all platforms
 
-#define pp_HTML         // output html version of smokeview scene
 //#define pp_PARTDEBUG    // debug output for particles file I/O
 //#define pp_WINDROSE_AVG // average windrose data
 //#define pp_SPECULAR     // add widgets for specular lighting parameters to the lighting dialog box
@@ -85,6 +84,7 @@
 //*** options: options being test on all platforms
 
 #ifdef pp_BETA   
+#define pp_HTML                // output html version of smokeview scene
 #define pp_DEBUG_SUBMENU       // debug output and testing for building menus
 //#define pp_SMOKE3D_LOADTEST  // load 3d smoke for all meshes for each time step 
 //#define pp_SMOKEALPHA        // experimental smoke alpha settings
