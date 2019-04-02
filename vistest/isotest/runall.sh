cf274347d200e21bf7c9bea3c188f82e6e374a61
gforney <gforney@gmail.com>
2019-04-02 14:46:42 -0400

    smv vv: update isosurface cases

--- vistest/isotest/runall.sh
+++ vistest/isotest/runall.sh
@@ -2,7 +2,7 @@
 
 QUEUE="-q batch4"
 
-qfds.sh -p 16 $QUEUE plume_1p5cm.fds
-qfds.sh -p 16 $QUEUE plume_3cm.fds
-qfds.sh -p 16 $QUEUE plume_6cm.fds
-
+qfds.sh -I -p 16 $QUEUE plume_1p5cm.fds
+qfds.sh -I -p 16 $QUEUE plume_3cm.fds
+qfds.sh -I -p 16 $QUEUE plume_6cm.fds
+qfds.sh -I $QUEUE plume_6cmb.fds
