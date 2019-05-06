82646b000aec16d132baac67765c5723ea3beeb3
gforney <gforney@gmail.com>
2019-05-05 22:24:26 -0400

    smokeview source: correction to rgb fire color specification (array index error)

--- Source/smokeview/getdatacolors.c
+++ Source/smokeview/getdatacolors.c
@@ -1402,8 +1402,8 @@ void UpdateSmokeColormap(int option){
         }
         else{
           rgb_colormap[4*n+0]=(float)fire_color_int255[0] /255.0;
-          rgb_colormap[4*n+1]=(float)fire_color_int255[0] /255.0;
-          rgb_colormap[4*n+2]=(float)fire_color_int255[0] /255.0;
+          rgb_colormap[4*n+1]=(float)fire_color_int255[1] /255.0;
+          rgb_colormap[4*n+2]=(float)fire_color_int255[2] /255.0;
         }
         if(alpha[n]==0){
           rgb_colormap[4*n+3]=0.0;
