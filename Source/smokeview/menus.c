b35302f8849b6e9e358b80d2c7fbaf0b706609b3
gforney <gforney@gmail.com>
2019-05-14 11:08:42 -0400

    smokeview source: correct ALT R listing in helkp menu

--- Source/smokeview/menus.c
+++ Source/smokeview/menus.c
@@ -9159,7 +9159,7 @@ updatemenu=0;
     glutAddMenuEntry(_("  i: toggle iso-surface visibility"), MENU_DUMMY);
   }
   glutAddMenuEntry(_("Misc"), MENU_DUMMY);
-  glutAddMenuEntry(_("  r/R/ALT R: render the current scene to an image file"), MENU_DUMMY);
+  glutAddMenuEntry(_("  r/R: render the current scene to an image file"), MENU_DUMMY);
   glutAddMenuEntry("             r: image has the same resolution as the scene", MENU_DUMMY);
   {
     char render_label[1024];
