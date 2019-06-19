b69598b3fd67bc2f51f75e4ae51a134ae07f4207
gforney <gforney@gmail.com>
2019-06-18 23:01:41 -0400

    smokeview source: add a vr init routine

--- Source/smokeview/vr.cpp
+++ Source/smokeview/vr.cpp
@@ -1,4 +1,19 @@
+#include <stdio.h>
 #include "options.h"
 #ifdef pp_OPENVR
-#include "openvr.h"
+//#include <SDL.h>
+//#include <SDL_opengl.h>
+#include <openvr.h>
+
+int InitVR(void){
+  vr::IVRSystem *m_pHMD;
+
+ // if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER)<0){
+ //   printf("%s - SDL could not initialize! SDL Error: %s\n", __FUNCTION__, SDL_GetError());
+ //   return false;
+ // }
+  vr::EVRInitError eError = vr::VRInitError_None;
+  m_pHMD = vr::VR_Init(&eError, vr::VRApplication_Scene);
+  return true;
+}
 #endif
