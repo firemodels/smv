f4bed96b1745d47cd068a0192d42e18607223fa5
gorney <gforney@gmail.com>
2019-07-23 15:55:24 -0400

    smokeview source: add routine to open a subset of a file

--- Source/shared/stdio_m.h
+++ Source/shared/stdio_m.h
@@ -14,6 +14,7 @@ void   fclose_m(FILE_m *stream_m);
 char   *fgets_m(char * str, int num, FILE_m *stream_m);
 int    feof_m(FILE_m *stream_m);
 FILE_m *fopen_m(char *file, char *mode);
+FILE_m *fopen_mo(char *file, FILE_SIZE offset, FILE_SIZE size, char *mode);
 size_t fread_m(void *ptr, size_t size, size_t nmemb, FILE_m *stream_m);
 size_t fread_mv(void **ptr, size_t size, size_t nmemb, FILE_m *stream_m);
 int    fseek_m(FILE_m *stream_m, long int offset, int whence);
