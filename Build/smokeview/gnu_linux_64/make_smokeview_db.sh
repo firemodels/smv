7e04fba0d75b2fc09b535527be03067cb098a965
gforney <gforney@gmail.com>
2019-07-24 09:55:44 -0400

    smv build: add an increment build option

--- Build/smokeview/gnu_linux_64/make_smokeview_db.sh
+++ Build/smokeview/gnu_linux_64/make_smokeview_db.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-arg=$1
 source ../../scripts/setopts.sh $*
 
 LIBDIR=../../LIBS/gnu_linux_64/
 source ../../scripts/test_libs.sh
 
-if [ "$arg" != "-i" ]; then
+if [ "$inc" == "" ]; then
   make -f ../Makefile clean
 fi
 eval make ${SMV_MAKE_OPTS} -f ../Makefile gnu_linux_64_db
