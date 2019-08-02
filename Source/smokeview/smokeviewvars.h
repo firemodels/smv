fa4b860eaaae8992b6afbb4005ebf4c4f8818c3d
gforney <gforney@gmail.com>
2019-08-02 17:40:33 -0600

    smv bundle: display both zone temperature layers by default

--- Source/smokeview/smokeviewvars.h
+++ Source/smokeview/smokeviewvars.h
@@ -805,7 +805,7 @@ SVEXTERN float zone_maxventflow;
 SVEXTERN unsigned char SVDECL(*hazardcolor,NULL);
 SVEXTERN float SVDECL(zone_ventfactor,1.0);
 SVEXTERN unsigned char SVDECL(*izonetu,NULL);
-SVEXTERN int SVDECL(show_zonelower, 0);
+SVEXTERN int SVDECL(show_zonelower, 1);
 SVEXTERN unsigned char SVDECL(*izonetl, NULL);
 SVEXTERN int nzone_times;
 SVEXTERN float barright;
