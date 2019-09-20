e0b2d4227642d05c9b3dad0e63de825e50c4f70a
gforney <gforney@gmail.com>
2019-09-20 15:15:54 -0400

    smv build: update osx debug entries for mpi builds

--- Build/smokeview/intel_osx_64/make_smokeview_db.sh
+++ Build/smokeview/intel_osx_64/make_smokeview_db.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+source ../../scripts/setopts.sh $*
+
+OPEN_MPI=
+if [ "$SMV_MPI" == "1" ]; then
+  OPEN_MPI='OPEN_MPI="true"'
+fi
+
+
+LIBDIR=../../LIBS/intel_osx_64/
+eval make -C ${LIBDIR} ${SMV_MAKE_OPTS} ${LUA_SCRIPTING} -f make_LIBS.make all_nolua_noglut
+
 make -f ../Makefile clean
-make -j 4 -f ../Makefile intel_osx_64_db
+eval make -j 4 $OPEN_MPI OPEN_MPI_ROOT="$MPIHOME" ${SMV_MAKE_OPTS} -f ../Makefile intel_osx_64_db
