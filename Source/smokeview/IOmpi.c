5e85381e73453d53c9fe2b6ad586a42ed28c6513
gforney <gforney@gmail.com>
2019-09-22 21:33:21 -0400

    smokeview source: move mpi routines to IOmpi.c and IOmpi.h files

--- Source/smokeview/IOmpi.c
+++ Source/smokeview/IOmpi.c
@@ -1,11 +1,32 @@
-#ifndef IOMPI_H_DEFINED
-#define IOMPI_H_DEFINED
+#ifdef pp_MPI
+#define IN_IOMPIC
+#include <stdio.h>
+#include "IOmpi.h"
 
-#ifdef IN_IOMPI_C
-#define  MPIEXTERN
-#define MPIDECL(var,val)  var=val
-#else
-#define MPIEXTERN extern CCC
-#define MPIDECL(var,val)  var
-#endif
+/* ------------------ MPITest ------------------------ */
+
+void MPITest(void){
+  MPI_Init(NULL, NULL);
+
+  // Get the number of processes
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  // Get the rank of the process
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  // Get the name of the processor
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
+
+  // Print off a hello world message
+  printf("Hello world from processor %s, rank %d out of %d processors\n",
+    processor_name, world_rank, world_size);
+
+  // Finalize the MPI environment.
+  MPI_Finalize();
+
+}
 #endif
--- Source/smokeview/IOmpi.h
+++ Source/smokeview/IOmpi.h
@@ -1,31 +1,15 @@
-#ifdef pp_MPI
-#define IN_IO_MPI
-#include IOmpi.h
+#ifndef IOMPI_H_DEFINED
+#define IOMPI_H_DEFINED
+#include <mpi.h>
 
-/* ------------------ MPITest ------------------------ */
+#ifdef IN_IOMPIC
+#define  MPIEXTERN
+#define MPIDECL(var,val)  var=val
+#else
+#define MPIEXTERN extern CCC
+#define MPIDECL(var,val)  var
+#endif
 
-void MPITest(void){
-  MPI_Init(NULL, NULL);
+MPIEXTERN void MPITest(void);
 
-  // Get the number of processes
-  int world_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-  // Get the rank of the process
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-  // Get the name of the processor
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int name_len;
-  MPI_Get_processor_name(processor_name, &name_len);
-
-  // Print off a hello world message
-  printf("Hello world from processor %s, rank %d out of %d processors\n",
-    processor_name, world_rank, world_size);
-
-  // Finalize the MPI environment.
-  MPI_Finalize();
-
-}
 #endif
