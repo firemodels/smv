2588e100d07dc7c44574b52ca28f649098685de8
gforney <gforney@gmail.com>
2019-10-08 09:57:51 -0400

    smokeview source: revert data dialog menu entry

--- Source/smokeview/menus.c
+++ Source/smokeview/menus.c
@@ -9020,7 +9020,7 @@ updatemenu=0;
   CREATEMENU(dialogmenu,DialogMenu);
 
   glutAddMenuEntry(_("Clip scene...  ALT c"), DIALOG_CLIP);
-  glutAddMenuEntry(_("Data... ALT b"), DIALOG_BOUNDS);
+  glutAddMenuEntry(_("Data bounds... ALT b"), DIALOG_BOUNDS);
 #ifdef pp_GLUTGET
   glutAddMenuEntry(_("Display...  ALT D"), DIALOG_DISPLAY);
 #else
