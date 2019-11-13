3c9d85c2201ebe32cdecf15ca71f15c73bc7c183
gforney <gforney@gmail.com>
2019-11-12 21:59:06 -0500

    set_path build: add option to skip final pause statemenet

--- Build/set_path/intel_win_64/make_set_path.bat
+++ Build/set_path/intel_win_64/make_set_path.bat
@@ -1,4 +1,5 @@
 @echo off
+set from=%1
 
 :: setup compiler environment
 call ..\..\..\Utilities\Scripts\setup_intel_compilers.bat
@@ -9,4 +10,6 @@ make SHELL="%ComSpec%" -f ../Makefile intel_win_64
 if "x%EXIT_SCRIPT%" == "x" goto skip1
 exit
 :skip1
+if x%from% == xbot goto skip2
 pause
+:skip2
