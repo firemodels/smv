f7cc8e11214cc155ebfab4f02176c63593f874a1
gforney <gforney@gmail.com>
2019-11-27 22:43:00 -0500

    smv vv: add levelset2 movie

--- Verification/scripts/Make_SMV_Movies.sh
+++ Verification/scripts/Make_SMV_Movies.sh
@@ -72,6 +72,7 @@ $QSMV -P 8 -c thouse5_movies.ssf        -d Visualization thouse5
 $QSMV -P 8 -c BT10m_2x2km_LS_movies.ssf -d WUI           BT10m_2x2km_LS
 $QSMV -P 8 -c hill_structure_movies.ssf -d WUI           hill_structure
 $QSMV -P 8 -c levelset1_movies.ssf      -d WUI           levelset1
+$QSMV -P 8 -c levelset2_movies.ssf      -d WUI           levelset2
 $QSMV -P 8 -c wind_test1_movies.ssf     -d WUI           wind_test1
 $QSMV -P 8 -c tree_test2_movies.ssf     -d WUI           tree_test2
 wait_cases_end
@@ -92,6 +93,7 @@ cd $WUIINDIR
 $MAKEMOVIE -o $OUTDIR BT10m_2x2km_LS_movie
 $MAKEMOVIE -o $OUTDIR hill_structure_movie
 $MAKEMOVIE -o $OUTDIR levelset1_movie
+$MAKEMOVIE -o $OUTDIR levelset2_movie
 $MAKEMOVIE -o $OUTDIR wind_test1_movie
 $MAKEMOVIE -o $OUTDIR tree_test2_movie
 
