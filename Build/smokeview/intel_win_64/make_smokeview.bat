eb72ae7cb7fcc724c95c3eb8b13ed50a76ef9310
gforney <gforney@gmail.com>
2020-01-07 13:40:36 -0500

    smv build: change intel compiler setup back to using script in smv repo

--- Build/smokeview/intel_win_64/make_smokeview.bat
+++ Build/smokeview/intel_win_64/make_smokeview.bat
@@ -4,7 +4,7 @@ call ..\scripts\set_smv_opts %*
 
 :: setup compiler environment
 if x%from% == xbot goto skip1
-call ..\..\..\..\fds\Build\scripts\setup_intel_compilers.bat
+call ..\..\..\Utilities\Scripts\setup_intel_compilers.bat
 :skip1
 
 set SMV_TESTFLAG=
