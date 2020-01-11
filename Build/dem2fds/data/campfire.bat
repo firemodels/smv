1c7aef0b819e1992dd4ca1fb20cea56e756c426e
gforney <gforney@gmail.com>
2020-01-11 15:18:13 -0500

    smv vv: minor edits to campfire scripts

--- Build/dem2fds/data/campfire.bat
+++ Build/dem2fds/data/campfire.bat
@@ -1,11 +1,19 @@
 @echo off
 :: generate terrain with &GEOM
-set option=-geom
+::set option=-geom
 
 set dem2fds=..\intel_win_64\dem2fds_win_64.exe
 set dem2fds=dem2fds
-::set "terrain=%userprofile%\terrain"
-set "terrain=%userprofile%\Google drive\documents\terrain"
+set "terrain1=%userprofile%\terrain"
+set "terrain2=%userprofile%\Google drive\documents\terrain"
+
+set terrain=
+if EXIST "%terrain1%" set "terrain=%terrain1%"
+if EXIST "%terrain2%" set "terrain=%terrain2%"
+if NOT "x%terrain%" == "x" goto endif1
+  echo ***error: the terrain file directory does not exist
+  exit /b
+:endif1
 
 %dem2fds% %option%       -dir "%terrain%\campfire" campfire1.in 
 %dem2fds% %option% -show -dir "%terrain%\campfire" campfire2.in 
