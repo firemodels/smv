1cda9dd8d9b9c437154523f3ea796192566342e5
gforney <gforney@gmail.com>
2020-01-11 14:31:24 -0500

    smv vv: change some file names

--- Build/dem2fds/data/campfire.bat
+++ Build/dem2fds/data/campfire.bat
@@ -4,7 +4,8 @@ set option=-geom
 
 set dem2fds=..\intel_win_64\dem2fds_win_64.exe
 set dem2fds=dem2fds
-set "terrain=%userprofile%\terrain"
+::set "terrain=%userprofile%\terrain"
+set "terrain=%userprofile%\Google drive\documents\terrain"
 
-%dem2fds% %option%       -dir "%terrain%\campfire" campfire.in 
+%dem2fds% %option%       -dir "%terrain%\campfire" campfire1.in 
 %dem2fds% %option% -show -dir "%terrain%\campfire" campfire2.in 
