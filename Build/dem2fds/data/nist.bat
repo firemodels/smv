f5f95c3e382e32db1315c3b5001e0d828e9af095
gforney <gforney@gmail.com>
2020-04-24 09:13:29 -0400

    dem2fds vv: update nist input files to use LATMINORIG keyword so that the fds input file has grid cells with whole units

--- Build/dem2fds/data/nist.bat
+++ Build/dem2fds/data/nist.bat
@@ -1,6 +1,6 @@
 @echo off
 :: generate terrain with &GEOM
-:: set option=-geom
+set "option=-geom -bingeom"
 
 set dem2fds=..\intel_win_64\dem2fds_win_64.exe
 ::set dem2fds=dem2fds
@@ -11,4 +11,4 @@ set HOME=%userprofile%\terrain
 set "DIR=%GOOGLE%"
 if exist %HOME% set "DIR=%HOME%"
 
-%dem2fds% %option% -show -elevdir "%DIR%\N40W078" -dir "%DIR%\nist" nist.in 
+%dem2fds% %option% -elevdir "%DIR%\N40W078" -dir "%DIR%\nist" nist.in 
