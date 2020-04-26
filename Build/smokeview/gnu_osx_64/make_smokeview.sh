b5870cf3c8aa9a2c9f2d1cdd973fbde9c730865d
gforney <gforney@gmail.com>
2020-04-26 13:52:34 -0400

    smv build: changes to get smokeview to build with osx/intel and osx/gnu compilers using the quartz x11 library

--- Build/smokeview/gnu_osx_64/make_smokeview.sh
+++ Build/smokeview/gnu_osx_64/make_smokeview.sh
@@ -2,7 +2,23 @@
 source ../../scripts/setopts.sh $*
 
 LIBDIR=../../LIBS/gnu_osx_64
-eval make -C ${LIBDIR} ${SMV_MAKE_OPTS} ${LUA_SCRIPTING} -f make_LIBS.make all_nolua_noglut
+curdir=`pwd`
+cd $LIBDIR
+build_libs=
+LIBS="libgd.a libglui.a libglut.a libjpeg.a libpng.a libz.a"
+for f in $LIBS
+do
+  if [ ! -e $f ]; then
+    echo $f does not exist
+    build_libs=1
+  fi
+done
+if [ "$build_libs" == "1" ]; then
+  cd $LIBDIR
+  echo building libs
+  eval ./make_LIBS.sh $*
+  cd $curdir
+fi
 
-make -f ../Makefile clean
+rm -f *.o *.mod smokeview_osx_64*
 eval make QUARTZ="$QUARTZ" GLUT="$GLUT" ${SMV_MAKE_OPTS} -f ../Makefile gnu_osx_64
--- Build/smokeview/intel_osx_64/make_smokeview.sh
+++ Build/smokeview/intel_osx_64/make_smokeview.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 source ../../scripts/setopts.sh $*
 
-LIBDIR=../../LIBS/intel_osx_64/
-eval make -C ${LIBDIR} ${SMV_MAKE_OPTS} ${LUA_SCRIPTING} -f make_LIBS.make all_nolua_noglut
+LIBDIR=../../LIBS/intel_osx_64
+curdir=`pwd`
+cd $LIBDIR
+build_libs=
+LIBS="libgd.a libglui.a libglut.a libjpeg.a libpng.a libz.a"
+for f in $LIBS
+do 
+  if [ ! -e $f ]; then
+    echo $f does not exist
+    build_libs=1
+  fi
+done
+if [ "$build_libs" == "1" ]; then
+  cd $LIBDIR
+  echo building libs
+  eval ./make_LIBS.sh $*
+  cd $curdir
+fi
 
-make -f ../Makefile clean
+rm -f *.o *.mod smokeview_osx_64*
 eval make -j 4 QUARTZ="$QUARTZ" GLUT="$GLUT" ${SMV_MAKE_OPTS} -f ../Makefile intel_osx_64
