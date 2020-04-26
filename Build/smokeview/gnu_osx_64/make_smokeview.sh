fa4ad440df1f235d82d9b9898fda9439375c142e
gforney <gforney@gmail.com>
2020-04-26 19:09:52 -0400

    smv build: make quartz lib the default for osx builds

--- Build/smokeview/gnu_osx_64/make_smokeview.sh
+++ Build/smokeview/gnu_osx_64/make_smokeview.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-source ../../scripts/setopts.sh $*
+OPTS="-q $*"
+source ../../scripts/setopts.sh $OPTS
 
 LIBDIR=../../LIBS/gnu_osx_64
-curdir=`pwd`
+
+CURDIR=`pwd`
 cd $LIBDIR
 build_libs=
 LIBS="libgd.a libglui.a libglut.a libjpeg.a libpng.a libz.a"
 for f in $LIBS
 do
   if [ ! -e $f ]; then
-    echo $f does not exist
     build_libs=1
   fi
 done
 if [ "$build_libs" == "1" ]; then
   cd $LIBDIR
-  echo building libs
-  eval ./make_LIBS.sh $*
-  cd $curdir
+  echo building $LIBS
+  eval ./make_LIBS.sh $OPTS
 fi
+cd $CURDIR
 
 rm -f *.o *.mod smokeview_osx_64*
 eval make QUARTZ="$QUARTZ" GLUT="$GLUT" ${SMV_MAKE_OPTS} -f ../Makefile gnu_osx_64
--- Build/smokeview/intel_osx_64/make_smokeview.sh
+++ Build/smokeview/intel_osx_64/make_smokeview.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-source ../../scripts/setopts.sh $*
+OPTS="-q $*"
+source ../../scripts/setopts.sh $OPTS
 
 LIBDIR=../../LIBS/intel_osx_64
-curdir=`pwd`
+
+CURDIR=`pwd`
 cd $LIBDIR
 build_libs=
 LIBS="libgd.a libglui.a libglut.a libjpeg.a libpng.a libz.a"
@@ -16,9 +18,9 @@ done
 if [ "$build_libs" == "1" ]; then
   cd $LIBDIR
   echo building libs
-  eval ./make_LIBS.sh $*
-  cd $curdir
+  eval ./make_LIBS.sh $OPTS
 fi
+cd $CURDIR
 
 rm -f *.o *.mod smokeview_osx_64*
 eval make -j 4 QUARTZ="$QUARTZ" GLUT="$GLUT" ${SMV_MAKE_OPTS} -f ../Makefile intel_osx_64
