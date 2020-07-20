175e0292539d01ecb6592140aadb0e751375b429
gforney <gforney@gmail.com>
2020-07-20 16:24:29 -0400

    smokeview source: add missing declaration

--- Source/smokeview/smokeheaders.h
+++ Source/smokeview/smokeheaders.h
@@ -34,6 +34,7 @@ boundsdata *GetPatchBoundsInfo(char *shortlabel);
 EXTERNCPP void GetSlicePercentileBounds(char *slicetype, float global_min, float global_max, float *per_min, float *per_max);
 EXTERNCPP void GetGlobalSliceBounds(void);
 EXTERNCPP void GetGlobalPatchBounds(void);
+EXTERNCPP void GetGlobalPartBounds(void);
 EXTERNCPP void GetLoadedPatchBounds(char *label, float *loaded_min, float *loaded_max);
 FILE_SIZE ReadSliceUseGluiBounds(char *file, int ifile, int time_frame, float *time_value, int flag, int set_slicecolor, int *errorcode);
 #endif
