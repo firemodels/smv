5c7a432d7f2490a706ce6cd313556b1ef04cd352
gforney <gforney@gmail.com>
2020-08-11 11:36:34 -0400

    repo scripts: modify set tag script to create release branch

--- scripts/set_releasetag.sh
+++ scripts/set_releasetag.sh
@@ -2,17 +2,13 @@
 
 directory=$1
 tag=$2
-name=$3
 
 cd ~/$directory
 
 git checkout master
 if [ "$tag" != "latest" ]; then
   if [ "$tag" != "" ]; then
-    if [ "$name" != "" ]; then
-      name="-b $name"
-    fi
-    echo git checkout $tag $name
-    git checkout $tag $name
+    git branch -d release >& /dev/null
+    git checkout $tag -b release
   fi
 fi
