a4e4191d094bedf70e4fc0aca569957d478dd0d1
gforney <gforney@gmail.com>
2020-10-21 16:08:11 -0400

    smokeview source: ignore .ter files when terrain is built by &GEOM's

--- Source/smokeview/readsmv.c
+++ Source/smokeview/readsmv.c
@@ -10193,7 +10193,9 @@ typedef struct {
   InitVolRenderSurface(FIRSTCALL);
   radius_windrose = 0.2*xyzmaxdiff;
 
+#ifndef pp_IGNORE_TER
   UpdateMeshTerrain(); // xxslow
+#endif
 
   ReadAllGeom();
   UpdateTriangles(GEOM_STATIC,GEOM_UPDATE_ALL);
