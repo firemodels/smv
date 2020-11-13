bca24ddb9efa57e86528c9c3cc8fbbb97e57c0ef
gforney <gforney@gmail.com>
2020-11-13 00:20:01 -0500

    lib build: minor formatting edites to build scripts

--- Build/LIBS/intel_linux_64/make_LIBS.sh
+++ Build/LIBS/intel_linux_64/make_LIBS.sh
@@ -23,16 +23,12 @@ BUILDDIR=`pwd`
 
 if [[ "$target" == "all" ]] || [[ "$target" == "glut" ]]; then
 if [ "$GLUT" == "freeglut" ]; then
-  echo
   echo "********** building freeglut"
-  echo
-  cd $BUILDDIR/freeglut3.0.0/gnu_linux_64
+  cd $BUILDDIR/freeglut3.0.0/gnu_linux_64 >& freeglut.out
   ./make_freeglut.sh $OPTS
 else
-  echo
   echo "********** building glut"
-  echo
-  cd $SRCDIR/glut-3.7.6
+  cd $SRCDIR/glut-3.7.6 >& glut.out
   ./makelib.sh $OPTS
 fi
 cp libglut.a $LIBDIR/.
@@ -40,51 +36,41 @@ fi
 
 if [[ "$target" == "all" ]] || [[ "$target" == "glui" ]]; then
 # GLUI
-echo
 echo "********** building glui"
-echo
 cd $SRCDIR/glui_v2_1_beta
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& glui.out
 cp libglui.a $LIBDIR/.
 fi
 
 if [[ "$target" == "all" ]] || [[ "$target" == "zlib" ]]; then
 # ZLIB
-echo
 echo "********** building zlib"
-echo
 cd $SRCDIR/zlib128
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& zlib.out
 cp libz.a $LIBDIR/.
 fi
 
 if [[ "$target" == "all" ]] || [[ "$target" == "jpeg" ]]; then
 # JPEG
-echo
 echo "********** building jpeg"
-echo
 cd $SRCDIR/jpeg-9b
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& jpeg.out
 cp libjpeg.a $LIBDIR/.
 fi
 
 if [[ "$target" == "all" ]] || [[ "$target" == "png" ]]; then
 # PNG
-echo
 echo "********** building png"
-echo
 cd $SRCDIR/png-1.6.21
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& png.out
 cp libpng.a $LIBDIR/.
 fi
 
 if [[ "$target" == "all" ]] || [[ "$target" == "gd" ]]; then
 # GD
-echo
 echo "********** building gd"
-echo
 cd $SRCDIR/gd-2.0.15
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& gd.out
 cp libgd.a $LIBDIR/.
 fi
 
@@ -95,13 +81,15 @@ if [ "$LUA" == "lua" ]; then
 # Lua # Lua interpreter
 cd $SRCDIR/lua-5.3.1/src
 export TARGET=liblua.a
-./makelib.sh $OPTS
+echo "********** building lua"
+./makelib.sh $OPTS >& lua.out
 cp liblua.a $LIBDIR/.
 
 # LPEG # Lua parsing libarary to parse SSF files
 cd $SRCDIR/lpeg-1.0.0
 export TARGET=linux
-./makelib.sh $OPTS
+echo "********** building lpeg"
+./makelib.sh $OPTS >& lpeg.out
 cp lpeg.so $LIBDIR/.
 fi
 fi
--- Build/LIBS/intel_osx_64/make_LIBS.sh
+++ Build/LIBS/intel_osx_64/make_LIBS.sh
@@ -20,90 +20,77 @@ BUILDDIR=`pwd`
 
 if [[ "$target" == "all" ]] || [[ "$target" == "gd" ]]; then
 # GD
-echo
 echo "********** building GD" 
-echo
 cd $SRCDIR/gd-2.0.15
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& gd.out
 cp libgd.a $LIBDIR/.
 fi
 
 if [[ "$target" == "all" ]] || [[ "$target" == "glui" ]]; then
 # GLUI
-echo
 echo "********** building glui"
-echo
 cd $SRCDIR/glui_v2_1_beta
-./makelib.sh $OPTS 
+./makelib.sh $OPTS  >& glui.out
 cp libglui.a $LIBDIR/.
 fi
 
 # GLUT
 if [[ "$target" == "all" ]] || [[ "$target" == "freeglut" ]] || [[ "$target" == "glut" ]]; then
 if [ "$GLUT" == "freeglut" ]; then
-echo
-echo "********** building glut"
-echo
+echo "********** building freeglut"
   cd $BUILDDIR/freeglut3.0.0/intel_osx_64
-  ./make_freeglut.sh $OPTS 
+  ./make_freeglut.sh $OPTS  >& freeglut.out
   cp libglut.a $LIBDIR/.
 else
   if [ "$QUARTZ" != "framework" ]; then
+echo "********** building glut"
     cd $SRCDIR/glut-3.7.6
-    ./makelib.sh $OPTS
+    ./makelib.sh $OPTS >& glut.out
     cp libglut.a $LIBDIR/.
+  else
+echo "********** using OSX provided glut"
   fi
 fi
 fi
 
 if [[ "$target" == "all" ]] || [[ "$target" == "jpeg" ]]; then
 # JPEG
-echo
 echo "********** building jpeg"
-echo
 cd $SRCDIR/jpeg-9b
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& jpeg.out
 cp libjpeg.a $LIBDIR/.
 fi
 
 if [[ "$target" == "all" ]] || [[ "$target" == "png" ]]; then
 # PNG
-echo
 echo "********** building png"
-echo
 cd $SRCDIR/png-1.6.21
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& png.out
 cp libpng.a $LIBDIR/.
 fi
 
 if [[ "$target" == "all" ]] || [[ "$target" == "zlib" ]]; then
 # ZLIB
-echo
 echo "********** building zlib"
-echo
 cd $SRCDIR/zlib128
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& zlib.out
 cp libz.a $LIBDIR/.
 fi
 
 if [[ "$target" == "all" ]] || [[ "$target" == "lua" ]]; then
 if [ "$LUA" == "lua" ]; then
 # Lua # Lua interpreter
-echo
 echo "********** building Lua"
-echo
 cd $SRCDIR/lua-5.3.1/src
 export TARGET=liblua.a
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& lua.out
 cp liblua.a $LIBDIR/.
 
 # LPEG # Lua parsing libarary to parse SSF files
-echo
 echo "********** building lpeg"
-echo
 cd $SRCDIR/lpeg-1.0.0
 export TARGET=macosx
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& lpeg.out
 cp lpeg.so $LIBDIR/.
 fi
 fi
