1a9e2658f35c743aa4679b6f4534a4dc21b0a5bd
gforney <gforney@gmail.com>
2021-04-10 22:57:53 -0400

    smokeview source: remove pp_DISABLE_ADJUSTY directive

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -50,7 +50,6 @@
 //#define pp_MERGE_GEOMS          // merge geometry and structure boundary file menu items
 //#define pp_SINGLE_FRAME_TEST   // test loading a single slice file frame (without using a script)
 //#define pp_SCRIPT_RENDER_FIX  // temporary fix to script rendering, needs to be explored further
-#define pp_DISABLE_ADJUSTY      // turn off y adjust when selecting a viewpoint
 //#define pp_SCRIPT_SETVIEW       // add SETVIEWPOINT command at the beginning of a script
 //#define pp_REMOVE_DUPLICATES     // remove duplicate geometry vertices
 #define pp_READALLGEOM_MT         // read geometry files in parallel
