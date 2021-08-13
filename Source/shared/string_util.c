1c6688163ed4e584fe31026d0be270252c3fedfc
gforney <gforney@gmail.com>
2021-08-13 09:26:43 -0400

    smokeview source: when converting floats to strings don't use exponential notation for ABS(number)<1000

--- Source/shared/string_util.c
+++ Source/shared/string_util.c
@@ -436,7 +436,7 @@ void Float2String(char *c_val, float val, int ndigits, int fixedpoint_labels){
 
   mantissa = GetMantissaExponent(ABS(val), &exponent);
   mantissa += 5.0*pow(10.0,-ndigits);
-  if(exponent>=0&&exponent<5||fixedpoint_labels==1){
+  if(exponent>=0&&exponent<5||fixedpoint_labels==1||ABS(val)<1000.0){
     char c_abs_val[32];
 
     val = SIGN(val)*mantissa*pow(10.0,exponent);
