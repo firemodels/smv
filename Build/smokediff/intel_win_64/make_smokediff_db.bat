3b7dc5095edfa1786901014bdcb8ea5676758127
gforney <gforney@gmail.com>
2021-08-20 14:55:09 -0400

    build scripts: simplify Intel compiler setup script and corresponding build scripts

--- Build/smokediff/intel_win_64/make_smokediff_db.bat
+++ Build/smokediff/intel_win_64/make_smokediff_db.bat
@@ -4,12 +4,7 @@ call ..\..\..\UtilitiesScripts\setup_intel_compilers.bat
 
 Title Building debug smokediff for 64 bit Windows
 
-set SMV_TESTFLAG=
-IF NOT DEFINED ONEAPI_ROOT goto skip_oneapi
-  set SMV_TESTFLAG=%SMV_TESTFLAG% -D pp_WIN_ONEAPI
-:skip_oneapi
-
 erase *.obj *.mod *.exe
-make SMV_TESTFLAG="%SMV_TESTFLAG%" -f ..\Makefile intel_win_64_db
+make -f ..\Makefile intel_win_64_db
 pause
 
