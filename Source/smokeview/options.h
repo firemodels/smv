51ca1be313e332933c912f461d77b3c559e1d161
gforney <gforney@gmail.com>
2021-09-19 22:55:15 -0400

    smokeview source: comment pp_GEOM_DIAG and pp_GEOM_ANGLE directives

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -54,8 +54,8 @@
 #define pp_HAVE_CFACE_NORMALS    //  read in and optionally draw cface normal vectors
 //#define pp_TERRAIN_OLD            // original terrain menus and dialog boxes
 //define pp_BNDF_MENU_BUG       // adds an extra menu field for geometry boundary files, a bug (highlight bug to make sure disabled code is not needed_
-#define pp_GEOM_DIAG          // surround geometry properties dialog with pp_GEOM_DIAG so it can be removed easily
-#define pp_GEOM_ANGLE         // surround geometry angle widgets so they can be removed easily
+//#define pp_GEOM_DIAG          // surround geometry properties dialog with pp_GEOM_DIAG so it can be removed easily
+//#define pp_GEOM_ANGLE         // surround geometry angle widgets so they can be removed easily
 
 #ifdef pp_GPU
 #define pp_GPUTHROTTLE  // pp_GPU directive must also be set
