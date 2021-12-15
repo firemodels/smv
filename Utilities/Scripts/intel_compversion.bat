010835ab61420752fe83da03a14046908280df58
gforney <gforney@gmail.com>
2021-12-15 15:46:23 -0500

    build scripts: update intel compiler version script

--- Utilities/Scripts/intel_compversion.bat
+++ Utilities/Scripts/intel_compversion.bat
@@ -4,10 +4,21 @@ call :is_file_installed sed || exit /b 1
 call :is_file_installed gawk|| exit /b 1
 call :is_file_installed icl || exit /b 1
 
-icl > c_version.txt 2>&1
+set ICL=icx
+set CTYPE=DPC++/C++
+if "x%INTEL_ICC%" == "x" goto skip_setintel
+  set ICL=%INTEL_ICC%
+:skip_setintel
+
+if NOT %ICL% == icl goto skipclassic
+set CTYPE=C/C++ Classic
+:skipclassic
+
+
+%ICL% > c_version.txt 2>&1
 head -1 c_version.txt | sed "s/^.*\(Version.*\).*$/\1/" | gawk "{print $2}" > vers.out
 set /p vers=<vers.out
-echo "Intel C/C++ %vers%"
+echo "Intel %CTYPE% %vers%"
 erase c_version.txt vers.out
 goto eof
 
