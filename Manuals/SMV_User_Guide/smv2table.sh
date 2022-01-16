0127cc8752f94eaf2a6f55053bd1c813b6529a7c
gforney <gforney@gmail.com>
2022-01-15 19:34:49 -0500

    smv ug: escape _ in label table generating script

--- Manuals/SMV_User_Guide/smv2table.sh
+++ Manuals/SMV_User_Guide/smv2table.sh
@@ -10,10 +10,10 @@ else
   KEY=SLCF
   ext=sf
 fi
-echo "\begin{tabular}{l|l|}"
+echo "\begin{tabular}{|l|l|}"
 echo "\hline"
 echo "$KEY Quantity & ini/ssf file Label \\\\ \hline"
-grep -A 3 $KEY $smvfile | grep -v $KEY | grep -v ${ext}$  | grep -v \- | \
+grep -A 3 $KEY $smvfile | grep -v $KEY | grep -v ${ext}$  | grep -v \- | sed s/_/\\\\_/g | \
 awk 'NR > 1 {print prev, "%", $0}; {prev = $0}' | \
 sed -n 1~2p | sort |\
 awk -F'%' '{print "{\\ct " $1 "} & {\\ct " $2 " } \\\\ \\hline"}'
