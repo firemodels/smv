e1103b55bfe401ef798c1908a386dd7af0e13e1f
gforney <gforney@gmail.com>
2022-01-31 21:38:57 -0500

    smokeview source: remove unused variables

--- Source/smokeview/readsmv.c
+++ Source/smokeview/readsmv.c
@@ -2577,7 +2577,6 @@ void UpdateSmokeTypes(void){
   nsmoketypes = 0;
   for(i = 0; i<nsmoke3dinfo; i++){
     smoke3ddata *smoke3di;
-    int j;
 
     smoke3di = smoke3dinfo+i;
     if(smoke3di->first_smoketype==1)nsmoketypes++;
