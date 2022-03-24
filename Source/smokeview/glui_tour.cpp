28f3d26cd93a642b2a8996b77bea3d573e860ee5
gforney <gforney@gmail.com>
2022-03-24 11:19:42 -0400

    smokeview source: add header file to source file weer it was included

--- Source/smokeview/glui_tour.cpp
+++ Source/smokeview/glui_tour.cpp
@@ -7,7 +7,46 @@
 #include <math.h>
 
 #include "smokeviewvars.h"
-#include "glui_tour.h"
+
+#define TOUR_CLOSE 99
+#define KEYFRAME_tXYZ 1
+#define KEYFRAME_INSERT 2
+#define KEYFRAME_DELETE 3
+#define KEYFRAME_PREVIOUS 4
+#define KEYFRAME_NEXT 5
+#define SAVE_SETTINGS_TOUR 7
+#define SHOWTOURROUTE 8
+#define VIEWTOURFROMPATH 9
+#define VIEWTOURFROMPATH1 41
+#define VIEWTOURFROMPATH2 42
+#define TOUR_SNAP 43
+#define SHOWTOURROUTE1 39
+#define SHOWTOURROUTE2 40
+#define TOUR_INSERT_NEW 32
+#define TOUR_INSERT_COPY 33
+#define TOUR_PREVIOUS 17
+#define TOUR_NEXT 18
+#define TOUR_DELETE 34
+#define TOUR_LABEL 19
+#define TOUR_HIDE 20
+#define KEYFRAME_viewXYZ 22
+#define VIEW_ALL_NODES 44
+#define VIEW_NEXT_NODE 45
+#define TOUR_LIST 24
+#define TOUR_AVATAR 31
+#define VIEW1 26
+#define VIEW_times 27
+#define TOUR_UPDATELABEL 28
+#define TOUR_USECURRENT 29
+#define TOUR_REVERSE 35
+#define TOUR_CIRCULAR_UPDATE 36
+#define KEYFRAME_UPDATE_ALL 38
+
+#define TOURMENU(f) callfrom_tourglui=1;TourMenu(f);callfrom_tourglui=0;
+
+#define SETTINGS_TOURS_ROLLOUT 0
+#define KEYFRAME_TOURS_ROLLOUT 1
+#define MODIFY_TOURS_ROLLOUT 2
 
 static int tour_hide=0;
 static char tour_label[sizeof(GLUI_String)];
