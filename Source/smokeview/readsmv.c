9724cae7b368e26b6a8d33df6e24975fa3307c9a
gforney <gforney@gmail.com>
2022-04-13 09:36:58 -0400

    smokeview source: fix typo in SHOWSLICEPLOT ini output

--- Source/smokeview/readsmv.c
+++ Source/smokeview/readsmv.c
@@ -11331,7 +11331,7 @@ int ReadIni2(char *inifile, int localfile){
     }
     if(Match(buffer, "SHOWSLICEPLOT")==1){
       fgets(buffer, 255, stream);
-      sscanf(buffer, " %f %f %f %f %i i", slice_xyz, slice_xyz+1, slice_xyz+2, &plot2d_size_factor, &vis_slice_plot, global_bounds_slice_plot);
+      sscanf(buffer, " %f %f %f %f %i %i", slice_xyz, slice_xyz+1, slice_xyz+2, &plot2d_size_factor, &vis_slice_plot, &global_bounds_slice_plot);
       continue;
     }
     if(Match(buffer, "SHOWHRRPLOT")==1){
