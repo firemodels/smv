0e7499f0b866f44cadcd243a540569a9032a417f
Jake <joshannessy@Affinity-fire.com>
2022-05-03 17:28:49 +0100

    Use firemodels/fig repo for test data
    Now that firemodels/fig has merged the appropriate test data we can use it directly.

--- Tests/get_test_data.sh
+++ Tests/get_test_data.sh
@@ -1,8 +1,8 @@
-COMMIT=06449f1db6f42c41f0d43d71f1c46513f98c7d3c
+COMMIT=97c852beff8c6142ec09dc0092381a605d08d943
 
 TEMP_DIR=$(mktemp -d)
 ZIP_PATH=$TEMP_DIR/test-data.zip
-wget https://github.com/JakeOShannessy/fig/archive/$COMMIT.zip -O $ZIP_PATH
+wget https://github.com/firemodels/fig/archive/$COMMIT.zip -O $ZIP_PATH
 unzip $ZIP_PATH
 mkdir -p fig
 mv fig-$COMMIT/* fig
