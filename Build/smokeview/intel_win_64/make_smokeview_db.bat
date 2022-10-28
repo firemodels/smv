fb3999237433677be70b5652986a3b5efe2e7055
gforney <gforney@gmail.com>
2022-10-27 22:24:53 -0400

    smv build: remove redirect to a file for the windows debug smokeview build

--- Build/smokeview/intel_win_64/make_smokeview_db.bat
+++ Build/smokeview/intel_win_64/make_smokeview_db.bat
@@ -27,8 +27,7 @@ if x%inc% == xinc goto skip_inc
 erase *.obj *.mod *.exe
 :skip_inc
 
-make -j 4 ICON="%ICON%" GLUT="%GLUT%" SHELL="%ComSpec%" SMV_TESTFLAG="%SMV_TESTFLAG%" SMV_TESTSTRING="%SMV_TESTSTRING%" -f ..\Makefile intel_win_64_db > compile.out 2>&1
-call :find_smokeview_warnings compile.out
+make -j 4 ICON="%ICON%" GLUT="%GLUT%" SHELL="%ComSpec%" SMV_TESTFLAG="%SMV_TESTFLAG%" SMV_TESTSTRING="%SMV_TESTSTRING%" -f ..\Makefile intel_win_64_db
 
 if x%from% == xbot goto skip2
 pause
