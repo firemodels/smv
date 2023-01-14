f83b4b655a986f0201db11f141017ecdac4525f4
Glenn Forney <gforney@gmail.com>
2023-01-14 09:50:49 -0500

    smokeview source: turn off colorbar adjustment

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -12,7 +12,7 @@
   #define PROGVERSION ""
 #endif
 
-#define pp_ADJUST_COLORBAR  // adjust "brightness" of colorbar to make transition more uniform
+//#define pp_ADJUST_COLORBAR  // adjust "brightness" of colorbar to make transition more uniform
 
 // use floating point to color data
 
