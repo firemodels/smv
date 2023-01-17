3aba4a9e1a36411dc8d8f4e580c87bcdb0e15b73
Glenn Forney <gforney@gmail.com>
2023-01-16 22:14:22 -0500

    smokeview source: turn off csv colorbars

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -13,7 +13,7 @@
 #endif
 
 //#define pp_COLORBAR_DEBUG     // output colorbar debug info
-#define pp_COLORBARS_CSV      // add csv colorbars//
+//#define pp_COLORBARS_CSV      // add csv colorbars//
 //#define pp_COLORBAR_CONSTANT  // make colorbar brightness constant in colorbar dialog box
 
 // use floating point to color data
