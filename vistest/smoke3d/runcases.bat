24ac385b33fc4f245a0a152371edc54b15d93661
gforney <gforney@gmail.com>
2023-02-06 11:02:18 -0500

    FDS source: add a batch file to run smoke test cases

---
+++ vistest/smoke3d/runcases.bat
@@ -0,0 +1,8 @@
+@echo off
+set fdsexe=%userprofile%\FireModels_fork\fds\Build\impi_intel_win_dv\fds_impi_intel_win_dv
+%fdsexe% smokex010.fds
+%fdsexe% smokex020.fds
+%fdsexe% smokex040.fds
+%fdsexe% smokex080.fds
+%fdsexe% smokex160.fds
+%fdsexe% smokex320.fds
