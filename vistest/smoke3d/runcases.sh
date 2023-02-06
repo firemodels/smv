1d764249ecd1b26810ff5461b33140d2d688980e
gforney <gforney@gmail.com>
2023-02-06 16:57:46 -0500

    smv vv: add bash script to run cases

---
+++ vistest/smoke3d/runcases.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+QFDS=$HOME/FireModels_fork/fds/Utilities/Scripts/qfds.sh
+$QFDS smokex010.fds
+$QDFS smokex020.fds
+$QFDS smokex040.fds
+$QFDS smokex080.fds
+$QFDS smokex160.fds
+$QFDS smokex320.fds
