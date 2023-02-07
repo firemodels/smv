1da92219e2da83713573af9038f856306be1e431
Glenn Forney <gforney@gmail.com>
2023-02-07 14:55:38 -0500

    smv vv: update 3d smoke test cases and add smoke scripts

--- vistest/smoke3d/runcases.bat
+++ vistest/smoke3d/runcases.bat
@@ -6,3 +6,9 @@ set fdsexe=%userprofile%\FireModels_fork\fds\Build\impi_intel_win_dv\fds_impi_in
 %fdsexe% smokex080.fds
 %fdsexe% smokex160.fds
 %fdsexe% smokex320.fds
+smokeview -runscript smokex010
+smokeview -runscript smokex020
+smokeview -runscript smokex040
+smokeview -runscript smokex080
+smokeview -runscript smokex160
+smokeview -runscript smokex320
