b0e6efca26285733764c3fcf992b33dc446214ba
Jake O'Shannessy <joshannessy@gmail.com>
2023-05-09 23:21:35 +0100

    cli parser: add new cli parser
    This file which contains a new CLI parser. This does two important
    things:
    
    * Disallows the CLI parser from modifying any global variables or
      otherwise affecting any other parts of the code.
    * Serializes all possible options to a struct which can be documented
      and type checked.
    
    This might seem like an unimportant reorganisation but will make it
    easier to do things in the future (next step being clarification of CHID
    vs filename etc.).
    
    This also adds some additional bounds checks.

---
+++ Source/smokeview/command_args.c
@@ -0,0 +1,322 @@
+/// @file command_args.
+
+// This file explicitly does not include "smokeviewcars.h" to avoid separate
+// this parsing from global variables.
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "MALLOCC.h"
+#include "command_args.h"
+
+const char *CLE_Message(enum CommandLineError cle) {
+  switch (cle) {
+  case CLE_OK:
+    return NULL;
+    break;
+  case CLE_MULTIPLE_LANG:
+    return "-lang defined multiple times";
+    break;
+  case CLE_NO_LANG:
+    return "-lang must be given an argument";
+    break;
+  default:
+    return "a commandline parsing error occurred";
+    break;
+  }
+}
+
+CommandlineArgs ParseCommandlineNew(int argc, char **argv,
+                                    enum CommandLineError *error) {
+  CommandlineArgs args = {0};
+  *error = CLE_OK;
+  if (argc >= 1) {
+    NewMemory((void **)&args.prog, strlen(argv[0]) + 1);
+    strcpy(args.prog, argv[0]);
+  }
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-ini") == 0) {
+      args.ini = true;
+    } else if (strcmp(argv[i], "-ng_ini") == 0) {
+      args.ng_ini = true;
+    } else if (strcmp(argv[i], "-volrender") == 0) {
+      args.volrender = true;
+    }
+    // We don't show the version if volrender is set? This is probably an
+    // unecesary hack
+    else if (strcmp(argv[i], "-volrender") != 0 &&
+             (strcmp(argv[i], "-version") == 0 || strcmp(argv[i], "-v") == 0)) {
+      args.print_version = true;
+    }
+#ifdef pp_OSX_HIGHRES
+    else if (strcmp(argv[i], "-1x") == 0) {
+      args.x1 = true;
+    } else if (strcmp(argv[i], "-2x") == 0) {
+      args.x2 = true;
+    }
+#endif
+    else if (strcmp(argv[i], "-update_bounds") == 0) {
+      args.update_bounds = true;
+    } else if (strcmp(argv[i], "-no_graphics") == 0) {
+      args.no_graphics = true;
+    } else if (strcmp(argv[i], "-update_slice") == 0) {
+      args.update_slice = true;
+    } else if (strcmp(argv[i], "-update") == 0) {
+      args.update = true;
+    } else if (strcmp(argv[i], "-nogpu") == 0) {
+      args.nogpu = true;
+    } else if (strcmp(argv[i], "-demo") == 0) {
+      args.demo = true;
+    } else if (strcmp(argv[i], "-info") == 0) {
+      args.info = true;
+    } else if (strcmp(argv[i], "-sizes") == 0) {
+      args.sizes = true;
+    } else if (strcmp(argv[i], "-stereo") == 0) {
+      args.stereo = true;
+    } else if (strcmp(argv[i], "-big") == 0) {
+      args.big = true;
+    } else if (strcmp(argv[i], "-timings") == 0) {
+      args.timings = true;
+    } else if (strcmp(argv[i], "-lang") == 0) {
+      // The next argument is a language name
+      ++i;
+      if (i < argc) {
+        if (args.lang != NULL) {
+          *error = CLE_MULTIPLE_LANG;
+          return args;
+        }
+        // Why the additional 48 bytes?
+        NewMemory((void **)&args.lang, strlen(argv[i]) + 48 + 1);
+        strcpy(args.lang, argv[i]);
+      } else {
+        *error = CLE_NO_LANG;
+        return args;
+      }
+    } else if (strcmp(argv[i], "-convert_ini") == 0) {
+      args.convert_ini = true;
+      // The next two arguments are the from and to paths for ini conversion.
+      char *local_ini_from = NULL, *local_ini_to = NULL;
+
+      if (++i < argc) {
+        local_ini_from = argv[i];
+      } else {
+        *error = CLE_NO_INI_ARGS;
+        return args;
+      }
+      if (++i < argc) {
+        local_ini_to = argv[i];
+      } else {
+        *error = CLE_NO_INI_ARGS;
+        return args;
+      }
+      if (local_ini_from != NULL && local_ini_to != NULL) {
+        NewMemory((void **)&args.ini_from, strlen(local_ini_from) + 1);
+        strcpy(args.ini_from, local_ini_from);
+
+        NewMemory((void **)&args.ini_to, strlen(local_ini_to) + 1);
+        strcpy(args.ini_to, local_ini_to);
+      }
+    } else if (strcmp(argv[i], "-convert_ssf") == 0) {
+      args.convert_ssf = true;
+      // The next two arguments are the from and to paths for ssf conversion.
+      char *local_ssf_from = NULL, *local_ssf_to = NULL;
+
+      if (++i < argc) {
+        local_ssf_from = argv[i];
+      } else {
+        *error = CLE_NO_SSF_ARGS;
+        return args;
+      }
+      if (++i < argc) {
+        local_ssf_to = argv[i];
+      } else {
+        *error = CLE_NO_SSF_ARGS;
+        return args;
+      };
+      if (local_ssf_from != NULL && local_ssf_to != NULL) {
+        NewMemory((void **)&args.ssf_from, strlen(local_ssf_from) + 1);
+        strcpy(args.ssf_from, local_ssf_from);
+
+        NewMemory((void **)&args.ssf_to, strlen(local_ssf_to) + 1);
+        strcpy(args.ssf_to, local_ssf_to);
+      }
+    } else if (strcmp(argv[i], "-update_ssf") == 0) {
+      args.update_ssf = true;
+    } else if (strcmp(argv[i], "-update_ini") == 0) {
+      // As per convert_ini, but in-place
+      args.update_ini = true;
+      char *local_ini_from = NULL, *local_ini_to = NULL;
+
+      if (++i < argc) {
+        local_ini_from = argv[i];
+      } else {
+        *error = CLE_NO_INI_ARGS;
+        return args;
+      }
+      local_ini_to = local_ini_from;
+      if (local_ini_from != NULL) {
+        NewMemory((void **)&args.ini_from, strlen(local_ini_from) + 1);
+        strcpy(args.ini_from, local_ini_from);
+
+        NewMemory((void **)&args.ini_to, strlen(local_ini_to) + 1);
+        strcpy(args.ini_to, local_ini_to);
+      }
+    } else if (strcmp(argv[i], "-isotest") == 0) {
+      args.isotest = true;
+    } else if (strcmp(argv[i], "-smoke3d") == 0) {
+      args.smoke3d = true;
+    } else if (strcmp(argv[i], "-no_slcf") == 0) {
+      args.no_slcf = true;
+    } else if (strcmp(argv[i], "-h") == 0 &&
+               strcmp(argv[i], "-help_all") != 0 &&
+               strcmp(argv[i], "-html") != 0) {
+      args.show_help_summary = true;
+    } else if (strcmp(argv[i], "-help_all") == 0) {
+      args.show_help_all = true;
+    } else if (strcmp(argv[i], "-noblank") == 0) {
+      args.noblank = true;
+    } else if (strcmp(argv[i], "-fed") == 0) {
+      args.fed = true;
+    } else if (strcmp(argv[i], "-verbose") == 0) {
+      args.verbose = true;
+    } else if (strcmp(argv[i], "-outline") == 0) {
+      args.outline = true;
+    } else if (strcmp(argv[i], "-make_movie") == 0) {
+      args.make_movie = true;
+    } else if (strcmp(argv[i], "-geominfo") == 0) {
+      args.geominfo = true;
+    } else if (strcmp(argv[i], "-fast") == 0) {
+      args.fast = true;
+    } else if (strcmp(argv[i], "-blank") == 0) {
+      args.blank = true;
+    } else if (strcmp(argv[i], "-gversion") == 0) {
+      args.gversion = true;
+    } else if (strcmp(argv[i], "-redirect") == 0) {
+      args.redirect = true;
+    } else if (strcmp(argv[i], "-runscript") == 0) {
+      args.runscript = true;
+    } else if (strcmp(argv[i], "-runhtmlscript") == 0) {
+      args.runhtmlscript = true;
+    }
+#ifdef pp_LUA
+    else if (strcmp(argv[i], "-runluascript") == 0) {
+      args.runluascript = true;
+    } else if (strcmp(argv[i], "-killscript") == 0) {
+      args.killscript = true;
+    } else if (strcmp(argv[i], "-luascript") == 0) {
+      ++i;
+      if (i < argc) {
+        NewMemory((void **)&args.scriptrenderdir, strlen(argv[i]) + 1);
+        strcpy(args.scriptrenderdir, argv[i]);
+      } else {
+        *error = CLE_ARGUMENT_EXPECTED;
+        return args;
+      }
+    }
+#endif
+    else if (strcmp(argv[i], "-scriptrenderdir") == 0) {
+      i++;
+      if (i < argc) {
+        NewMemory((void **)&args.scriptrenderdir, strlen(argv[i]) + 1);
+        strcpy(args.scriptrenderdir, argv[i]);
+      } else {
+        *error = CLE_ARGUMENT_EXPECTED;
+        return args;
+      }
+    } else if (strcmp(argv[i], "-skipframe") == 0) {
+      args.skipframe_defined = true;
+      ++i;
+      if (i < argc) {
+        sscanf(argv[i], "%i", &args.skipframe);
+      } else {
+        *error = CLE_ARGUMENT_EXPECTED;
+        return args;
+      }
+    } else if (strcmp(argv[i], "-startframe") == 0) {
+      args.startframe_defined = true;
+      ++i;
+      if (i < argc) {
+        sscanf(argv[i], "%i", &args.startframe);
+      } else {
+        *error = CLE_ARGUMENT_EXPECTED;
+        return args;
+      }
+    } else if (strcmp(argv[i], "-volrender") == 0) {
+      args.volrender = true;
+    } else if (strcmp(argv[i], "-script") == 0) {
+      ++i;
+      if (i < argc) {
+        NewMemory((void **)&args.script, strlen(argv[i]) + 1);
+        strcpy(args.script, argv[i]);
+      } else {
+        *error = CLE_ARGUMENT_EXPECTED;
+        return args;
+      }
+    } else if (strcmp(argv[i], "-htmlscript") == 0) {
+      ++i;
+      if (i < argc) {
+        NewMemory((void **)&args.htmlscript, strlen(argv[i]) + 1);
+        strcpy(args.htmlscript, argv[i]);
+      } else {
+        *error = CLE_ARGUMENT_EXPECTED;
+        return args;
+      }
+    } else if (strcmp(argv[i], "-noexit") == 0) {
+      args.noexit = true;
+    } else if (strcmp(argv[i], "-setup") == 0) {
+      args.setup = true;
+    } else if (strcmp(argv[i], "-bindir") == 0) {
+      ++i;
+      if (i < argc) {
+        NewMemory((void **)&args.bindir, strlen(argv[i]) + 1);
+        strcpy(args.bindir, argv[i]);
+      } else {
+        *error = CLE_ARGUMENT_EXPECTED;
+        return args;
+      }
+    } else if (strcmp(argv[i], "-casedir") == 0) {
+      ++i;
+      if (i < argc) {
+        NewMemory((void **)&args.casedir, strlen(argv[i]) + 1);
+        strcpy(args.casedir, argv[i]);
+      } else {
+        *error = CLE_ARGUMENT_EXPECTED;
+        return args;
+      }
+    } else if (strcmp(argv[i], "-threads") == 0) {
+      args.threads_defined = true;
+      ++i;
+      if (i < argc) {
+        sscanf(argv[i], "%i", &args.threads);
+      } else {
+        *error = CLE_ARGUMENT_EXPECTED;
+        return args;
+      }
+    } else {
+      if (args.input_file == NULL) {
+        NewMemory((void **)&args.input_file, strlen(argv[i]) + 1);
+        strcpy(args.input_file, argv[i]);
+      } else {
+        *error = CLE_MULTIPLE_INPUTS;
+        return args;
+      }
+    }
+  }
+  return args;
+}
+
+/// @brief Free any allocated buffers in @ref CommandlineArgs. Performs
+/// null-checks on all buffers.
+/// @param args The previously parse command line arguments.
+void FreeCommandlineArgs(CommandlineArgs *args) {
+  FREEMEMORY(args->prog);
+  FREEMEMORY(args->input_file);
+  FREEMEMORY(args->bindir);
+  FREEMEMORY(args->casedir);
+  FREEMEMORY(args->lang);
+  FREEMEMORY(args->ini_from);
+  FREEMEMORY(args->ini_to);
+  FREEMEMORY(args->ssf_from);
+  FREEMEMORY(args->ssf_to);
+}
---
+++ Source/smokeview/command_args.h
@@ -0,0 +1,127 @@
+/// @file command_args.h
+#ifndef PATHS_H_DEFINED
+#define PATHS_H_DEFINED
+
+#include <stdbool.h>
+
+/// @brief The parsed version of commandline arguments.
+typedef struct CommandlineArgs {
+  /// @brief output smokeview parameter values to smokeview.ini
+  bool ini;
+  /// @brief non-graphics version of -ini
+  bool ng_ini;
+  /// @brief generate images of volume rendered smoke and fire
+  bool volrender;
+  /// @brief Either -version or -v was set, so print the version of smokeview
+  /// and exit.
+  bool print_version;
+  bool update_bounds;
+  bool no_graphics;
+  /// @brief calculate slice file parameters
+  bool update_slice;
+  bool nogpu;
+  /// @brief use demonstrator mode of Smokeview"
+  bool demo;
+  /// @brief generate casename.slcf and casename.viewpoint files containing
+  /// slice file and viewpiont info
+  bool info;
+  /// @brief output files sizes then exit
+  bool sizes;
+  /// @brief activate stereo mode
+  bool stereo;
+  /// @brief hide scene and data when moving scene or selecting menus
+  bool big;
+#ifdef pp_OSX_HIGHRES
+  /// @brief On Mac, turn off 2x scene scaling (do not scale scene)
+  bool x1;
+  /// @brief On Mac, turn on 2x scene scaling
+  bool x2;
+#endif
+  /// @brief show startup timings
+  bool timings;
+  bool convert_ini;
+  bool convert_ssf;
+  /// @brief update case.ini to the current format
+  bool update_ini;
+  bool update_ssf;
+  bool isotest;
+  /// @brief only show 3D smoke
+  bool smoke3d;
+  bool no_slcf;
+  bool show_help_summary;
+  bool show_help_all;
+  bool noblank;
+  /// @brief pre-calculate all FED slice files
+  bool fed;
+  bool verbose;
+  /// @brief show geometry bound boxes instead of geometry
+  bool outline;
+  /// @brief open the movie generating dialog box
+  bool make_movie;
+  /// @brief output information about geometry triangles
+  bool geominfo;
+  /// @brief assume slice files exist in order to reduce startup time
+  bool fast;
+  bool blank;
+  bool gversion;
+  bool redirect;
+  /// @brief Run the default SSF script, i.e. CHID.ssf
+  bool runscript;
+  bool runhtmlscript;
+  /// @brief Run the SSF script at this path
+  char *script;
+  /// @brief Run the HTML script at this path
+  char *htmlscript;
+#ifdef pp_LUA
+  /// @brief Run the default lua script, i.e. CHID.lua
+  bool runluascript;
+  /// @brief If the lua script fails, kill smokeview and return an error exit
+  /// code.
+  bool killscript;
+  /// @brief Run a custom lua script at this path.
+  char *luascript;
+#endif
+  /// @brief set directory containing script rendered images (override directory
+  /// specified by RENDERDIR script keyword)
+  char *scriptrenderdir;
+  // TODO: consider sentinel values
+  bool skipframe_defined;
+  /// @brief render every n frames, only valid if skipframe_defined is true
+  int skipframe;
+  bool startframe_defined;
+  /// @brief start rendering at frame n, only valid if startframe_defined is
+  /// true
+  int startframe;
+  bool noexit;
+  /// @brief only show geometry
+  bool setup;
+  bool threads_defined;
+  /// @brief equivalent to -update_bounds and -update_slice
+  bool update;
+  int threads;
+  char *prog;
+  char *input_file;
+  char *bindir;
+  char *casedir;
+  /// @brief language code e.g., de, es, fr, for German, Spanish, French or
+  /// Italian
+  char *lang;
+  char *ini_from;
+  char *ini_to;
+  char *ssf_from;
+  char *ssf_to;
+} CommandlineArgs;
+
+enum CommandLineError {
+  CLE_MULTIPLE_LANG,
+  CLE_NO_LANG,
+  CLE_NO_SSF_ARGS,
+  CLE_NO_INI_ARGS,
+  CLE_MULTIPLE_INPUTS,
+  CLE_OK,
+  CLE_ARGUMENT_EXPECTED,
+};
+const char *CLE_Message(enum CommandLineError cle);
+CommandlineArgs ParseCommandlineNew(int argc, char **argv,
+                                    enum CommandLineError *error);
+#endif
