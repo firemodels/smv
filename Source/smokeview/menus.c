5c20d08cd8b0e4701e6fd30e9f5437eaa94b4a5b
gforney <gforney@gmail.com>
2023-06-20 15:59:00 -0400

    smokeview source: fix to circular colorbar menus when there are no circular colorbars

--- Source/smokeview/menus.c
+++ Source/smokeview/menus.c
@@ -8294,12 +8294,12 @@ void MakeColorbarMenu(int *menuptr,
   int submenu4 = 0, submenu5 = 0, submenu6 = 0;
   int submenu7 = 0;
 
-  MakeSubColorbarMenu(&submenu1, &nmenus, "linear",     CBMenu);
-  MakeSubColorbarMenu(&submenu2, &nmenus, "circular",   CBMenu);
   MakeSubColorbarMenu(&submenu3, &nmenus, "rainbow",    CBMenu);
+  MakeSubColorbarMenu(&submenu1, &nmenus, "linear",     CBMenu);
   MakeSubColorbarMenu(&submenu4, &nmenus, "divergent",  CBMenu);
-  MakeSubColorbarMenu(&submenu6, &nmenus, "deprecated", CBMenu);
+  MakeSubColorbarMenu(&submenu2, &nmenus, "circular",   CBMenu);
   MakeSubColorbarMenu(&submenu7, &nmenus, "original",   CBMenu);
+  MakeSubColorbarMenu(&submenu6, &nmenus, "deprecated", CBMenu);
   MakeSubColorbarMenu(&submenu5, &nmenus, "user",       CBMenu);
 #endif
 
@@ -8308,7 +8308,7 @@ void MakeColorbarMenu(int *menuptr,
   if(submenu3 > 0)GLUTADDSUBMENU("rainbow",    submenu3);
   if(submenu1 > 0)GLUTADDSUBMENU("linear",     submenu1);
   if(submenu4 > 0)GLUTADDSUBMENU("divergent",  submenu4);
-  if(submenu3 > 0)GLUTADDSUBMENU("circular",   submenu2);
+  if(submenu2 > 0)GLUTADDSUBMENU("circular",   submenu2);
   if(submenu7 > 0 )GLUTADDSUBMENU("original",  submenu7);
   if(submenu6 > 0)GLUTADDSUBMENU("deprecated", submenu6);
   if(submenu5 > 0)GLUTADDSUBMENU("user",       submenu5);
