3417081ac25ec90c77f144f907741c8816b659dd
gforney <gforney@gmail.com>
2023-06-23 21:02:58 -0400

    Revert "build scripts: add scripts to build using cmake"
    This reverts commit 54c7d3205bb7f07cbde7c83866dda89efe36e55b.

--- Build/scripts/build_cmake.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-CURDIR=`pwd`
-cd ../..
-cmake -B cbuild -DCMAKE_BUILD_TYPE=Release
-cmake --build cbuild --config Release
-cmake --install cbuild --config Release --prefix dist
-cd $CURDIR
