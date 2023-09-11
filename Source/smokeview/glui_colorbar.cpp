017334ac6a5d5266e56b1c1a98d00e0cab235ada
gforney <gforney@gmail.com>
2023-09-11 15:26:03 -0400

    smokeview source: remove pp_COLOR_HIDE directive

--- Source/smokeview/glui_colorbar.cpp
+++ Source/smokeview/glui_colorbar.cpp
@@ -1006,9 +1006,8 @@ extern "C" void GluiColorbarSetup(int main_window){
   glui_colorbar->add_column_to_panel(PANEL_cb_select3,false);
   BUTTON_cb_next     = glui_colorbar->add_button_to_panel(PANEL_cb_select3, _("Next"),     COLORBAR_NEXT, ColorbarCB);
 
-#ifdef pp_COLOR_HIDE
   glui_colorbar->add_checkbox_to_panel(PANEL_cb_select, _("Show scene"), &colorbar_showscene);
-#endif
+
   char label_nodes[sizeof(GLUI_String)];
   strcpy(label_nodes, "nodes");
   STATICTEXT_cb_node_label = glui_colorbar->add_statictext_to_panel(PANEL_cb_select, label_nodes);
--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -20,13 +20,14 @@
 
 //#define pp_SMOKE16      // load 16 bit smoke files
 //#define pp_VOLSMOKE     // add option to compress volume rendered data
-#define pp_FAST           // set fast startup by default
-//#define pp_CHECK_FILES  // check if compressed files exist at startup
-#define pp_COLOR_HIDE     // add checkbox to hide/unhide scene when editing a colorbar
-//#define pp_COLOR_PLOT     /  add checkbox for showing CIELab colorbar delta distance plot
 //#define pp_GAMMA        // show gamma correction checkbox
 #define pp_BLACKBODY      // use blackbody theory for generating fire colors
 // pp_BLACKBODY_OUT       // output generated blackbody color data 
+//#define pp_CHECK_FILES  // check if compressed files exist at startup
+
+#define pp_FAST           // set fast startup by default
+
+//#define pp_COLOR_PLOT     /  add checkbox for showing CIELab colorbar delta distance plot
 
 //#define pp_BNDF         // merge geometry and structured boundary files in load menus
 
