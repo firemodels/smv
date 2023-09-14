774f99e976b1b033a42f880079a42e7103184da9
gforney <gforney@gmail.com>
2023-09-14 10:27:02 -0400

    smv vv: add LITE_Cases.sh script

---
+++ Verification/scripts/LITE_Cases.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+$QFDS -d Visualization plume5c.fds
+$QFDS -p 2 -d Visualization thouse5.fds
+$QFDS -d Visualization cell_test.fds
+$QFDS -d Visualization smokex010.fds
+$QFDS -d Visualization smokex020.fds
+$QFDS -d Visualization smokex040.fds
+$QFDS -d Visualization smokex080.fds
+$QFDS -d Visualization smokex160.fds
+$QFDS -d Visualization colorbar.fds
+$QFDS -d Visualization smoke_test2.fds
+$QFDS -d Visualization fed_test.fds
+$QFDS -d Visualization part_color.fds
+$QFDS -d Visualization slicemask.fds
+$QFDS -d Visualization smoke_sensor.fds
+$QFDS -d Visualization smoke1.fds
+$QFDS -d Visualization smoke2.fds
+$QFDS -d Visualization sprinkler_many.fds
+$QFDS -d Visualization testsort.fds
+$QFDS -d Visualization vcirctest.fds
