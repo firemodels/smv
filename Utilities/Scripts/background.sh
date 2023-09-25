b8d83e57142f3b5da3733b935aaecf9458df63a1
gforney <gforney@gmail.com>
2023-09-25 14:17:33 -0400

    smv vv: fix to background program

--- Utilities/Scripts/background.sh
+++ Utilities/Scripts/background.sh
@@ -107,6 +107,24 @@ fi
 if [ "$SLEEP" == "" ]; then
   $EXE $input
 else
-  $REPOROOT/smv/Utilities/Scripts/run_prog.sh $EXE $input &
+  LOCKBASE=`whoami`_fdslock
+  LOCKFILE=/tmp/${input}_${LOCKBASE}$$
+
+  NPROCS=`grep processor /proc/cpuinfo | wc -l`
+  NJOBS=0
+  ls -l /tmp/*${LOCKBASE}* >& $LSOUT
+  if [ "`grep 'No such' $LSOUT`" == "" ]; then
+    NJOBS=`ls -l /tmp/*${LOCKBASE}* | wc -l`
+  fi
+
+  while [ $NJOBS -gt $NPROCS ]; do
+    sleep 5
+    NJOBS=0
+    ls -l /tmp/*${LOCKBASE}* >& $LSOUT
+    if [ "`grep 'No such' $LSOUT`" == "" ]; then
+      NJOBS=`ls -l /tmp/*${LOCKBASE}* | wc -l`
+    fi
+  done
   sleep 5
+  $REPOROOT/smv/Utilities/Scripts/run_prog.sh $EXE $input &
 fi
