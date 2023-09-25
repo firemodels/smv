0e03cb73932a9330e15bff659aff289583972620
gforney <gforney@gmail.com>
2023-09-25 09:46:46 -0400

    scripts: correction to run_prog script

--- Utilities/Scripts/run_prog.sh
+++ Utilities/Scripts/run_prog.sh
@@ -4,17 +4,26 @@ input=$2
 
 LOCKBASE=`whoami`_fdslock
 LOCKFILE=/tmp/${input}_${LOCKBASE}$$
+LSOUT=/tmp/lsout.$$
 
 NPROCS=`grep processor /proc/cpuinfo | wc -l`
-NJOBS=`ls -l /tmp/*${LOCKBASE}* | wc -l`
+NJOBS=0
+ls -l /tmp/*${LOCKBASE}* >& $LSOUT
+if [ "`grep 'No such' $LSOUT`" == "" ]; then
+  NJOBS=`ls -l /tmp/*${LOCKBASE}* | wc -l`
+fi
 
-sleep 5
 while [ $NJOBS -gt $NPROCS ]; do
   sleep 5
-  NJOBS=`ls -l /tmp/*${LOCKBASE}* | wc -l`
+  NJOBS=0
+  ls -l /tmp/*${LOCKBASE}* >& $LSOUT
+  if [ "`grep 'No such' $LSOUT`" == "" ]; then
+    NJOBS=`ls -l /tmp/*${LOCKBASE}* | wc -l`
+  fi
 done
 sleep 5
 
+rm $LSOUT
 touch $LOCKFILE
 $EXE $input
 rm -f $LOCKFILE
