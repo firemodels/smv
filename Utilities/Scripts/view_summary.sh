84b542217e823a03f565c816fea842dd25ab8dfd
gforney <gforney@gmail.com>
2023-09-26 10:10:19 -0400

    smv vv: redirect view summary output to /dev/null

--- Utilities/Scripts/view_summary.sh
+++ Utilities/Scripts/view_summary.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-cd ../../Manuals/SMV_Summary
-firefox diffs.html
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ "`which firefox`" == "" ]; then
+  echo "***error: web browser, firefox, is not installed"
+  exit
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR/../../..
+ROOT_DIR=`pwd`
+
+SUMMARY_DIR=$ROOT_DIR/smv/Manuals/SMV_Summary
+SUMMARY_FILE=$SUMMARY_DIR/diffs.html
+if [ -e $SUMMARY_FILE ]; then
+  firefox $SUMMARY_FILE >& /dev/null
+else
+  echo "***error: $SUMMARY_FILE does not exist"
+fi
