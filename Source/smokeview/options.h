b3f8b2e1858b684092d1cebc38bcfc151987c594
gforney <gforney@gmail.com>
2023-10-09 21:59:24 -0400

    smokeview source: remove pp_THREADBUFFER directive

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -42,7 +42,6 @@
 
 //#define pp_DPRINT       // turn on debug print (file, line number)
 
-#define pp_THREADBUFFER
 //#define pp_CRASH_TEST       // test detection of division by zero or use of undefined pointer
 #define pp_GPU                // support the GPU
 #define pp_THREAD             // turn on multi-threading
