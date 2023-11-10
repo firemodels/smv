ff3c6da7c7196ee6510933a819f264f109239def
Jake O'Shannessy <joshannessy@gmail.com>
2023-11-10 20:53:17 +0000

    ci: fetch thouse5 from fig and run render tests
    This fetches a completed FDS run from the fig repo (thouse5) and runs
    the SSF scripts for it on Github Actions.
    
    Currently it fetches data from a fork of fig, but will be pointed toward
    firemodels/fig if the PR on that repo is merged.

--- Tests/get_test_data.ps1
+++ Tests/get_test_data.ps1
@@ -1,9 +1,9 @@
-$commit = "97c852beff8c6142ec09dc0092381a605d08d943"
+$commit = "67300634bc63eb9a6959abf6623b4922ff7b1e34"
 
 $zip_path = "$env:temp\test-data.zip"
 $unzip_path = "$env:temp\test-data-out"
-Invoke-WebRequest -Uri https://github.com/firemodels/fig/archive/$commit.zip -OutFile $zip_path
+Invoke-WebRequest -Uri https://github.com/JakeOShannessy/fig/archive/$commit.zip -OutFile $zip_path
 Expand-Archive $zip_path -DestinationPath $unzip_path -Force
 mkdir fig -Force
 Move-Item $unzip_path/fig-$commit/* fig -Force
-
+Copy-Item -Force -Recurse fig/smv/Tests/Visualization ../Verification
