84a0c1fc0f44e1391bba5a92c843360456c7ab49
Jake O'Shannessy <joshannessy@gmail.com>
2023-11-23 09:54:51 +0000

    removed shadowed variables

--- Source/smokeview/readsmv.c
+++ Source/smokeview/readsmv.c
@@ -6881,7 +6881,7 @@ int ReadSMV_Init() {
 /// @return zero on success, non-zero on failure
 int ReadSMV_Parse(bufferstreamdata *stream) {
   int i;
-  float processing_time, wrapup_time, getfilelist_time;
+  float wrapup_time;
   int have_zonevents,nzventsnew=0;
   devicedata *devicecopy;
   int do_pass4=0, do_pass5=0;
