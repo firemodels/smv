e7dd39b08d159c3c22538b1fbae1925c206cc2fc
gforney <gforney@gmail.com>
2023-12-06 14:50:14 -0500

    Revert "smokeview source: fix memory access problems identified by the intel sanitize option"
    This reverts commit b3e965207c141ec4ceb6621a9f9c1945b31e92d5.

--- Source/smokeview/IOplot2d.c
+++ Source/smokeview/IOplot2d.c
@@ -10,20 +10,12 @@
 /* ------------------ GetCsvData ------------------------ */
 
 csvdata *GetCsvData(int file_index, int col_index, csvfiledata **csvf_ptr){
+  csvfiledata *csvfi;
   csvdata *csvi=NULL;
 
-  if(csvf_ptr != NULL){
-    csvfiledata *csvfi;
-
-    csvfi = csvfileinfo    + file_index;
-    if(csvfi->csvinfo!=NULL){
-      csvi  = csvfi->csvinfo + col_index;
-      *csvf_ptr = csvfi;
-    }
-    else{
-      *csvf_ptr = NULL;
-    }
-  }
+  csvfi = csvfileinfo    + file_index;
+  if(csvfi->csvinfo!=NULL)csvi  = csvfi->csvinfo + col_index;
+  if(csvf_ptr != NULL)*csvf_ptr = csvfi;
   return csvi;
 }
 
