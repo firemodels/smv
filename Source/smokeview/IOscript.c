8ac370b981da5d198f9527d34a3005897eae67bc
gforney <gforney@gmail.com>
2023-12-11 16:28:04 -0500

    smokeview source: also let '#' be a comment character in a smokeview script file

--- Source/smokeview/IOscript.c
+++ Source/smokeview/IOscript.c
@@ -232,6 +232,7 @@ int GetParamBuffer(FILE *stream){
     if(fgets(param_buffer, 1024, stream)==NULL)return SCRIPT_EOF;
     line_number++;
     comment = strstr(param_buffer, "//");
+    if(comment==NULL)comment = strstr(param_buffer, "#");
     if(comment!=NULL)comment[0]=0;
     TrimBack(param_buffer);
     if(strlen(param_buffer)==0)continue;
@@ -429,7 +430,8 @@ int CheckScript(char *file){
       return return_val;
     }
     line_number++;
-    comment = strstr(buffer, "//");
+    comment = strstr(param_buffer, "//");
+    if(comment==NULL)comment = strstr(param_buffer, "#");
     if(comment != NULL)comment[0] = 0;
     keyword = TrimFrontBack(buffer);
     if(strlen(buffer)==0)continue;
@@ -471,7 +473,8 @@ keyworddata *GetScriptKeyword(FILE *stream){
 
     if(fgets(keyword_buffer, 1024, stream)==NULL)return NULL;
     line_number++;
-    comment = strstr(keyword_buffer, "//");
+    comment = strstr(param_buffer, "//");
+    if(comment==NULL)comment = strstr(param_buffer, "#");
     if(comment != NULL)comment[0] = 0;
     TrimBack(keyword_buffer);
     if(strlen(keyword_buffer)==0||keyword_buffer[0]==' ')continue;
