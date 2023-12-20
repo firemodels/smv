33c25d8f646bf857818ed0731bbd1201e98c903b
gforney <gforney@gmail.com>
2023-12-20 15:44:38 -0500

    smokeview source: remove multi threading from read buffer routine, simplify read buffer routine

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -38,7 +38,6 @@
 
 //#define pp_LOAD_INC     // add menu item for loading new data
 
-#define pp_READBUFFER_THREAD // use pthreads in ReadBuffer routine (don't use pthreads when used in smokezip and smokediff)
 //#define pp_CRASH_TEST       // test detection of division by zero or use of undefined pointer
 #define pp_GPU                // support the GPU
 #define pp_THREAD             // turn on multi-threading
