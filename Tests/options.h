5f6b393cddc2e340f3f628b52513cee024b8aded
gforney <gforney@gmail.com>
2024-01-09 10:47:21 -0500

    parse_simple source: turn off pp_THREAD

--- Tests/options.h
+++ Tests/options.h
@@ -42,7 +42,7 @@
 
 //#define pp_CRASH_TEST       // test detection of division by zero or use of undefined pointer
 #define pp_GPU                // support the GPU
-#define pp_THREAD             // turn on multi-threading
+//#define pp_THREAD             // turn on multi-threading
 //#define pp_LOAD_NEWDATA     // add button for loading new data
 
 #ifdef pp_GPU
