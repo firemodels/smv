2dbaae75966d8854d145a4e4fa37ba4af3236c44
gforney <gforney@gmail.com>
2024-01-13 10:34:16 -0500

    smv vv: update run restart cases script

--- Verification/scripts/Run_RESTART_Cases.sh
+++ Verification/scripts/Run_RESTART_Cases.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-JPREFIX=$1
-if [ "$JPREFIX" == "" ]; then
-  JPREFIX="RES"
-fi
-PREFIX="-j $JPREFIX"
 CURDIR=`pwd`
 cd ../../../fds
 FDSREPO=`pwd`
 QFDS=$FDSREPO/Utilities/Scripts/qfds.sh
 cd $CURDIR/..
+
+function usage {
+echo "Run_SMV_Cases.sh [-d -h -m max_iterations -o nthreads -p -q queue -s ]"
+echo "Runs Smokeview verification suite"
+echo ""
+echo "Options"
+echo "-h - display this message"
+echo "-j p - specify a job prefix"
+echo "-q queue - run cases using the queue named queue"
+exit
+}
 
 wait_cases_end()
 {
@@ -28,15 +34,42 @@ wait_cases_end()
    fi
 }
 
+QUEUE=
+JOBPREFIX=
+while getopts 'hj:q:' OPTION
+do
+case $OPTION in
+  h)
+   usage;
+   ;;
+  j)
+   JOBPREFIX="$OPTARG"
+   ;;
+  q)
+   QUEUE="$OPTARG"
+   ;;
+esac
+#shift
+done
+
+if [ "$QUEUE" != "" ]; then
+  QUEUE="-q $QUEUE"
+fi
+if { "$JOBPREFIX" != "" ]; then
+  JOBPREFIX="RES"
+fi
+prefix=$JOBPREFIX
+JOBPREFIX="-j $JOBPREFIX"
+
 echo "running base cases"
 echo ""
-$QFDS -p 6 $PREFIX -d Visualization geom1.fds
-$QFDS -p 2 $PREFIX -d Visualization test1.fds
-$QFDS -p 6 $PREFIX -d Visualization geom2.fds
-$QFDS -p 2 $PREFIX -d Visualization test2.fds
+$QFDS -p 6 $JOBPREFIX $QUEUE -d Visualization geom1.fds
+$QFDS -p 2 $JOBPREFIX $QUEUE -d Visualization test1.fds
+$QFDS -p 6 $JOBPREFIX $QUEUE -d Visualization geom2.fds
+$QFDS -p 2 $JOBPREFIX $QUEUE -d Visualization test2.fds
 
 wait_cases_end $JPREFIX
 echo "running restart jobs"
 echo ""
-$QFDS -p 6 $PREFIX -d Visualization geom1_restart.fds
-$QFDS -p 2 $PREFIX -d Visualization test1_restart.fds
+$QFDS -p 6 $JOBPREFIX $QUEUE -d Visualization geom1_restart.fds
+$QFDS -p 2 $JOBPREFIX $QUEUE -d Visualization test1_restart.fds
