7168df917484515cf6c696b459472f70d8c45091
gforney <gforney@gmailcom>
2024-01-12 22:31:18 -0500

    smv vv: update restart script

--- Verification/scripts/RESTART2_Cases.sh
+++ Verification/scripts/RESTART2_Cases.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+JPREF=$1
+if [ "$JPREF" ]; then
+ JPREF="-j $JPREF"
+fi
 
-$QFDS -p 6 -d Visualization/RESTART3 geom3.fds
-$QFDS -p 2 -d Visualization/RESTART3 test3.fds
+$QFDS -p 6 $JPREF -d Visualization/RESTART3 geom3.fds
+$QFDS -p 2 $JPREF -d Visualization/RESTART3 test3.fds
