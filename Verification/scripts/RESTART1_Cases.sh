ce64046a5f27c34ea79c8aaf5df0abb14ef775ac
gforney <gforney@gmailcom>
2024-01-12 22:12:53 -0500

    smv vv: update run cases scripts

--- Verification/scripts/RESTART1_Cases.sh
+++ Verification/scripts/RESTART1_Cases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$QFDS -p 6 -j RES -d Visualization/RESTART1 geom1.fds
-$QFDS -p 2 -j RES -d Visualization/RESTART1 test1.fds
-$QFDS -p 6 -j RES -d Visualization/RESTART2 geom2.fds
-$QFDS -p 2 -j RES -d Visualization/RESTART2 test2.fds
+$QFDS -p 6 -d Visualization/RESTART1 geom1.fds
+$QFDS -p 2 -d Visualization/RESTART1 test1.fds
+$QFDS -p 6 -d Visualization/RESTART2 geom2.fds
+$QFDS -p 2 -d Visualization/RESTART2 test2.fds
--- Verification/scripts/RESTART2_Cases.sh
+++ Verification/scripts/RESTART2_Cases.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$QFDS -p 6 -j RES -d Visualization/RESTART3 geom3.fds
-$QFDS -p 2 -j RES -d Visualization/RESTART3 test3.fds
+$QFDS -p 6 -d Visualization/RESTART3 geom3.fds
+$QFDS -p 2 -d Visualization/RESTART3 test3.fds
