086f4e492c53e43c8c474de44d28cea249c3a1a9
gforney <gforney@gmail.com>
2024-04-25 23:07:56 -0400

    update python sample case gui

--- vistest/Examples/fdsdemo.py
+++ vistest/Examples/fdsdemo.py
@@ -13,28 +13,56 @@ root = Tk()
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 
-button_width=13
+button_width=8
+edit_width=10
 
 # link windows batch files to python commands
 
-def edit_case(): subprocess.Popen(["notepad","simple1.fds",""])
-def run_case():  subprocess.Popen(["fds_local.bat","simple1.fds",""])
+def make_case(): os.system("makecase " + fire_size.get() + " " + door_height.get() + " " + gravx.get() + " " + gravz.get())
+
+cmd = "fds_local simple1.fds"
+args = shlex.split(cmd)
+def run_case(): subprocess.Popen(args)
+
 cmd = "smokeview simple1"
 args = shlex.split(cmd)
 def view_case(): subprocess.Popen(args)
-#def view_case(): os.system("smokeview simple1")
 
-#cmd = "smokeview simple1"
-#args = shlex.split(cmd)
-#p = sub.Popen(args,,)
- 
 root.title('FDS Demo')
 root.resizable(0, 0)
 
 R=0
+
+Label(root, text="fire size (kW):").grid(sticky="e",row=R, column=0)
+fire_size=Entry(root, width = edit_width)
+fire_size.grid(sticky="e",row=R, column=1)
+fire_size.insert(0,"1000")
+
 R=R+1
-Button(root, text="Edit case",  width=button_width, command=edit_case).grid(row=R, column=1)
-Button(root, text="Run case",   width=button_width, command=run_case).grid(row=R,  column=2)
-Button(root, text="View case",  width=button_width, command=view_case).grid(row=R, column=3)
+Label(root, text="door height (m):").grid(sticky="e",row=R, column=0)
+door_height=Entry(root, width = edit_width)
+door_height.grid(sticky="e",row=R, column=1)
+door_height.insert(0,"2.0")
+
+R=R+1
+Label(root, text="gravity x (m/s2):").grid(row=R, column=0)
+gravx=Entry(root, width = edit_width)
+gravx.grid(sticky="e",row=R, column=1)
+gravx.insert(0,"0.0")
+
+R=R+1
+Label(root, text="gravity z (m/s2):").grid(sticky="e",row=R, column=0)
+gravz=Entry(root, width = edit_width)
+gravz.grid(sticky="e",row=R, column=1)
+gravz.insert(0,"-9.8")
+
+#R=R+1
+#option1 = IntVar()
+#Chkoption1 = Checkbutton(root, text="option1", width = 15, variable = option1).grid(row=R, column=0)
+
+R=R+1
+Button(root, text="Make case",  width=button_width, command=make_case).grid(row=R, column=0)
+Button(root, text="Run case",   width=button_width, command=run_case).grid(row=R,  column=1)
+Button(root, text="View case",  width=button_width, command=view_case).grid(row=R, column=2)
 
 root.mainloop()
---
+++ vistest/Examples/makecase.bat
@@ -0,0 +1,20 @@
+@echo off
+set firesize=%1
+set doorheight=%2
+set gravx=%3
+set gravz=%4
+set CHID=simple1
+set outfile=%CHID%.fds
+echo ^&HEAD CHID='%CHID%', TITLE='Simple demonstration case.'  /    > %outfile%
+echo ^&MESH IJK=36,24,24, XB=0.0,3.6,0.0,2.4,0.0,2.4 /             >> %outfile%
+echo ^&MISC GVEC=%gravx%,0.0,%gravz% /                                     >> %outfile%
+echo ^&TIME T_END=60.0 /                                           >> %outfile%
+echo ^&REAC FUEL='PROPANE', SOOT_YIELD=0.01 /                      >> %outfile%  
+echo ^&SURF ID='BURNER', HRRPUA=%firesize%, COLOR='RED' /          >> %outfile%
+echo ^&OBST XB=0.0,0.4,1.0,1.4,0.0,0.2 /                           >> %outfile%
+echo ^&VENT XB=0.0,0.4,1.0,1.4,0.2,0.2, SURF_ID='BURNER' /         >> %outfile%
+echo ^&VENT XB=3.6,3.6,0.8,1.6,0.0,%doorheight%, SURF_ID='OPEN' /  >> %outfile%
+echo ^&BNDF QUANTITY='GAUGE HEAT FLUX' /                           >> %outfile%
+echo ^&SLCF PBY=1.2, QUANTITY='TEMPERATURE', VECTOR=.TRUE. /       >> %outfile%
+echo ^&TAIL /                                                      >> %outfile%
+
