3513d181d754205d811f4694ab6da2f7aeee1320
gforney <gforney@gmail.com>
2024-04-28 22:37:27 -0400

    demo: add an 'outside' space to demo case

--- vistest/Examples/makecase.sh
+++ vistest/Examples/makecase.sh
@@ -15,7 +15,12 @@ cat << EOF > $outfile
 &SURF ID='BURNER', HRRPUA=$firesize, COLOR='RED' /          
 &OBST XB=0.0,0.4,1.0,1.4,0.0,0.2 /                           
 &VENT XB=0.0,0.4,1.0,1.4,0.2,0.2, SURF_ID='BURNER' /         
-&VENT XB=3.6,3.6,0.8,1.6,0.0,$doorheight, SURF_ID='OPEN' /  
+&OBST XB=3.5,3.6,0.0,2.4,0.0,2.4 /                           
+&HOLE XB=3.4,3.7,0.8,1.6,0.0,$doorheight /                  
+&VENT XB=3.6,4.8,0.0,2.4,2.4,2.4, SURF_ID='OPEN' /           
+&VENT XB=3.6,4.8,0.0,0.0,0.0,2.4, SURF_ID='OPEN' /           
+&VENT XB=3.6,4.8,2.4,2.4,0.0,2.4, SURF_ID='OPEN' /           
+&VENT XB=4.8,4.8,0.0,2.4,0.0,2.4, SURF_ID='OPEN' /           
 &BNDF QUANTITY='GAUGE HEAT FLUX' /                           
 &SLCF PBY=1.2, QUANTITY='TEMPERATURE', VECTOR=.TRUE. /       
 &TAIL /                                                      
