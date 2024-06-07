d6a565684d11ad5b284765086896035213138c33
gforney <gforney@gmail.com>
2024-06-07 19:11:19 -0400

    smv vv: minor updates to benchmark cases and scripts

--- vistest/benchmarks/runcases.sh
+++ vistest/benchmarks/runcases.sh
@@ -2,11 +2,11 @@
 
 QFDS=../../../fds/Utilities/Scripts/qfds.sh
 
-$QFDS -t -p 64 plume64.fds
-$QFDS -t -p 64 plume64r.fds
+$QFDS -t -p  2 thouse5.fds
+$QFDS -t -p  2 thouse5r.fds
 $QFDS -t -p  8 mplume5c8.fds
 $QFDS -t -p  8 mplume5c8r.fds
 $QFDS -t -p 18 levelset2.fds
 $QFDS -t -p 18 levelset2r.fds
-$QFDS -t -p  2 thouse5.fds
-$QFDS -t -p  2 thouse5r.fds
+$QFDS -t -p 64 plume64.fds
+$QFDS -t -p 64 plume64r.fds
