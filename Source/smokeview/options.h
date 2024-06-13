3821905d6ac8404ab8a5f99e20e1e68a5dcc8cfe
gforney <gforney@gmail.com>
2024-06-13 00:02:59 -0400

    smokeview source: add pp_OBST directdive

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -3,6 +3,7 @@
 
 #include "options_common.h"
 
+//#define pp_FDS            // create a 1 mesh input file
 //#define pp_PARTBOUND_MULTI // compute particle bounds in the background
 //#define pp_LUA            // turn on LUA option
 #define pp_SLICE_MULTI      // load slice files in parallel
