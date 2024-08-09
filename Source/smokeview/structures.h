8d0be599ff1bb6a4309fa5c5bf37c1526e6ab159
gforney <gforney@gmail.com>
2024-08-09 13:37:08 -0400

    smokeview source: remove pp_SLICE_MULTI directive

--- Source/smokeview/structures.h
+++ Source/smokeview/structures.h
@@ -1064,9 +1064,6 @@ typedef struct _slicedata {
 #ifdef pp_SLICEFRAME
   framedata *frameinfo;
 #endif
-#ifdef pp_SLICE_MULTI
-  int loadstatus;
-#endif
 } slicedata;
 
 /* --------------------------  slicemenudata ------------------------------------ */
