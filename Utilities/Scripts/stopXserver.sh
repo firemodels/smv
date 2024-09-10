ca570264daa5e3e00ebbf0e7cb2bdb55edc3a5fc
gforney <gforney@gmail.com>
2024-09-10 11:23:31 -0400

    smv vv: correct Xserver scripts so smokeview can share a node when running in batch mode on a linux system

--- Utilities/Scripts/stopXserver.sh
+++ Utilities/Scripts/stopXserver.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 if [ "`uname`" != "Darwin" ]; then
+  display_port=`id -u`
+  lockfile=/tmp/startXlock$display_port
   PAUSE=1
   echo "shutting down graphics environment (pausing $PAUSE s)"
   sleep $PAUSE
   kill $SMV_ID
+  rm -f $lockfile
 fi
