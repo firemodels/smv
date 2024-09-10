7a53f7c139a9d2f51eb2f2c74a26a50c376cbd16
gforney <gforney@gmail.com>
2024-09-10 09:48:58 -0400

    smv vv: add scripts to test smokeview image generation

---
+++ Verification/scripts/SMV_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+$QFDS -d Visualization plume5c.fds
+$QFDS -d Visualization thouse5.fds
---
+++ Verification/scripts/test_pictures.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------------
+# --------------------- beginning of script ---------------------------------
+# ---------------------------------------------------------------------------
+
+CURDIR=`pwd`
+cd ../../..
+export GITROOT=`pwd`
+cd $CURDIR/..
+export BASEDIR=`pwd`
+PLATFORM=linux
+COMPILER=intel
+DEBUG=
+TEST=
+use_installed=
+RUN_SMV=1
+RUN_WUI=1
+QUEUE=batch
+
+export SMV=$GITROOT/smv/Build/smokeview/${COMPILER}_$VERSION2/smokeview_$VERSION
+export SMVBINDIR=$GITROOT/bot/Bundlebot/smv/for_bundle
+
+RUNSMV="$GITROOT/smv/Utilities/Scripts/qsmv.sh -j $JOBPREFIX $use_installed -q $QUEUE"
+export QFDS=$RUNSMV
+export RUNCFAST=$RUNSMV
+
+echo Generating images
+
+cd $GITROOT/smv/Verification
+scripts/SMV_test.sh
+cd $CURDIDR
