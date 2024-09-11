150b51ae3a69673cc55dc2b202e449a88be2030c
gforney <gforney@gmail.com>
2024-09-10 20:48:23 -0400

    smv vv: remove GETNEWPORT routine from startXserver script

--- Utilities/Scripts/startXserver.sh
+++ Utilities/Scripts/startXserver.sh
@@ -2,26 +2,12 @@
 IDFILE=$1
 DISPLAY_PORT=$2
 
-display_port=`id -u`
 PAUSE=1
-GETNEWPORT () 
-{
-  nmatches=`ps a -e | grep Xvfb | grep $display_port | grep -v grep | wc | awk '{print $1}'`
-  while [ $nmatches -ne 0 ] ; do
-    display_port=`expr $display_port + 1`
-    nmatches=`ps a -e | grep Xvfb | grep $display_port | grep -v grep | wc | awk '{print $1}'`
-  done
-}
 
 if [ "`uname`" != "Darwin" ]; then
   echo "setting up graphics environment (pausing $PAUSE s)"
-  if [ "$DISPLAYPPORT" == "" ]; then
-    GETNEWPORT 
-  else
-    display_port=$DISPLAY_PORT
-  fi
-  Xvfb :$display_port -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
+  Xvfb :$DISPLAY_PORT -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
   echo $! > $IDFILE
-  export DISPLAY=:$display_port
+  export DISPLAY=:$DISPLAY_PORT
   sleep $PAUSE
 fi
