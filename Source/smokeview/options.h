7b644f09fece010253bd0f7bf0ee19a79a8db6d8
gforney <gforney@gmail.com>
2024-11-06 12:43:31 -0500

    smokeview source: add option to display values besides selected particles

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -13,6 +13,7 @@
 #define pp_THREAD           // turn on multi-threading
 //#define pp_FDS            // create a 1 mesh input file
 #define pp_HIDDEN6            // handle hidden6 info on obst line
+#define pp_PART_SHOW        // add option to show selected particle values as well as tags
 
 #ifdef pp_FRAME          // turn on each frame type if pp_FRAME is set
 #define pp_BOUNDFRAME    // turn on frame code for boundary files
