1f55321539a9dd4b18fe614c8c18d33a1ebd08ac
gforney <gforney@gmail.com>
2024-11-08 09:37:00 -0500

    smokeview source: remove pp_HIDDEN6 directive

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -12,7 +12,6 @@
 #define pp_GPU              // support the GPU
 #define pp_THREAD           // turn on multi-threading
 //#define pp_FDS            // create a 1 mesh input file
-#define pp_HIDDEN6            // handle hidden6 info on obst line
 #define pp_PART_SHOW        // add option to show selected particle values as well as tags
 #define pp_OBST_DEBUG          // use new DrawFace routine by default
 
