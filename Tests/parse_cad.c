a473187fe228cdffcc5b6737a047aa1a62211c99
Jake O'Shannessy <joshannessy@gmail.com>
2025-01-05 22:03:14 +0000

    add CAD parsing geometry to cmake tests

--- Tests/parse_cad.c
+++ Tests/parse_cad.c
@@ -12,9 +12,17 @@ int show_version;
 char append_string[1024];
 
 int main(int argc, char **argv) {
+  if(argc < 2) {
+    fprintf(stderr, "error: insufficient arguments");
+  }
   initMALLOC();
+  char *input_path = argv[1];
   cadgeom_collection *coll = CreateCADGeomCollection(20);
   assert(NCADGeom(coll) == 0);
+  int res = ReadCADGeomToCollection(coll, input_path, 100.0);
+  fprintf(stderr, "#CAD defs: %i\n", NCADGeom(coll));
+  assert(res == 0);
+  assert(NCADGeom(coll) == 1);
   FreeCADGeomCollection(coll);
   return 0;
 }
