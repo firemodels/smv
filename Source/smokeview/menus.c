a77bd2e15ae6f5aacefb618a004c9953767dde5c
gforney <gforney@gmail.com>
2025-01-14 11:33:53 -0500

    smokeview source: fix clang warnings in menus.c

--- Source/smokeview/menus.c
+++ Source/smokeview/menus.c
@@ -386,16 +386,16 @@ int ComparePatchLabels(const void *arg1, const void *arg2){
 /* ----------------------- PrintFileSizes ----------------------------- */
 
 void PrintFileSizes(char *type, float val, float val2){
-  char label[100], label2[100], *labelptr, *labelptr2;
+  char label[100], label2[100], *labelptr=NULL, *labelptr2=NULL;
 
   if(val>0.0)labelptr=GetFloatFileSizeLabel(val, label);
   if(val2>0.0)labelptr2=GetFloatFileSizeLabel(val2, label2);
   if(val>0.0){
     if(val2>0.0){
-      printf("  %s/compressed: %s/%s\n", type,labelptr,labelptr2);
+      if(labelptr!=NULL&&labelptr2!=NULL)printf("  %s/compressed: %s/%s\n", type,labelptr,labelptr2);
     }
     else{
-      printf("  %s: %s\n", type,labelptr);
+      if(labelptr != NULL)printf("  %s: %s\n", type,labelptr);
     }
   }
 }
@@ -588,8 +588,6 @@ void ShowMultiSliceMenu(int value){
       slicedata *sd;
 
       mslicei = global_scase.slicecoll.multisliceinfo+value;
-      mdisplay = 0;
-
       sd = global_scase.slicecoll.sliceinfo+mslicei->islices[0];
       if(slicefile_labelindex==sd->slicefile_labelindex){
         if(plotstate!=DYNAMIC_PLOTS){
@@ -3064,6 +3062,8 @@ void MemoryTest(void){
     }
     STOP_TIMER(mem_timer);
 
+    diskread = 0;
+    diskwrite = 0;
     stream = fopen("test.bin", "wb");
     if(stream != NULL){
       diskwrite = 1;
@@ -4207,9 +4207,9 @@ void SetupPart(int value){
   if(value>=0){
     partdata *parti;
 
+    assert(value >= 0 && value < global_scase.npartinfo);
+    value = CLAMP(value, 0, global_scase.npartinfo - 1);
     parti = global_scase.partinfo+value;
-    assert(value>=0&&value<global_scase.npartinfo);
-    value = CLAMP(value, 0, global_scase.npartinfo-1);
     parti->finalize = 1;
   }
   else{
