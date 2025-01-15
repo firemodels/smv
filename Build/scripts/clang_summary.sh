afe9b10802b935ce5ce6f76c0594ac27f2a43f2d
gforney <gforney@gmailcom>
2025-01-15 18:43:11 -0500

    clang check: update clang check scripts

--- Build/scripts/clang_summary.sh
+++ Build/scripts/clang_summary.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 OUTFILE=$1
-echo > $OUTFILE
-echo `pwd` >> $OUTFILE
-for file in *.i; do
-  echo "" >> $OUTFILE
-  echo "**********************" >> $OUTFILE
-  echo $file >> $OUTFILE
-  echo "**********************" >> $OUTFILE
-  cat $file >> $OUTFILE
+if [ "$OUTFILE" == "" ]; then
+  OUTFILE=clang_warnings.txt
+fi
+HAVE_WARNINGS=
+for file in *.chk; do
+  if [ -s $file ]; then
+    HAVE_WARNINGS=1
+    break
+  fi
+done
+echo clang warnings > $OUTFILE
+echo -------------- >> $OUTFILE
+if [ "$HAVE_WARNINGS" == "" ]; then
+  echo no warnings were found > $OUTFILE
+  exit
+fi
+for file in *.chk; do
+  if [ -s $file ]; then
+    nwarnings=`tail -1 $file | awk '{print $1}'`
+    echo $file: $nwarnings >> $OUTFILE
+  fi
 done
--- Source/scripts/check_code.sh
+++ Source/scripts/check_code.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 file=$1
-clang --analyze -I ../smokeview -I ../glui_v2_1_beta -I ../shared -I ../glew -I . -I ../gd-2.0.15 -I ../zlib128 $file >& $file.out
+base=${file%.*}
+chkfile=${base}.chk
+clang --analyze -I ../smokeview -I ../glui_v2_1_beta -I ../shared -I ../glew -I . -I ../gd-2.0.15 -I ../zlib128 $file >& $chkfile
