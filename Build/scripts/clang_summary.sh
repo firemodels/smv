bd8c6283313bb9a97877cf2b3da7e2551673ca0a
gforney <gforney@gmail.com>
2025-01-16 15:50:17 -0500

    smc check: output number of files scanned and number of warnings found when scanning smokeview source

--- Build/scripts/clang_summary.sh
+++ Build/scripts/clang_summary.sh
@@ -14,11 +14,17 @@ echo clang warnings > $OUTFILE
 echo -------------- >> $OUTFILE
 if [ "$HAVE_WARNINGS" == "" ]; then
   echo no warnings were found > $OUTFILE
-  exit
 fi
+total_warnings=0
+nfiles=0
 for file in *.chk; do
+  nfiles=$((nfiles+1))
   if [ -s $file ]; then
     nwarnings=`tail -1 $file | awk '{print $1}'`
+    total_warnings=$((total_warnings+nwarnings))
     echo $file: $nwarnings >> $OUTFILE
   fi
 done
+echo
+echo $nfiles files scanned, $total_warnings warnings found
+
