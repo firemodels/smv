0f6ddc43a0079825fb54304d27769b6e7d518fb1
gforney <gforney@gmailcom>
2025-01-16 21:35:43 -0500

    checker: correcteion to check_code script

--- Source/scripts/check_code.sh
+++ Source/scripts/check_code.sh
@@ -4,5 +4,5 @@ base=${file%.*}
 chkfile=${base}.chk
 #CHECKS="-Xanalyzer -analyzer-checker=core.DivideZero"
 CHECKS=
-INC=-I ../smokeview -I ../glui_v2_1_beta -I ../shared -I ../glew -I . -I ../gd-2.0.15 -I ../zlib128
-clang --analyze $INC $file >& $chkfile
+INC="-I ../smokeview -I ../glui_v2_1_beta -I ../shared -I ../glew -I . -I ../gd-2.0.15 -I ../zlib128"
+clang --analyze $CHECKS $INC $file >& $chkfile
