cc51c8035006e67f34b8ac090f947b04e49de6b2
gforney <gforney@gmail.com>
2025-01-16 23:18:00 -0500

    smokeview source: correct clang warnings in IOgeometry.c

--- Source/smokeview/IOgeometry.c
+++ Source/smokeview/IOgeometry.c
@@ -3115,7 +3115,7 @@ FILE_SIZE ReadGeom0(geomdata *geomi, int load_flag, int type, int *geom_frame_in
   // surf_ind ntris
   icount=-1;
   for(iframe=-1;iframe<ntimes_local;){
-    float times_local[2];
+    float times_local[2]={0.0, 0.0};
     geomlistdata *geomlisti;
     int nverts, ntris;
     int  skipframe;
@@ -3438,7 +3438,7 @@ FILE_SIZE ReadGeom2(geomdata *geomi, int load_flag, int type){
       if(geomi->geomtype==GEOM_CGEOM){
         NewMemory((void **)&locations, ntris*sizeof(int));
         NewMemory((void **)&geom_ind,  ntris*sizeof(int));
-        texture_coords = NULL;
+        NewMemory((void **)&texture_coords, 6 * sizeof(float));
       }
       else{
         NewMemory((void **)&texture_coords,6*ntris*sizeof(float));
@@ -3525,14 +3525,19 @@ FILE_SIZE ReadGeom2(geomdata *geomi, int load_flag, int type){
             for(ii = 0; ii < ntris; ii++){
               float *text_coords;
               int *tri_ind;
-              float *xy;
+              float *xy, xy_default[2] = {0.0, 0.0};
               vertdata *vert;
 
               text_coords = texture_coords + 6 * ii;
               tri_ind = ijk + 3 * ii;
 
               vert = verts + tri_ind[0] - 1;
-              xy = vert->xyz;
+              if(vert->xyz == NULL){
+                xy = xy_default;
+              }
+              else{
+                xy = vert->xyz;
+              }
               text_coords[0] = (xy[0] - xmin) * xfactor;
               text_coords[1] = (xy[1] - ymin) * yfactor;
 
@@ -3998,11 +4003,7 @@ void DrawGeomVData(vslicedata *vd){
   /* ------------------ DrawGeomData ------------------------ */
 
 #define GEOMVAL(index) ( patchi->is_compressed==0 ? vals[(index)] : (float)cvals[(index)] )
-#define GEOMTEXTURE(index, vmin, vmax) ( \
-        patchi->is_compressed==0 ? \
-        CLAMP( (vals[(index)]-vmin)/(vmax-vmin),0.0,1.0) : \
-        CLAMP( (float)cvals[(index)]/255.0,0.0,1.0) \
-        )
+#define GEOMTEXTURE(index, vmin, vmax) ( CLAMP( (vals[(index)]-vmin)/(vmax-vmin),0.0,1.0) )
 #define GEOMTEXTURE2(geomval, vmin, vmax) ( \
         patchi->is_compressed==0 ? \
         CLAMP( (geomval-vmin)/(vmax-vmin),0.0,1.0) : \
@@ -4013,7 +4014,7 @@ void DrawGeomData(int flag, slicedata *sd, patchdata *patchi, int geom_type){
   int i;
   unsigned char *ivals, *cvals;
   int is_ccell = 0;
-  float *vals;
+  float *vals=NULL;
 
   int set_valmin, set_valmax;
   char *label;
@@ -4051,7 +4052,9 @@ void DrawGeomData(int flag, slicedata *sd, patchdata *patchi, int geom_type){
      show_slice_shaded[IN_SOLID_GLUI]==1||
      show_slice_shaded[IN_GAS_GLUI] == 1))
      ){
-    for(i = 0; i < 1; i++){
+    int nloops = 1;
+    if(vals == NULL)nloops = 0;
+    for(i = 0; i < nloops; i++){
       geomdata *geomi;
       geomlistdata *geomlisti;
       int ntris, j, enable_lighting;
