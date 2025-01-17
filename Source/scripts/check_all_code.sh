468e054c949f2d5d683594e3e8810d9dc876de5e
gforney <gforney@gmail.com>
2025-01-17 11:45:04 -0500

    checker: remove check_all_code script - use the makefile target

--- Source/scripts/check_all_code.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-for file in *.c *.cpp; do
-   echo analysing $file
-   $SCRIPT_DIR/check_code.sh $file
-done
