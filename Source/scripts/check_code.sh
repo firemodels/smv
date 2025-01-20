9ac7c7addba42d8be4e02abe25fa6734a1323a22
gforney <gforney@gmailcom>
2025-01-20 11:13:29 -0500

    checker: uncomment pp_CLANG_TEST directive when -t option is used

--- Source/scripts/check_code.sh
+++ Source/scripts/check_code.sh
@@ -10,7 +10,7 @@ base=${file%.*}
 chkfile=${base}.chk
 CHECKS=
 if [ "$option" == "-t" ]; then
-  CHECKS="-Xanalyzer -analyzer-checker=core,deadcode"
+  CHECKS="-Xanalyzer -analyzer-checker=core,deadcode -D pp_CLANG_TEST"
 fi
 INC="-I ../smokeview -I ../glui_v2_1_beta -I ../shared -I ../glew -I . -I ../gd-2.0.15 -I ../zlib128"
 clang --analyze $CHECKS $INC $file >& $chkfile
