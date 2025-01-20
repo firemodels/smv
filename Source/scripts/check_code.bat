39dad175e4531da4b9d1fd77426f0c8e79a24247
gforney <gforney@gmail.com>
2025-01-20 11:16:43 -0500

    checker: define pp_CLANG_TEST when using -t option

--- Source/scripts/check_code.bat
+++ Source/scripts/check_code.bat
@@ -6,7 +6,7 @@ for %%f in (%file%) do (
 )
 set "CLANGOPTS=--analyze"
 if not "x%option%" == "x-t%" goto endif1
-  set "CLANGOPTS=--analyze -Xanalyzer -analyzer-checker=core,deadcode"
+  set "CLANGOPTS=--analyze -Xanalyzer -analyzer-checker=core,deadcode -D pp_CLANG_TEST"
 :endif1
 set COPTS=-D NDEBUG -D WIN32 -D _CONSOLE -D X64 -D GLEW_STATIC -D PTW32_STATIC_LIB
 set INC=-I ../glut_gl -I ../pthreads -I ../smokeview -I ../glui_v2_1_beta -I ../shared -I ../glew -I . -I ../gd-2.0.15 -I ../zlib128 %file%
