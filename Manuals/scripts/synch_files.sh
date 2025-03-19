f382b07e8123f999c118d4c1f367933213948ead
gforney <gforney@gmailcom>
2025-03-18 21:58:47 -0400

    scripts: move sync_files.sh script to Manuals/scripts directory

--- Manuals/scripts/synch_files.sh
+++ Manuals/scripts/synch_files.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 bibdir=../../../fds/Manuals/Bibliography
 scriptdir=../../../fds/Manuals/scripts
+cp $scriptdir/check_manuals.py ../scripts/.
+
+cd ../Bibliography
 cp $bibdir/FDS_general.bib .
 cp $bibdir/FDS_mathcomp.bib .
 cp $bibdir/FDS_refs.bib .
 cp $bibdir/authors.tex .
-cp $scriptdir/check_manuals.py ../scripts/.
