c763a1fd37f404c7d96659e715fed3ba8021913e
gforney <gforney@gmail.com>
2025-04-30 11:24:19 -0400

    smokeview source: correction to 3D temperature fire display (uncomment pp_SMOKE3D directive)

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -8,7 +8,7 @@
 #define pp_GPU              // support the GPU
 #define pp_THREAD           // turn on multi-threading
 //#define pp_FIRE_HIST        // option to output histogram of 3d fire data
-//#define pp_SMOKE3D          // tentative correction to 3D smoke using temperature
+#define pp_SMOKE3D          // tentative correction to 3D smoke using temperature
 //#define pp_SMOKE3D_GPU       // enable GPU option for 3D smoke
 
 #ifdef pp_FRAME          // turn on each frame type if pp_FRAME is set
