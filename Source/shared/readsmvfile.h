66f9e42df122e0535c28aa2a17caa1d12de1bbe5
gforney <gforney@gmail.com>
2025-05-22 13:27:51 -0400

    Revert "smokeview source: add error messages and abort smokeview if vent coordinates are out of bounds"
    This reverts commit 67cf4b6889b2f119c6f7052b6b538317694cf5f5.

--- Source/shared/readsmvfile.h
+++ Source/shared/readsmvfile.h
@@ -14,7 +14,7 @@ extern CCC parse_options parse_opts;
 EXTERNCPP int GetNDevices(char *file);
 EXTERNCPP void AddCfastCsvf(smv_case *scase);
 EXTERNCPP int ReadSMV_Init(smv_case *scase);
-EXTERNCPP int ReadSMV_Parse(smv_case *scase, bufferstreamdata *stream, int *abort_vent);
+EXTERNCPP int ReadSMV_Parse(smv_case *scase, bufferstreamdata *stream);
 EXTERNCPP int CompareSmoketypes(const void *arg1, const void *arg2);
 EXTERNCPP int IsDupTexture(smv_case *scase, texturedata *texti);
 EXTERNCPP int IsTerrainTexture(smv_case *scase, texturedata *texti);
