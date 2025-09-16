02a1bfd2a18cc2b125d2f20abb3fcfe20b73b0f5
gforney <gforney@gmail.com>
2025-09-16 19:47:56 -0400

    pnginfo build: correct pnginfo osx makefile entries

--- Build/pnginfo/gnu_osx_64/make_pnginfo.sh
+++ Build/pnginfo/gnu_osx_64/make_pnginfo.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-curdir=`pwd`
-LIBDIR=../../LIBS/gnu_osx_64/
-if [ "$BUILD_LIBS" == "1" ]; then
-  cd $LIBDIR
-  ./make_LIBS.sh
-  cd $curdir
-else
-  eval make -C ${LIBDIR} ${SMV_MAKE_OPTS} -f make_LIBS.make all
-fi
+OPTS="-G $*"
 
-rm -f *.o png_osx_64
-make -f ../Makefile gnu_osx_64
+source ../../scripts/setopts.sh $OPTS
+
+LIBDIR=../../LIBS/gnu_osx_64
+
+CURDIR=`pwd`
+cd $LIBDIR
+build_libs=
+LIBS="libgd.a libglui.a libjpeg.a libpng.a libz.a"
+
+for f in $LIBS
+do
+  if [ ! -e $f ]; then
+    build_libs=1
+  fi
+done
+if [ "$BUILD_LIBS" == "1" ]; then
+  build_libs=1
+fi
+if [ "$build_libs" == "1" ]; then
+  echo building $LIBS
+  eval ./make_LIBS.sh $OPTS
+fi
+cd $CURDIR
+
+if [ "$BUILD_ALL" == "1" ]; then
+  rm -f *.o *.mod smokeview_osx_64*
+fi
+eval make COMPILER=${COMPILER} COMPILER2=${COMPILER2} GLUT="$GLUT" ${SMV_MAKE_OPTS} -f ../Makefile gnu_osx_64
