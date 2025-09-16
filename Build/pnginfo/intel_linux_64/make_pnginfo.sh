a6e5405dfe098fbbda5c2ad0dd6dbb4cbe9fc226
gforney <gforney@gmail.com>
2025-09-16 19:33:11 -0400

    pnginfo build: corrections to pnginfo intel_linux_64 and gnu_linux_64 makefile entries

--- Build/pnginfo/intel_linux_64/make_pnginfo.sh
+++ Build/pnginfo/intel_linux_64/make_pnginfo.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-rm -f *.o convert_linux_64
-make -f ../Makefile intel_linux_64
+source ../../scripts/setopts.sh $*
+
+# Exit immediately if any of the build steps fail
+set -e
+
+curdir=`pwd`
+LIBDIR=../../LIBS/intel_linux_64/
+if [ "$BUILD_LIBS" == "1" ]; then
+  cd $LIBDIR
+  ./make_LIBS.sh
+  cd $curdir
+else
+  eval make -C ${LIBDIR} ${SMV_MAKE_OPTS} -f make_LIBS.make all
+fi
+
+if [ "$BUILD_ALL" == "1" ]; then
+  make -f ../Makefile clean
+fi
+eval make -j 4 ${SMV_MAKE_OPTS} -f ../Makefile intel_linux_64
