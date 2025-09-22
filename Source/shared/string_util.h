c6d034604d59b5a60a963f78c94ae1d80d4ca250
gforney <gforney@gmail.com>
2025-09-22 17:39:10 -0400

    smokeview source: remove FlipImage routine - not needed

--- Source/shared/string_util.h
+++ Source/shared/string_util.h
@@ -179,7 +179,6 @@ EXTERNCPP void           PRINTversion(char *progname);
 #endif
 EXTERNCPP void           EncodeData(unsigned char *buffer, int nbuffer, unsigned char *data, int ndata, int skip, int channel);
 EXTERNCPP                unsigned char *DecodeData(unsigned char *buffer, int nbuffer, int *ndataptr, int skip, int channel);
-EXTERNCPP                unsigned char *FlipImage(unsigned char *image, int width, int height, int skip);
 
 // vvvvvvvvvvvvvvvvvvvvvvvv variables vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
