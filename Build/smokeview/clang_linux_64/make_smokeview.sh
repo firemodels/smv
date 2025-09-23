b80302fe21b80ff4bbc812670fce7c9126c935bc
gforney <gforney@gmailcom>
2025-09-22 23:05:18 -0400

    smv build: add linux clang build script and makefile target

---
+++ Build/smokeview/clang_linux_64/make_smokeview.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source ../../scripts/setopts.sh $*
+
+# Exit immediately if any of the build steps fail
+set -e
+
+curdir=`pwd`
+LIBDIR=../../LIBS/clang_linux_64/
+if [ "$BUILD_LIBS" == "1" ]; then
+  cd $LIBDIR
+  ./make_LIBS.sh
+  cd $curdir
+else
+  eval make -C ${LIBDIR} ${SMV_MAKE_OPTS} ${LUA_SCRIPTING} -f make_LIBS.make all
+fi
+
+if [ "$BUILD_ALL" == "1" ]; then
+  make -f ../Makefile clean
+fi
+eval make -j 4 ${SMV_MAKE_OPTS} -f ../Makefile clang_linux_64
