c13f4357c9d36de7ea1ef20d873738879cd01de5
gforney <gforney@gmail.com>
2025-09-24 23:42:48 -0400

    build: add windows clang makefile entries and build scripts for wind2fds

---
+++ Build/wind2fds/clang_win_64/make_wind2fds.bat
@@ -0,0 +1,11 @@
+@echo off
+set arg1=%1
+
+Title Building wind2fds for 64 bit Windows
+
+erase *.obj *.exe
+make SHELL="%ComSpec%" -f ..\Makefile clang_win_64
+if x%arg1% == xbot goto skip2
+pause
+:skip2
+
