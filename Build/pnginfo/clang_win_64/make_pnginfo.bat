c547ece8da8e4113049bc96cc5a1445570d0cf7e
Jake O'Shannessy <joshannessy@gmail.com>
2025-10-08 17:55:32 +0100

    build: add $(LIB) variable to handle llvm-lib vs lib
    This also adds clang build scripts to CI.

--- Build/pnginfo/clang_win_64/make_pnginfo.bat
+++ Build/pnginfo/clang_win_64/make_pnginfo.bat
@@ -3,9 +3,11 @@ set arg1=%1
 
 Title Building pnginfo for 64 bit Windows
 
+:: build libraries if one is missing
+call ..\..\scripts\test_clang_libs.bat ..\..\LIBS\
+
 erase *.obj *.exe
 make SHELL="%ComSpec%" -f ..\Makefile clang_win_64
 if x%arg1% == xbot goto skip2
 pause
 :skip2
-
--- Source/jpeg-9b/makelib.bat
+++ Source/jpeg-9b/makelib.bat
@@ -6,7 +6,7 @@ erase *.o *.obj libjpeg.a libjpeg.lib
 set target=libjpeg.lib
 if %COMPILER% == gcc set target=libjpeg.a
 if exist finished erase finished
-make COMPILER=%COMPILER% SIZE=%SIZE% RM=erase -f ./makefile %target%
+make COMPILER=%COMPILER% LIB=%LIB% SIZE=%SIZE% RM=erase -f ./makefile %target%
 if %COPYLIB% == 1 copy %FROMLIB% %TOLIB%
 echo finished > finished
 if "x%EXIT_SCRIPT%" == "x" goto skip1
