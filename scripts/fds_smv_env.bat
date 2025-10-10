25ae9ce09dfbeea48beca1517b3fcb4b40b36575
gforney <gforney@gmail.com>
2025-10-09 21:00:02 -0400

    gui script: remove unused settings from settings script

--- scripts/fds_smv_env.bat
+++ scripts/fds_smv_env.bat
@@ -19,15 +19,10 @@ if "x%git_root%" NEQ "x" goto skipgit
 set git_root=%userprofile%\FireModels_fork
 :skipgit
 
-::set fdswikirepo=%git_root%\wikis
 set git_drive=c:
 
-::*** Linux/OSX
-::set userhome=/home/gforney
-::set bot_home=/home4/gforney
+::*** linux/osx - note: osx root uses linux_git_root variable
 set linux_git_root=FireModels_fork
-::set compiler_dir=fire-notes/INSTALL/LINUX/INTEL_19u1
-::set misc_dir=fire-notes/INSTALL/LIBS/LINUX/LIB64
 
 :: ---- MPI library locations ----
 
@@ -35,24 +30,12 @@ set linux_git_root=FireModels_fork
 set linux_mpi_version=INTEL
 set linux_intel_mpi_version=oneapi
 
-
 set osx_mpi_version=4.1.0
-::set osx_openmpi_dir=/opt/openmpi410_oneapi_64
-
-::set linux_comp_version=20u1
 set osx_comp_version=oneapi
 
 :: ---- INTEL compiler version ----
 
 set INTELVERSION=INTELoneapi1p6
-
-:: ---- Guide locations ----
-
-::set GUIDE_DIR=.bundle/pubs
-
-:: ---- openmpi library locations ----
-
-::set OPENMPI_DIR=.bundle/OPENMPI
 
 :: plink options
 set plink_options=-no-antispoof
