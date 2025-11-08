8431bb687f81316d216f284f96a396f8b6c0cfeb
gforney <gforney@gmailcom>
2025-11-08 18:38:14 -0500

    scripts: change pause time to 10 s when running xvfb-run

--- Utilities/Scripts/XVFB-RUN.sh
+++ Utilities/Scripts/XVFB-RUN.sh
@@ -4,5 +4,5 @@ notfound=`xvfb-run 2>&1 >/dev/null | tail -1 | grep "not found" | wc -l`
 if [ $notfound -eq 1 ]; then
    echo "***error: xvfb-run not installed"
 else
-   xvfb-run -s "-fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24" -a $*
+   xvfb-run -s "-w 10 -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24" -a $*
 fi
