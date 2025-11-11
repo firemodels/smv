50b70241b40d17f6cd578343cdb5b9fc06e5121a
gforney <gforney@gmailcom>
2025-11-10 20:45:36 -0500

    build scripts: remove _64 from set_path scripts

--- Build/set_path/intel_win/make_set_path.bat
+++ Build/set_path/intel_win/make_set_path.bat
@@ -6,7 +6,7 @@ call ..\..\..\Utilities\Scripts\setup_intel_compilers.bat
 
 Title Building 64 bit Windows setpath
 erase *.obj *.exe
-make SHELL="%ComSpec%" -f ../Makefile intel_win_64
+make SHELL="%ComSpec%" -f ../Makefile intel_win
 if "x%EXIT_SCRIPT%" == "x" goto skip1
 exit
 :skip1
