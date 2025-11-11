2316545fabf3db3e7f661a0ab11b9ca9b883ae16
gforney <gforney@gmailcom>
2025-11-10 20:44:27 -0500

    build scripts: remove _64 from scripts scripts

--- Build/scripts/test_gnu_libs.bat
+++ Build/scripts/test_gnu_libs.bat
@@ -9,25 +9,25 @@ cd %LIBDIR%
 set LIBDIR=%CD%
 cd %CURDIR%
 
-if NOT exist %LIBDIR%\gnu_win_64\gd.a       set BUILDLIBS=1
-if NOT exist %LIBDIR%\gnu_win_64\glui.a     set BUILDLIBS=1
+if NOT exist %LIBDIR%\gnu_win\gd.a       set BUILDLIBS=1
+if NOT exist %LIBDIR%\gnu_win\glui.a     set BUILDLIBS=1
 
 if NOT x%GLUT% == xglut goto skip_glut
-  if NOT exist %LIBDIR%\gnu_win_64\glut32.a set BUILDLIBS=1
+  if NOT exist %LIBDIR%\gnu_win\glut32.a set BUILDLIBS=1
 :skip_glut
 
 if NOT x%GLUT% == xfreeglut goto skip_freeglut
-  if NOT exist %LIBDIR%\gnu_win_64\freeglut_staticd.a   set BUILDLIBS=1
+  if NOT exist %LIBDIR%\gnu_win\freeglut_staticd.a   set BUILDLIBS=1
 :skip_freeglut
 
-if NOT exist %LIBDIR%\gnu_win_64\jpeg.a     set BUILDLIBS=1
-if NOT exist %LIBDIR%\gnu_win_64\png.a      set BUILDLIBS=1
-if NOT exist %LIBDIR%\gnu_win_64\pthreads.a set BUILDLIBS=1
-if NOT exist %LIBDIR%\gnu_win_64\zlib.a     set BUILDLIBS=1
+if NOT exist %LIBDIR%\gnu_win\jpeg.a     set BUILDLIBS=1
+if NOT exist %LIBDIR%\gnu_win\png.a      set BUILDLIBS=1
+if NOT exist %LIBDIR%\gnu_win\pthreads.a set BUILDLIBS=1
+if NOT exist %LIBDIR%\gnu_win\zlib.a     set BUILDLIBS=1
 
 if %BUILDLIBS% == 0 goto eof
 
-cd %LIBDIR%\gnu_win_64
+cd %LIBDIR%\gnu_win
 call make_LIBS_bot %GLUT%
 cd %CURDIR%
 
