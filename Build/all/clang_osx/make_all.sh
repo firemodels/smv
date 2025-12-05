719a1fc44c8700d14b274dfd3e576ec3295dc284
gforney <gforney@gmail.com>
2025-12-05 09:17:39 -0500

    build scripts: add some osx targets to the build all scripts

---
+++ Build/all/clang_osx/make_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euxo pipefail
+
+BUILDDIR=$(pwd)/../..
+BUILD=clang_osx
+
+function build_target() {
+    TARGET=$1
+    pushd "$BUILDDIR"/"$TARGET"/$BUILD || exit 1
+    ./make_"${TARGET}".sh
+    popd
+}
+
+build_target background
+build_target convert
+build_target env2mod
+build_target fds2fed
+build_target flush
+# build_target get_time - only runs on windows
+build_target pnginfo
+# build_target set_path - only runs on windows
+build_target smokediff
+build_target smokeview
+build_target smokezip
+build_target timep
+build_target wind2fds
