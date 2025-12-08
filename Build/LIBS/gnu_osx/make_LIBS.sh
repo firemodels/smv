93712c6a9bfe3dae1d1417b0a932ebb602926361
gforney <gforney@gmail.com>
2025-12-07 22:36:12 -0500

    build scripts: build gnu osx smokeview libraries in parallel

--- Build/LIBS/gnu_osx/make_LIBS.sh
+++ Build/LIBS/gnu_osx/make_LIBS.sh
@@ -17,39 +17,80 @@ cd $SRCDIR
 SRCDIR=`pwd`
 
 # GD
+echo building GD library
 cd $SRCDIR/gd-2.3.3
-./makelib.sh $OPTS
-cp libgd.a $LIBDIR/.
+./makelib.sh $OPTS  >& $LIBDIR/gd.out &
+pid_gd=$!
+
 
 # GLUI
+echo building glui library
 cd $SRCDIR/glui_v2_1_beta
 if [ "$GLUT" == "freeglut" ]; then
-  ./makelib.sh $OPTS -f
+  ./makelib.sh $OPTS -f >& $LIBDIR/glui.out &
+  pid_glui=$!
 else
-  ./makelib.sh $OPTS
+  ./makelib.sh $OPTS >& $LIBDIR/glui.out &
+  pid_glui=$!
 fi
-cp libglui.a $LIBDIR/.
 
 # FREEGLUT
 if [ "$GLUT" == "freeglut" ]; then
   cd $BUILDDIR/freeglut3.0.0/gnu_osx
-  ./make_freeglut.sh $OPTS
-  cp libglut.a $LIBDIR/.
+  echo building glut
+  ./make_freeglut.sh $OPTS >& $LIBDIR/glut.out &
+  pid_glut=$!
 else
   echo ***using OSX provided glut
 fi
 
 # JPEG
+echo building jpeg library
 cd $SRCDIR/jpeg-9b
-./makelib.sh $OPTS
-cp libjpeg.a $LIBDIR/.
+./makelib.sh $OPTS >& $LIBDIR/jpeg.out &
+pid_jpeg=$!
 
 # PNG
 cd $SRCDIR/png-1.6.48
-./makelib.sh $OPTS
-cp libpng.a $LIBDIR/.
+./makelib.sh $OPTS >& $LIBDIR/png.out &
+pid_png=$!
 
 # ZLIB
+echo building zlib library
 cd $SRCDIR/zlib131
-./makelib.sh $OPTS
+./makelib.sh $OPTS >& $LIBDIR/zlib.out &
+png_zlib=$!
+
+#-------------copying libraries
+
+wait $pid_gd
+cd $SRCDIR/gd-2.3.3
+echo GD library built
+cp libgd.a $LIBDIR/.
+
+wait $pid_glui
+cd $SRCDIR/glui_v2_1_beta
+echo glui library built
+cp libglui.a $LIBDIR/.
+
+if [ "$GLUT" == "freeglut" ]; then
+  wait $pid_glut
+  cd $BUILDDIR/freeglut3.0.0/gnu_osx
+  echo glut library built
+  cp libglut.a $LIBDIR/.
+fi
+
+wait $pid_jpeg
+cd $SRCDIR/jpeg-9b
+echo jpeg library built
+cp libjpeg.a $LIBDIR/.
+
+wait $pid_png
+cd $SRCDIR/png-1.6.48
+echo png library built
+cp libpng.a $LIBDIR/.
+
+wait $pid_zlib
+cd $SRCDIR/zlib131
+echo zlib library built
 cp libz.a $LIBDIR/.
