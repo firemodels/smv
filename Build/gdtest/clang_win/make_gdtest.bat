c075c8bb8e699aa5ef542d885535de6a377f58e6
gforney <gforney@gmail.com>
2025-12-19 11:38:57 -0500

    gdtest: add windows clang entry

---
+++ Build/gdtest/clang_win/make_gdtest.bat
@@ -0,0 +1,15 @@
+@echo off
+set arg1=%1
+
+:: setup compiler environment
+if x%arg1% == xbot goto skip1
+call ..\..\..\Utilities\Scripts\setup_intel_compilers.bat
+:skip1
+
+Title Building flushcache for Windows
+
+:: build libraries if one is missing
+call ..\..\scripts\test_libs.bat ..\..\LIBS\
+
+erase *.obj *.exe
+make SHELL="%ComSpec%" -f ..\Makefile clang_win
