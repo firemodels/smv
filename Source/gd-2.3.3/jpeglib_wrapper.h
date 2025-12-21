d847729fa5f8e94f2417ddc1315e2082ec247b5c
gforney <gforney@gmail.com>
2025-12-20 20:01:17 -0500

    gd source: revert previous jpeg_wrapper change

--- Source/gd-2.3.3/jpeglib_wrapper.h
+++ Source/gd-2.3.3/jpeglib_wrapper.h
@@ -2,7 +2,6 @@
 #define JPEGLIB_WRAPPER_H
 
 /* Rename 'boolean' for libjpeg to avoid conflicts */
-typedef int jpeg_boolean;
 #define boolean jpeg_boolean
 
 /* Include the actual libjpeg header */
