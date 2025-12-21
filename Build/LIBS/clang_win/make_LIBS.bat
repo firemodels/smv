a62185ba11c16cc5ed1bc44628378f2708b58ab0
gforney <gforney@gmail.com>
2025-12-20 22:02:50 -0500

    lib build: output compile results to files when building windows libraries

--- Build/LIBS/clang_win/make_LIBS.bat
+++ Build/LIBS/clang_win/make_LIBS.bat
@@ -28,39 +28,39 @@ set BUILDDIR=%CD%
 
 :: ZLIB
 cd %SRCDIR%\zlib131
-start %WAIT% makelib %OPTS% -copy libz.lib %LIBDIR%\zlib.lib
-
+start "" cmd /c "makelib %OPTS% -copy libz.lib     %LIBDIR%\zlib.lib             > %LIBDIR%\zlib.out 2>&1 "
+      
 :: JPEG
 cd %SRCDIR%\jpeg-9b
-start %WAIT% makelib %OPTS% -copy libjpeg.lib  %LIBDIR%\jpeg.lib
+start "" cmd /c "makelib %OPTS% -copy libjpeg.lib  %LIBDIR%\jpeg.lib             > %LIBDIR%\jpeg.out 2>&1 "
 
 :: PNG
 cd %SRCDIR%\png-1.6.48
-start %WAIT% makelib %OPTS% -copy libpng.lib %LIBDIR%\png.lib
+start "" cmd /c "makelib %OPTS% -copy libpng.lib   %LIBDIR%\png.lib              > %LIBDIR%\png.out 2>&1 "
 
 :: GD
 cd %SRCDIR%\gd-2.3.3
-start %WAIT% call makelib %OPTS% -copy libgd.lib %LIBDIR%\gd.lib
+start "" cmd /c "makelib %OPTS% -copy libgd.lib %LIBDIR%\gd.lib              > %LIBDIR%\gd.out 2>&1 "
 
 :: GLUT
 if x%arg3% == xfreeglut goto skip_glut
   cd %SRCDIR%\glut-3.7.6
-  start %WAIT% makelib %OPTS% -copy libglutwin.lib %LIBDIR%\glut32.lib
+  start "" cmd /c "makelib %OPTS% -copy libglutwin.lib %LIBDIR%\glut32.lib   > %LIBDIR%\glut32.out 2>&1 "
 :skip_glut
 
 :: GLUI
 cd %SRCDIR%\glui_v2_1_beta
 if x%arg3% == xfreeglut goto skip_glui1
-  start %WAIT% makelib %OPTS% -copy libglui.lib %LIBDIR%\glui.lib
+  start "" cmd /c "makelib %OPTS% -copy libglui.lib %LIBDIR%\glui.lib         > %LIBDIR%\glui.out 2>&1 "
 :skip_glui1
 
 if NOT x%arg3% == xfreeglut goto skip_glui2
-  start %WAIT% makelib_freeglut %OPTS% -copy libglui.lib %LIBDIR%\glui.lib
+  start "" cmd /c "makelib_freeglut %OPTS% -copy libglui.lib %LIBDIR%\glui.lib > %LIBDIR%\glui.out 2>&1 "
 :skip_glui2
 
 :: pthreads
 cd %SRCDIR%\pthreads
-start %WAIT% makelib %OPTS% -copy libpthreads.lib %LIBDIR%\pthreads.lib
+start "" cmd /c "makelib %OPTS% -copy libpthreads.lib %LIBDIR%\pthreads.lib > %LIBDIR%\pthread.out 2>&1 "
 
 :: FREEGLUT
 if NOT x%arg3% == xfreeglut goto skip_freeglut
