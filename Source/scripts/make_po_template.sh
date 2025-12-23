995ed85b52a727cdc39f45501ae4a108fd7a7bdb
gforney <gforney@gmail.com>
2025-12-22 23:22:10 -0500

    scripts: remove unused script

--- Source/scripts/make_po_template.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-MAKEPO=../../makepo/intel_linux/makepo_linux
-#cat *.c *.cpp | $MAKEPO | sort -u | $MAKEPO -a > smokeview_template.po
-cd ../smokeview
-cat *.c *.cpp | $MAKEPO | sort -u  > smokeview_template.po
