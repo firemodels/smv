3d601e5130e77b5a0106bfc7fea9cd7cb67fe3fd
gforney <gforney@gmail.com>
2026-02-22 10:49:03 -0500

    build scripts: update linux lib build scripts to add option to use freeglut from github

--- Build/LIBS/gnu_osx/make_LIBS.sh
+++ Build/LIBS/gnu_osx/make_LIBS.sh
@@ -22,75 +22,76 @@ git clean -dxf
 echo
 
 # GD
-echo building GD library
+echo "*** building GD library"
 cd $SRCDIR/gd-2.3.3
 ./makelib.sh $OPTS  >& $LIBDIR/gd.out &
 pid_gd=$!
 
 
-# GLUI
-echo building glui library
-cd $SRCDIR/glui_v2_1_beta
-if [ "$GLUT" == "freeglut" ]; then
-  ./makelib.sh $OPTS -f >& $LIBDIR/glui.out &
-  pid_glui=$!
-else
-  ./makelib.sh $OPTS >& $LIBDIR/glui.out &
-  pid_glui=$!
-fi
-
-# FREEGLUT
-if [ "$GLUT" == "freeglut" ]; then
-  FREEGLUTLIB=/usr/local/lib/libglut.a
-  if [ -e $FREEGLUTLIB ]; then
-    cp $FREEGLUTLIB $LIBDIR/.
-    echo libglut.a copied
-  else
-    echo ***using OSX provided glut
-  fi
-fi
-
 # JPEG
-echo building jpeg library
+echo "*** building jpeg library"
 cd $SRCDIR/jpeg-9b
 ./makelib.sh $OPTS >& $LIBDIR/jpeg.out &
 pid_jpeg=$!
 
 # PNG
-echo building png library
+echo "*** building png library"
 cd $SRCDIR/png-1.6.48
 ./makelib.sh $OPTS >& $LIBDIR/png.out &
 pid_png=$!
 
 # ZLIB
-echo building zlib library
+echo "*** building zlib library"
 cd $SRCDIR/zlib131
 ./makelib.sh $OPTS >& $LIBDIR/zlib.out &
 png_zlib=$!
+
+# FREEGLUT
+if [ "$GLUT" == "freeglut" ]; then
+  cd $LIBDIR/../freeglut/gnu_osx
+  echo "*** building freeglut library"
+  ./make_freeglut.sh &
+  pid_freeglut=$!
+else
+  echo ***using OSX provided glut
+fi
+
+# GLUI
+cd $SRCDIR/glui_v2_1_beta
+if [ "$GLUT" == "freeglut" ]; then
+  wait $pid_freeglut
+  echo freeglut library built
+  echo "*** building glui library
+  ./makelib.sh $OPTS -f >& $LIBDIR/glui.out &
+else
+  echo "*** building glui library
+  ./makelib.sh $OPTS >& $LIBDIR/glui.out &
+fi
+  pid_glui=$!
 
 #-------------copying libraries
 
 wait $pid_gd
 cd $SRCDIR/gd-2.3.3
-echo GD library built
+echo "*** GD library built"
 cp libgd.a $LIBDIR/.
 
 wait $pid_glui
 cd $SRCDIR/glui_v2_1_beta
-echo glui library built
+echo "*** glui library built"
 cp libglui.a $LIBDIR/.
 
 wait $pid_jpeg
 cd $SRCDIR/jpeg-9b
-echo jpeg library built
+echo "*** jpeg library built"
 cp libjpeg.a $LIBDIR/.
 
 wait $pid_png
 cd $SRCDIR/png-1.6.48
-echo png library built
+echo "*** png library built"
 cp libpng.a $LIBDIR/.
 
 wait $pid_zlib
 cd $SRCDIR/zlib131
-echo zlib library built
+echo "*** zlib library built"
 cp libz.a $LIBDIR/.
--- Build/LIBS/intel_linux/make_LIBS.sh
+++ Build/LIBS/intel_linux/make_LIBS.sh
@@ -22,11 +22,9 @@ cd $DIR
 OPTS="-I $*"
 source ../../../Source/scripts/setopts.sh $OPTS
 
-if [[ "$target" == "all" ]] || [[ "$target" == "clean" ]]; then
-  rm -f *.a *.out *.wrn
-  if [ "$target" == "clean" ]; then
-    exit
-  fi
+rm -f *.a *.out *.wrn
+if [ "$target" == "clean" ]; then
+  exit
 fi
 
 LIBDIR=`pwd`
@@ -38,112 +36,85 @@ SRCDIR=`pwd`
 cd ../Build
 BUILDDIR=`pwd`
 
-# GLUT
-
-if [[ "$target" == "all" ]] || [[ "$target" == "glut" ]]; then
-  if [ "$GLUT" == "freeglut" ]; then
-    echo "********** building freeglut"
-    cd $BUILDDIR/freeglut3.0.0/gnu_linux
-    ./make_freeglut.sh $OPTS >& $LIBDIR/freeglut.out &
-    pid_glut=$!
-  else
-    echo "********** building glut"
-    cd $SRCDIR/glut-3.7.6
-    ./makelib.sh $OPTS >& $LIBDIR/glut.out &
-    pid_glut=$!
-  fi
-fi
-
-if [[ "$target" == "all" ]] || [[ "$target" == "glui" ]]; then
-# GLUI
-  echo "********** building glui"
-  cd $SRCDIR/glui_v2_1_beta
-  ./makelib.sh $OPTS >& $LIBDIR/glui.out &
-  pid_glui=$!
-fi
-
-if [[ "$target" == "all" ]] || [[ "$target" == "zlib" ]]; then
 # ZLIB
-  echo "********** building zlib"
-  cd $SRCDIR/zlib131
-  ./makelib.sh $OPTS >& $LIBDIR/zlib.out &
-  pid_zlib=$!
-fi
+echo "*** building zlib"
+cd $SRCDIR/zlib131
+./makelib.sh $OPTS >& $LIBDIR/zlib.out &
+pid_zlib=$!
 
-if [[ "$target" == "all" ]] || [[ "$target" == "jpeg" ]]; then
 # JPEG
-  echo "********** building jpeg"
-  cd $SRCDIR/jpeg-9b
-  ./makelib.sh $OPTS >& $LIBDIR/jpeg.out &
-  pid_jpeg=$!
-fi
+echo "*** building jpeg"
+cd $SRCDIR/jpeg-9b
+./makelib.sh $OPTS >& $LIBDIR/jpeg.out &
+pid_jpeg=$!
 
-if [[ "$target" == "all" ]] || [[ "$target" == "png" ]]; then
 # PNG
-  echo "********** building png"
-  cd $SRCDIR/png-1.6.48
-  ./makelib.sh $OPTS >& $LIBDIR/png.out &
-  pid_png=$!
-fi
+echo "*** building png"
+cd $SRCDIR/png-1.6.48
+./makelib.sh $OPTS >& $LIBDIR/png.out &
+pid_png=$!
 
-if [[ "$target" == "all" ]] || [[ "$target" == "gd" ]]; then
 # GD
-  echo "********** building gd"
-  cd $SRCDIR/gd-2.3.3
-  ./makelib.sh $OPTS >& $LIBDIR/gd.out &
-  pid_gd=$!
+echo "*** building gd"
+cd $SRCDIR/gd-2.3.3
+./makelib.sh $OPTS >& $LIBDIR/gd.out &
+pid_gd=$!
+
+# GLUT
+if [ "$GLUT" == "freeglut" ]; then
+  echo "*** building freeglut"
+  cd $BUILDDIR/freeglut/intel_linux
+  ./make_freeglut.sh $OPTS >& $LIBDIR/freeglut.out &
+  pid_glut=$!
+else
+  echo "*** building glut"
+  cd $SRCDIR/glut-3.7.6
+  ./makelib.sh $OPTS >& $LIBDIR/glut.out &
+  pid_glut=$!
 fi
+wait $pid_glut
+GET_WARNINGS glut
+echo "*** glut library built"
+
+# GLUI
+echo "*** building glui"
+cd $SRCDIR/glui_v2_1_beta
+./makelib.sh $OPTS >& $LIBDIR/glui.out &
+pid_glui=$!
 
 #-------copy files
 
-if [[ "$target" == "all" ]] || [[ "$target" == "glut" ]]; then
-  wait $pid_glut
-  GET_WARNINGS glut
-  echo glut library built
-  if [ "$GLUT" == "freeglut" ]; then
-    cd $BUILDDIR/freeglut3.0.0/gnu_linux
-  else
-    cd $SRCDIR/glut-3.7.6
-  fi
+if [ "$GLUT" == "" ]; then
+  cd $SRCDIR/glut-3.7.6
   cp libglut.a $LIBDIR/.
 fi
 
-if [[ "$target" == "all" ]] || [[ "$target" == "glui" ]]; then
-  wait $pid_glui
-  GET_WARNINGS glui
-  echo glui library built
-  cd $SRCDIR/glui_v2_1_beta
-  cp libglui.a $LIBDIR/.
-fi
+wait $pid_glui
+GET_WARNINGS glui
+echo "*** glui library built"
+cd $SRCDIR/glui_v2_1_beta
+cp libglui.a $LIBDIR/.
 
-if [[ "$target" == "all" ]] || [[ "$target" == "zlib" ]]; then
-  wait $pid_zlib
-  GET_WARNINGS zlib
-  echo zlib library built
-  cd $SRCDIR/zlib131
-  cp libz.a $LIBDIR/.
-fi
+wait $pid_zlib
+GET_WARNINGS zlib
+echo "*** zlib library built"
+cd $SRCDIR/zlib131
+cp libz.a $LIBDIR/.
 
-if [[ "$target" == "all" ]] || [[ "$target" == "jpeg" ]]; then
-  wait $pid_jpeg
-  GET_WARNINGS jpeg
-  echo jpeg library built
-  cd $SRCDIR/jpeg-9b
-  cp libjpeg.a $LIBDIR/.
-fi
+wait $pid_jpeg
+GET_WARNINGS jpeg
+echo "*** jpeg library built"
+cd $SRCDIR/jpeg-9b
+cp libjpeg.a $LIBDIR/.
 
-if [[ "$target" == "all" ]] || [[ "$target" == "png" ]]; then
-  wait $pid_png
-  GET_WARNINGS png
-  echo png library built
-  cd $SRCDIR/png-1.6.48
-  cp libpng.a $LIBDIR/.
-fi
+wait $pid_png
+GET_WARNINGS png
+echo "*** png library built"
+cd $SRCDIR/png-1.6.48
+cp libpng.a $LIBDIR/.
 
-if [[ "$target" == "all" ]] || [[ "$target" == "gd" ]]; then
-  wait $pid_gd
-  GET_WARNINGS gd
-  echo gd library built
-  cd $SRCDIR/gd-2.3.3
-  cp libgd.a $LIBDIR/.
-fi
+wait $pid_gd
+GET_WARNINGS gd
+echo "*** gd library built"
+cd $SRCDIR/gd-2.3.3
+cp libgd.a $LIBDIR/.
