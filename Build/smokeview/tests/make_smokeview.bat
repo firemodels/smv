08ae618df999c68f8c97753fb2d3b9eb3f7b6ac7
gforney <gforney@gmail.com>
2026-02-25 19:07:22 -0500

    scripts: add smokeview build tests

---
+++ Build/smokeview/tests/make_smokeview.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+set CURDIR=%CD%
+set compile=%1
+set glut=%2
+if not "x%compile%" == "xclang"      set compile=intel
+if not "x%glut%"    == "xfreeglut"  set    glut=glut
+echo *** build smokeview using %compile% and %glut%
+echo *** cleaning repo
+cd ..\..\..
+git clean -dxf > Nul
+cd Build\smokeview\%compile%_win
+start "building %compile% using %glut%" /wait cmd /c  "make_smokeview -%glut%  "
+echo *** build complete
