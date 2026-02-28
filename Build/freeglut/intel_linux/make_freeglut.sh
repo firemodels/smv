2cd92bc5b3d46e19df1858d9493749221b7fa33c
gforney <gforney@gmail.com>
2026-02-28 07:37:37 -0500

    freeglut build: remove gamemode from freeglut intel_linux build scripts

--- Build/freeglut/intel_linux/make_freeglut.sh
+++ Build/freeglut/intel_linux/make_freeglut.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+FREEGLUTFILE=../../../../libs/freeglut/lib/libglut.a
+
 #*** clean old files
 echo "*** removing old files"
  git clean -dxf
@@ -8,6 +10,7 @@ echo "*** removing old files"
 #*** configure
 echo "*** configuring"
 cmake ../../../../freeglut -DCMAKE_INSTALL_PREFIX=../../../../libs/freeglut \
+                           -DFREEGLUT_BUILD_GAMEMODE=OFF \
                            -DCMAKE_BUILD_TYPE=Release \
                            -DFREEGLUT_BUILD_DEMOS=OFF \
                            -DCMAKE_C_COMPILER=icx \
@@ -21,3 +24,10 @@ make
 #*** install
 echo "*** installing"
 make install
+if [ -e $FREEGLUTFILE ]; then
+  ar -t $FREEGLUTFILE | awk '/gamemode/ {print $0}' | xargs -I{} ar d $FREEGLUTFILE {}
+  ranlib $FREEGLUTFILE
+  ar -t $FREEGLUTFILE | grep gamemode || echo "No gamemode objects found"
+else
+  echo ***error: freeglut file, $FREEGLUTFILE, does not exist
+fi
