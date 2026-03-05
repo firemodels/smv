3c7ed27b7771b0ed85e00bcb403dea35fe3dc989
gforney <gforney@gmail.com>
2026-03-05 14:51:57 -0500

    build scripts: correct some typos in env2mod and flush build scripts intel -> clang

--- Build/env2mod/clang_win/make_env2mod.bat
+++ Build/env2mod/clang_win/make_env2mod.bat
@@ -9,7 +9,7 @@ if NOT x%GLUT% == xfreeglut set GLUT=glut
 if not x%inc% == xinc erase *.obj *.exe 2> Nul
 
 :: build libraries if one is missing
-call ..\..\scripts\test_libs.bat ..\..\LIBS\intel_win %GLUT%
+call ..\..\scripts\test_libs.bat ..\..\LIBS\clang_win %GLUT%
 
 :: setup compiler environment
 if not defined ONEAPI_ROOT call ..\..\..\Utilities\Scripts\setup_compilers.bat intel
