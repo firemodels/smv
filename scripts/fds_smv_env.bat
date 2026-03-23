d582294e0cc9de90d429afff13aeda1c0dd89a14
gforney <gforney@gmail.com>
2026-03-23 10:39:10 -0400

    scripts: simplify gui settings script

--- scripts/fds_smv_env.bat
+++ scripts/fds_smv_env.bat
@@ -1,44 +1,23 @@
 @echo off
 
-:: ---- smokeview revision ----
-
-set smv_revision=unknown
-set fds_revision=unknown
-if exist %userprofile%\.bundle\smv_revision.txt (
-  set /p smv_revision=<%userprofile%\.bundle\smv_revision.txt
-)
-
 :: ---- log entry date ----
 
 set smvlogdate="18-Mar-2026"
 
+:: ---- smokeview revision ----
+
+set smv_revision=unknown
+if exist %userprofile%\.bundle\smv_revision.txt (
+  set /p smv_revision=<%userprofile%\.bundle\smv_revision.txt
+)
+
 :: ---- repo locations ----
 
 ::*** PC
-if "x%git_root%" NEQ "x" goto skipgit
 set git_root=%userprofile%\FireModels_fork
-:skipgit
-
-set git_drive=c:
 
 ::*** linux/osx - note: osx root uses linux_git_root variable
 set linux_git_root=FireModels_fork
-
-:: ---- MPI library locations ----
-
-:: set to INTEL if using Intel MPI library
-set linux_mpi_version=INTEL
-set linux_intel_mpi_version=oneapi
-
-set osx_mpi_version=4.1.0
-set osx_comp_version=oneapi
-
-:: ---- INTEL compiler version ----
-
-set INTELVERSION=INTELoneapi1p6
-
-:: plink options
-set plink_options=-no-antispoof
 
 :: ---- hostnames ----
 
