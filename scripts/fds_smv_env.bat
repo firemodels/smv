135a457eb77b63f1979c530b35e50937414be4ca
gforney <gforney@gmail.com>
2026-03-23 10:58:57 -0400

    scripts: set repo location relative to python script location

--- scripts/fds_smv_env.bat
+++ scripts/fds_smv_env.bat
@@ -4,21 +4,6 @@
 
 set smvlogdate="18-Mar-2026"
 
-:: ---- smokeview revision ----
-
-set smv_revision=unknown
-if exist %userprofile%\.bundle\smv_revision.txt (
-  set /p smv_revision=<%userprofile%\.bundle\smv_revision.txt
-)
-
-:: ---- repo locations ----
-
-::*** PC
-set git_root=%userprofile%\FireModels_fork
-
-::*** linux/osx - note: osx root uses linux_git_root variable
-set linux_git_root=FireModels_fork
-
 :: ---- hostnames ----
 
 ::*** linux
@@ -30,3 +15,20 @@ set linux_logon=%linux_username%@%linux_hostname%
 set osx_hostname=wildfire.campus.nist.gov
 set osx_username=%username%
 set osx_logon=%osx_username%@%osx_hostname%
+
+:: ------- should not need to change settings below -----------
+
+:: ---- smokeview revision ----
+
+set smv_revision=unknown
+if exist %userprofile%\.bundle\smv_revision.txt set /p smv_revision=<%userprofile%\.bundle\smv_revision.txt
+
+:: ---- repo locations ----
+
+::*** PC
+cd ..\..
+set git_root=%CD%
+
+::*** linux/osx - note: osx root uses linux_git_root variable
+set linux_git_root=FireModels_fork
+for %%a in ("%git_root%") do set linux_git_root=%%~nxa
