20de77b6d262ac76eead5a39d8d5dc7e29e1f0e8
gforney <gforney@gmail.com>
2026-04-04 20:14:27 -0400

    smokeview source: only call SetTimeFrameIndexWorker once per time step

--- Source/smokeview/callbacks.c
+++ Source/smokeview/callbacks.c
@@ -1672,7 +1672,6 @@ void SetTimeFrameIndexWorker(int frameindex, int stept_arg){
 void SetTimeFrameIndex(int frameindex, int stept_arg){
   INIT_PRINT_TIMER(frame_timer);
   SetTimeFrameIndexWorker(frameindex, stept_arg);
-  SetTimeFrameIndexWorker(frameindex, stept_arg);
   PRINT_TIMER(frame_timer, "SetTimeFrameIndex");
 }
 
