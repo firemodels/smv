20c1ec59084eaf2082bc52766c547c0a2b64152e
gforney <gforney@gmail.com>
2026-04-09 13:53:59 -0400

    smv vv: merge wui and smv cases

--- Verification/scripts/SMV_Cases.sh
+++ Verification/scripts/SMV_Cases.sh
@@ -55,3 +55,10 @@ $QFDS -d Visualization -p 3 vectorskipz.fds
 $QFDS -d Visualization version.fds
 $QFDS -d Visualization version2.fds
 $QFDS -d Visualization windrose.fds
+
+$QFDS -p 2  -d WUI hill_structure.fds
+$QFDS -p 9  -d WUI levelset2.fds
+$QFDS -p 16 -d WUI pine_tree.fds
+$QFDS       -d WUI tree_test2.fds
+$QFDS       -d WUI wind_test1.fds
+$QFDS -p 2  -d WUI wind_test2.fds
