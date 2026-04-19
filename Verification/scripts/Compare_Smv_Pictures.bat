3f8e5d2509aeb645cb1e4ee7b5efe4791ee5275f
gforney <gforney@gmail.com>
2026-04-19 16:28:48 -0400

    smv vv: sort compared images in decreasing order of differences

--- Verification/scripts/Compare_Smv_Pictures.bat
+++ Verification/scripts/Compare_Smv_Pictures.bat
@@ -21,6 +21,11 @@ set SMVREPODIR=%CD%
 git describe --dirty --long > %CURDIR%\smvrepo.txt
 set /p SMVREPO=<%CURDIR%\smvrepo.txt
 
+cd %CURDIR%\..\..\..\bot
+set BOTREPODIR=%CD%
+set GAWK=%BOTREPODIR%\Scripts\bin\gawk.exe
+set SORT=%BOTREPODIR%\Scripts\bin\sort.exe
+
 cd %CURDIR%\..\..\Manuals\SMV_Summary
 set SUMMARYDIR=%CD%
 
@@ -31,6 +36,9 @@ set WEBDIFFDIR=%SUMMARYDIR%\diffs\images
 set HTMLFILE=%SUMMARYDIR%\index.html
 
 for %%d in ( SMV_User_Guide SMV_Verification_Guide ) do (
+if %%d == SMV_User_Guide         set FILELIST=%CURDIR%\user_filelist.txt
+if %%d == SMV_Verification_Guide set FILELIST=%CURDIR%\veri_filelist.txt
+if exist !FILELIST! erase !FILELIST!
 cd %MANDIR%\%%d\SCRIPT_FIGURES
 for %%f in (*.png) do (
     set "FROMFILE=%FROMDIR%\%%f"
@@ -40,9 +48,13 @@ for %%f in (*.png) do (
     if exist "!FROMFILE!" if exist "!TOFILE!" (
        echo comparing %%f
        magick compare -metric RMSE "!FROMFILE!" "!TOFILE!" "!DIFFFILE!" > "!OUTFILE!" 2>&1
+       %GAWK% -v fname="%%f" -F"[()]" "{print fname, $2}" !OUTFILE! >> !FILELIST!
     )
 )
 )
+
+%SORT% -k2,2gr %CURDIR%\user_filelist.txt -o %CURDIR%\user_filelist.txt 
+%SORT% -k2,2gr %CURDIR%\veri_filelist.txt -o %CURDIR%\veri_filelist.txt
 
 set WIDTH=300
 
@@ -59,27 +71,31 @@ for %%d in (SMV_User_Guide SMV_Verification_Guide) do (
 echo ^<a name="%%d"^>
 if %%d == SMV_User_Guide         echo [SMV_User_Guide]
 if %%d == SMV_User_Guide         echo [^<a href="#SMV_Verification_Guide"^>SMV_Verification_Guide^</a^>]
+
 if %%d == SMV_Verification_Guide echo [^<a href="#SMV_User_Guide"^>SMV_User_Guide^</a^>]
 if %%d == SMV_Verification_Guide echo [SMV_Verification_Guide]
 
+if %%d == SMV_User_Guide         set FILELOOP=%CURDIR%\user_filelist.txt 
+if %%d == SMV_Verification_Guide set FILELOOP=%CURDIR%\veri_filelist.txt 
+
 echo ^<table border=on^>
 cd %MANDIR%\%%d\SCRIPT_FIGURES
-for %%f in (*.png) do (
-    set "FROMFILE=%FROMDIR%\%%f"
-    set "TOFILE=%%f"
-    set "DIFFFILE=%DIFFDIR%\%%f"
-    set "OUTFILE=%DIFFDIR%\%%f.txt"
+for /f "tokens=1,2" %%A in (!FILELOOP!) do (
+    set "FROMFILE=%FROMDIR%\%%A"
+    set "TOFILE=%%A"
+    set "DIFFFILE=%DIFFDIR%\%%A"
+    set "OUTFILE=%DIFFDIR%\%%A.txt"
     if exist "!FROMFILE!" if exist "!TOFILE!" (
       set /p FIRSTLINE=<!OUTFILE!
-      copy !FROMFILE! %WEBFROMDIR%\%%f >NUL 2>&1
-      copy !TOFILE!   %WEBTODIR%\%%f   >NUL 2>&1
-      copy !DIFFFILE! %WEBDIFFDIR%\%%f >NUL 2>&1
+      copy !FROMFILE! %WEBFROMDIR%\%%A >NUL 2>&1
+      copy !TOFILE!   %WEBTODIR%\%%A   >NUL 2>&1
+      copy !DIFFFILE! %WEBDIFFDIR%\%%A >NUL 2>&1
       echo ^<tr^>
-      echo ^<td^>^<img src="diffs/base/%%f"   width=%WIDTH% ^>^</td^>
-      echo ^<td^>^<img src="images/%%f"       width=%WIDTH% ^>^</td^>
-      echo ^<td^>^<img src="diffs/images/%%f" width=%WIDTH% ^>^</td^>
+      echo ^<td^>^<img src="diffs/base/%%A"   width=%WIDTH% ^>^</td^>
+      echo ^<td^>^<img src="images/%%A"       width=%WIDTH% ^>^</td^>
+      echo ^<td^>^<img src="diffs/images/%%A" width=%WIDTH% ^>^</td^>
       echo ^</tr^>
-      echo ^<tr^>^<td colspan=3 align=center^>%%f - !FIRSTLINE!^</th^>^</tr^>
+      echo ^<tr^>^<td colspan=3 align=center^>%%A - %%B^</th^>^</tr^>
     )
 )
 echo ^</table^>
